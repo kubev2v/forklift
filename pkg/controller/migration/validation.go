57abecd63969fdb7606df66241dcf6f64aa4a354
Sam Lucidi <slucidi@redhat.com>
2021-02-11 17:09:02 -0500

    Validate the list of VMs to be canceled

--- pkg/controller/migration/validation.go
+++ pkg/controller/migration/validation.go
@@ -2,12 +2,14 @@ package migration
 
 import (
 	"context"
+	"errors"
 	libcnd "github.com/konveyor/controller/pkg/condition"
 	liberr "github.com/konveyor/controller/pkg/error"
 	libref "github.com/konveyor/controller/pkg/ref"
 	api "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1alpha1"
 	plancnt "github.com/konveyor/forklift-controller/pkg/controller/plan"
-	"k8s.io/apimachinery/pkg/api/errors"
+	"github.com/konveyor/forklift-controller/pkg/controller/provider/web"
+	k8serr "k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -16,6 +18,8 @@ import (
 const (
 	PlanNotValid = "PlanNotValid"
 	PlanNotReady = "PlanNotReady"
+	VMNotFound   = "VMNotFound"
+	VMNotUnique  = "VMNotUnique"
 	Running      = "Running"
 	Executing    = plancnt.Executing
 	Succeeded    = plancnt.Succeeded
@@ -35,8 +39,9 @@ const (
 
 // Reasons
 const (
-	NotSet   = "NotSet"
-	NotFound = "NotFound"
+	NotSet    = "NotSet"
+	NotFound  = "NotFound"
+	Ambiguous = "Ambiguous"
 )
 
 // Statuses
@@ -46,7 +51,7 @@ const (
 )
 
 //
-// Validate the plan resource.
+// Validate the migration resource.
 func (r *Reconciler) validate(migration *api.Migration) (plan *api.Plan, err error) {
 	newCnd := libcnd.Condition{
 		Type:     PlanNotValid,
@@ -66,7 +71,7 @@ func (r *Reconciler) validate(migration *api.Migration) (plan *api.Plan, err err
 		Name:      ref.Name,
 	}
 	err = r.Get(context.TODO(), key, plan)
-	if errors.IsNotFound(err) {
+	if k8serr.IsNotFound(err) {
 		err = nil
 		newCnd.Reason = NotFound
 		migration.Status.SetCondition(newCnd)
@@ -86,6 +91,63 @@ func (r *Reconciler) validate(migration *api.Migration) (plan *api.Plan, err err
 				Message:  "The `plan` does not have Ready condition.",
 			})
 		return
+	}
+
+	// Validate the refs in the Cancel array
+	notFound := libcnd.Condition{
+		Type:     VMNotFound,
+		Status:   True,
+		Reason:   NotFound,
+		Category: Warn,
+		Message:  "VM not found.",
+		Items:    []string{},
+	}
+	ambiguous := libcnd.Condition{
+		Type:     VMNotUnique,
+		Status:   True,
+		Reason:   Ambiguous,
+		Category: Warn,
+		Message:  "VM reference is ambiguous.",
+		Items:    []string{},
+	}
+	source := plan.Spec.Provider.Source
+	provider := &api.Provider{}
+	key = client.ObjectKey{
+		Namespace: source.Namespace,
+		Name:      source.Name,
+	}
+	err = r.Get(context.TODO(), key, provider)
+	if err != nil {
+		err = liberr.Wrap(err)
+		return
+	}
+
+	inventory, err := web.NewClient(provider)
+	if err != nil {
+		return
+	}
+	for _, ref := range migration.Spec.Cancel {
+		_, err = inventory.VM(&ref)
+		if err != nil {
+			if errors.As(err, &web.NotFoundError{}) {
+				notFound.Items = append(notFound.Items, ref.String())
+				err = nil
+				continue
+			}
+			if errors.As(err, &web.RefNotUniqueError{}) {
+				ambiguous.Items = append(ambiguous.Items, ref.String())
+				err = nil
+				continue
+			}
+			return
+		}
+	}
+
+	if len(notFound.Items) > 0 {
+		migration.Status.SetCondition(notFound)
+	}
+	if len(ambiguous.Items) > 0 {
+		migration.Status.SetCondition(ambiguous)
 	}
 
 	return
