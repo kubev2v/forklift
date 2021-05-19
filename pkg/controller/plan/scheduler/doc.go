e70d6d03b3ce1cb4601cbad0f0b93712a7f6ab55
Sam Lucidi <slucidi@redhat.com>
2021-05-19 15:04:30 -0400

    Add oVirt Builder and Scheduler scaffold

--- pkg/controller/plan/scheduler/doc.go
+++ pkg/controller/plan/scheduler/doc.go
@@ -5,6 +5,7 @@ import (
 	api "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1"
 	"github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1/plan"
 	plancontext "github.com/konveyor/forklift-controller/pkg/controller/plan/context"
+	"github.com/konveyor/forklift-controller/pkg/controller/plan/scheduler/ovirt"
 	"github.com/konveyor/forklift-controller/pkg/controller/plan/scheduler/vsphere"
 	"github.com/konveyor/forklift-controller/pkg/settings"
 )
@@ -22,6 +23,11 @@ func New(ctx *plancontext.Context) (scheduler Scheduler, err error) {
 	switch ctx.Source.Provider.Type() {
 	case api.VSphere:
 		scheduler = &vsphere.Scheduler{
+			Context:     ctx,
+			MaxInFlight: settings.Settings.MaxInFlight,
+		}
+	case api.OVirt:
+		scheduler = &ovirt.Scheduler{
 			Context:     ctx,
 			MaxInFlight: settings.Settings.MaxInFlight,
 		}
