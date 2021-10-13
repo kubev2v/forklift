fa4d0429250f11c9fc59521571403af9ecc2d7ef
Sam Lucidi <slucidi@redhat.com>
2021-10-13 09:29:45 -0400

    Fix pointer bug in ovirt builder's mapDisks

--- pkg/controller/plan/adapter/ovirt/builder.go
+++ pkg/controller/plan/adapter/ovirt/builder.go
@@ -294,7 +294,8 @@ func (r *Builder) mapFirmware(vm *model.Workload, cluster *model.Cluster, object
 
 func (r *Builder) mapDisks(vm *model.Workload, dataVolumes []cdi.DataVolume, object *cnv.VirtualMachineSpec) {
 	dvMap := make(map[string]*cdi.DataVolume)
-	for _, dv := range dataVolumes {
+	for i := range dataVolumes {
+		dv := dataVolumes[i]
 		dvMap[dv.Spec.Source.Imageio.DiskID] = &dv
 	}
 
