961d5e08b8e5aefa2f58fddcd49659ef8731e511
Tomáš Golembiovský <tgolembi@redhat.com>
2022-11-29 07:50:12 +0100

    controller: Use different image for cold and warm migration
    Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>

--- pkg/controller/plan/kubevirt.go
+++ pkg/controller/plan/kubevirt.go
@@ -936,6 +936,13 @@ func (r *KubeVirt) guestConversionPod(vm *plan.VMStatus, vmCr *VirtualMachine, c
 
 	// qemu group
 	fsGroup := int64(107)
+        // virt-v2v image
+        var virtV2vImage string
+        if r.Plan.Spec.Warm {
+            virtV2vImage = Settings.Migration.VirtV2vImageWarm
+        } else {
+            virtV2vImage = Settings.Migration.VirtV2vImageCold
+        }
 	pod = &core.Pod{
 		ObjectMeta: meta.ObjectMeta{
 			Namespace: r.Plan.Spec.TargetNamespace,
@@ -954,7 +961,7 @@ func (r *KubeVirt) guestConversionPod(vm *plan.VMStatus, vmCr *VirtualMachine, c
 			Containers: []core.Container{
 				{
 					Name:            "virt-v2v",
-					Image:           Settings.Migration.VirtV2vImage,
+					Image:           virtV2vImage,
 					VolumeMounts:    volumeMounts,
 					VolumeDevices:   volumeDevices,
 					ImagePullPolicy: core.PullIfNotPresent,
