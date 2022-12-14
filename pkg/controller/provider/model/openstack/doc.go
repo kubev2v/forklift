4f43dbb561630159fee48d14ecf00d4f3180b63f
Miguel Martín <32071463+mmartinv@users.noreply.github.com>
2022-12-14 17:20:12 +0100

    Define the model for the OpenStack Provider
    Signed-off-by: Arik Hadas <ahadas@redhat.com>
    Co-authored-by: Miguel Martín <32071463+mmartinv@users.noreply.github.com>

---
+++ pkg/controller/provider/model/openstack/doc.go
@@ -0,0 +1,20 @@
+package openstack
+
+import (
+	"github.com/konveyor/forklift-controller/pkg/controller/provider/model/ocp"
+)
+
+//
+// Build all models.
+func All() []interface{} {
+	return []interface{}{
+		&ocp.Provider{},
+		&Region{},
+		&Project{},
+		&Flavor{},
+		&Image{},
+		&VM{},
+		&Volume{},
+		&Snapshot{},
+	}
+}
