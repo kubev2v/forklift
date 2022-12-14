db0799e8744c62f9f7684a59a9efce3896b575bc
Miguel Martín <32071463+mmartinv@users.noreply.github.com>
2022-12-14 17:21:11 +0100

    Implement the OpenStack Provider Inventory API
    Signed-off-by: Arik Hadas <ahadas@redhat.com>
    Co-authored-by: Miguel Martín <32071463+mmartinv@users.noreply.github.com>

---
+++ pkg/controller/provider/web/openstack/base.go
@@ -0,0 +1,87 @@
+package openstack
+
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	model "github.com/konveyor/forklift-controller/pkg/controller/provider/model/openstack"
+	"github.com/konveyor/forklift-controller/pkg/controller/provider/web/base"
+	libmodel "github.com/konveyor/forklift-controller/pkg/lib/inventory/model"
+	"github.com/konveyor/forklift-controller/pkg/lib/logging"
+)
+
+//
+// Package logger.
+var log = logging.WithName("web|openstack")
+
+//
+// Fields.
+const (
+	DetailParam = base.DetailParam
+	NameParam   = base.NameParam
+)
+
+//
+// Base handler.
+type Handler struct {
+	base.Handler
+}
+
+//
+// Build list predicate.
+func (h Handler) Predicate(ctx *gin.Context) (p libmodel.Predicate) {
+	q := ctx.Request.URL.Query()
+	name := q.Get(NameParam)
+	if len(name) > 0 {
+		path := strings.Split(name, "/")
+		name := path[len(path)-1]
+		p = libmodel.Eq(NameParam, name)
+	}
+
+	return
+}
+
+//
+// Build list options.
+func (h Handler) ListOptions(ctx *gin.Context) libmodel.ListOptions {
+	detail := h.Detail
+	if detail > 0 {
+		detail = model.MaxDetail
+	}
+	return libmodel.ListOptions{
+		Predicate: h.Predicate(ctx),
+		Detail:    detail,
+		Page:      &h.Page,
+	}
+}
+
+//
+// Path builder.
+type PathBuilder struct {
+	// Database.
+	DB libmodel.DB
+	// Cached resources.
+	cache map[string]string
+}
+
+// Build.
+func (r *PathBuilder) Path(m model.Model) (path string) {
+	var err error
+	if r.cache == nil {
+		r.cache = map[string]string{}
+	}
+	switch m.(type) {
+	case *model.VM:
+		path = m.(*model.VM).Name
+	}
+
+	if err != nil {
+		log.Error(
+			err,
+			"path builder failed.",
+			"model",
+			libmodel.Describe(m))
+	}
+
+	return
+}
---
+++ pkg/controller/provider/web/openstack/doc.go
@@ -0,0 +1,26 @@
+package openstack
+
+import (
+	api "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1"
+	"github.com/konveyor/forklift-controller/pkg/controller/provider/web/base"
+	"github.com/konveyor/forklift-controller/pkg/lib/inventory/container"
+	libweb "github.com/konveyor/forklift-controller/pkg/lib/inventory/web"
+)
+
+//
+// Routes
+const (
+	Root = base.ProvidersRoot + "/" + string(api.OpenStack)
+)
+
+//
+// Build all handlers.
+func Handlers(container *container.Container) []libweb.RequestHandler {
+	return []libweb.RequestHandler{
+		&ProviderHandler{
+			Handler: base.Handler{
+				Container: container,
+			},
+		},
+	}
+}
