c65e314e1386610c35c207eec0d808b82ddb8d59
Miguel Martín <32071463+mmartinv@users.noreply.github.com>
2022-12-14 17:20:12 +0100

    Define the model for the OpenStack Provider
    Signed-off-by: Arik Hadas <ahadas@redhat.com>
    Co-authored-by: Miguel Martín <32071463+mmartinv@users.noreply.github.com>

---
+++ pkg/controller/provider/model/openstack/tree.go
@@ -0,0 +1,24 @@
+package openstack
+
+import (
+	"github.com/konveyor/forklift-controller/pkg/controller/provider/model/base"
+	libref "github.com/konveyor/forklift-controller/pkg/lib/ref"
+)
+
+//
+// Kinds
+var (
+	VMKind       = libref.ToKind(VM{})
+	ProjectKind  = libref.ToKind(Project{})
+	ImageKind    = libref.ToKind(Image{})
+	FlavorKind   = libref.ToKind(Flavor{})
+	VolumeKind   = libref.ToKind(Volume{})
+	SnapshotKind = libref.ToKind(Snapshot{})
+)
+
+//
+// Types.
+type Tree = base.Tree
+type TreeNode = base.TreeNode
+type BranchNavigator = base.BranchNavigator
+type ParentNavigator = base.ParentNavigator
