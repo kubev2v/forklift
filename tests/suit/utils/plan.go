bf814820bcda8557cc1e895e4130c809968b7e97
Evgeny Slutsky <eslutsky@redhat.com>
2023-01-05 15:24:39 +0200

    added test for creating storage map and plan
    Signed-off-by: Evgeny Slutsky <eslutsky@redhat.com>

---
+++ tests/suit/utils/plan.go
@@ -0,0 +1,93 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	forkliftv1 "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1"
+	"github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1/plan"
+	"github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1/provider"
+	"github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1/ref"
+	corev1 "k8s.io/api/core/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
+	crclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"time"
+)
+
+// CreatePlanFromDefinition is used by tests to create a Plan
+func CreatePlanFromDefinition(cl crclient.Client, def *forkliftv1.Plan) error {
+	var err error
+	err = cl.Create(context.TODO(), def, &crclient.CreateOptions{})
+
+	if err == nil || apierrs.IsAlreadyExists(err) {
+		return nil
+	}
+	return err
+}
+
+func NewPlan(namespace string, providerIdentifier forkliftv1.Provider, planName string, storageMap string, networkMap string, vmIds []string) *forkliftv1.Plan {
+
+	plan := &forkliftv1.Plan{
+		TypeMeta: v1.TypeMeta{
+			Kind:       "Plan",
+			APIVersion: "forklift.konveyor.io/v1beta1",
+		},
+		ObjectMeta: v1.ObjectMeta{
+			Name:      planName,
+			Namespace: providerIdentifier.Namespace,
+		},
+		Spec: forkliftv1.PlanSpec{
+			Provider: provider.Pair{
+				Destination: corev1.ObjectReference{
+					Name:      "host",
+					Namespace: namespace,
+				},
+				Source: corev1.ObjectReference{
+					Name:      providerIdentifier.Name,
+					Namespace: providerIdentifier.Namespace,
+				}},
+			Archived:        false,
+			Warm:            false,
+			TargetNamespace: namespace,
+			Map: plan.Map{
+				Storage: corev1.ObjectReference{
+					Name:      storageMap,
+					Namespace: providerIdentifier.Namespace,
+				},
+				Network: corev1.ObjectReference{
+					Name:      networkMap,
+					Namespace: providerIdentifier.Namespace,
+				},
+			},
+			VMs: []plan.VM{
+				{
+					Ref: ref.Ref{
+						//ID: vm.MustId(),
+						Name: vmIds[0],
+					},
+				},
+			},
+		},
+	}
+	return plan
+}
+
+func WaitForPlanReadyWithTimeout(cl crclient.Client, namespace string, planName string, timeout time.Duration) error {
+	planIdentifier := types.NamespacedName{Namespace: namespace, Name: planName}
+
+	returnedMap := &forkliftv1.Plan{}
+
+	err := wait.PollImmediate(3*time.Second, timeout, func() (bool, error) {
+		err := cl.Get(context.TODO(), planIdentifier, returnedMap)
+		if err != nil || !returnedMap.Status.Conditions.IsReady() {
+			return false, err
+		}
+		return true, nil
+	})
+	if err != nil {
+		return fmt.Errorf("Plan %s not ready within %v", planName, timeout)
+	}
+	return nil
+}
