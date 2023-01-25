kubev2v/forklift

commit 7918bbdbbcaabc047f0a4223eb30745335a4ee4a

build: Build separate images for cold and warm migration
Later we plan to use CentoOS Stream 9 as a base image for our
containers. However, virt-v2v in C9S does not have the features needed
for warm migration. Therefore we will need to have one image for warm
migration with C8S and another image for cold migration with C9S.

For now both images will be based on C8S. The switch to C9S for cold
migration image will be done later when the controller and the container
entrypoint are prepared for the change.

Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>

Makefile
@@ -105,8 +105,15 @@ push-virt-v2v-image: build-virt-v2v-image
 	$(CONTAINER_CMD) tag virt-v2v:forklift-virt-v2v ${VIRT_V2V_IMAGE}
 	$(CONTAINER_CMD) push ${VIRT_V2V_IMAGE}
 
+build-virt-v2v-warm-image:
+	bazel run virt-v2v:forklift-virt-v2v-warm
+
+push-virt-v2v-warm-image: build-virt-v2v-warm-image
+	$(CONTAINER_CMD) tag virt-v2v:forklift-virt-v2v-warm ${VIRT_V2V_IMAGE}
+	$(CONTAINER_CMD) push ${VIRT_V2V_IMAGE}
+
 build-operator-bundle-image:
-	bazel run operator:forklift-operator-bundle-image --action_env CONTROLLER_IMAGE=${CONTROLLER_IMAGE} --action_env VALIDATION_IMAGE=${VALIDATION_IMAGE} --action_env OPERATOR_IMAGE=${OPERATOR_IMAGE} --action_env VIRT_V2V_IMAGE=${VIRT_V2V_IMAGE} --action_env API_IMAGE=${API_IMAGE}
+	bazel run operator:forklift-operator-bundle-image --action_env CONTROLLER_IMAGE=${CONTROLLER_IMAGE} --action_env VALIDATION_IMAGE=${VALIDATION_IMAGE} --action_env OPERATOR_IMAGE=${OPERATOR_IMAGE} --action_env VIRT_V2V_IMAGE=${VIRT_V2V_IMAGE_COLD} --action_env API_IMAGE=${API_IMAGE}
 
 push-operator-bundle-image: build-operator-bundle-image
 	 $(CONTAINER_CMD) tag operator:forklift-operator-bundle-image ${OPERATOR_BUNDLE_IMAGE}
@@ -119,7 +126,7 @@ push-operator-index-image: build-operator-index-image
 	$(CONTAINER_CMD) tag operator:forklift-operator-index-image ${OPERATOR_INDEX_IMAGE}
 	$(CONTAINER_CMD) push ${OPERATOR_INDEX_IMAGE}
 
-push-all-images: push-api-image push-controller-image push-validation-image push-operator-image push-virt-v2v-image push-operator-bundle-image push-operator-index-image
+push-all-images: push-api-image push-controller-image push-validation-image push-operator-image push-virt-v2v-image push-virt-v2v-warm-image push-operator-bundle-image push-operator-index-image
 
 bazel-generate:
 	bazel run //:gazelle

BUILD.bazel
@@ -111,3 +111,12 @@ container_push(
     repository = "$${REGISTRY_ACCOUNT:-}$${REGISTRY_ACCOUNT:+/}forklift-virt-v2v",
     tag = "$${REGISTRY_TAG:-devel}",
 )
+
+container_push(
+    name = "push-forklift-virt-v2v-warm",
+    format = "Docker",
+    image = "//virt-v2v:forklift-virt-v2v-warm",
+    registry = "$${REGISTRY:-quay.io}",
+    repository = "$${REGISTRY_ACCOUNT:-}$${REGISTRY_ACCOUNT:+/}forklift-virt-v2v-warm",
+    tag = "$${REGISTRY_TAG:-devel}",
+)

hack/release-images.sh
@@ -14,11 +14,13 @@ MUST_GATHER_API_IMAGE=${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-must-gather-api:$
 UI_IMAGE=${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-ui:${REGISTRY_TAG}
 UI_PLUGIN_IMAGE=${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-console-plugin:${REGISTRY_TAG}
 VALIDATION_IMAGE=${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-validation:${REGISTRY_TAG}
-VIRT_V2V_IMAGE=${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-virt-v2v:${REGISTRY_TAG}
+VIRT_V2V_IMAGE_COLD=${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-virt-v2v:${REGISTRY_TAG}
+#VIRT_V2V_IMAGE_WARM=${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-virt-v2v-warm:${REGISTRY_TAG}
 API_IMAGE=${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-api:${REGISTRY_TAG}
 
 bazel run push-forklift-api
 bazel run push-forklift-virt-v2v
+bazel run push-forklift-virt-v2v-warm
 bazel run push-forklift-controller
 bazel run push-forklift-validation
 bazel run push-forklift-operator
@@ -29,7 +31,7 @@ bazel run push-forklift-operator-bundle \
     --action_env UI_IMAGE=${UI_IMAGE} \
     --action_env UI_PLUGIN_IMAGE=${UI_PLUGIN_IMAGE} \
     --action_env VALIDATION_IMAGE=${VALIDATION_IMAGE} \
-    --action_env VIRT_V2V_IMAGE=${VIRT_V2V_IMAGE} \
+    --action_env VIRT_V2V_IMAGE=${VIRT_V2V_IMAGE_COLD} \
     --action_env CONTROLLER_IMAGE=${CONTROLLER_IMAGE} \
     --action_env API_IMAGE=${API_IMAGE}
 bazel run push-forklift-operator-index \

virt-v2v/BUILD.bazel
@@ -17,7 +17,19 @@ container_image(
     empty_dirs = ["/disks"],
     entrypoint = ["/usr/local/bin/entrypoint"],
     env = {"LIBGUESTFS_BACKEND": "direct"},
-    files = ["bin/entrypoint"],
+    files = ["cold/entrypoint"],
+    user = "1001",
+    visibility = ["//visibility:public"],
+)
+
+container_image(
+    name = "forklift-virt-v2v-warm",
+    base = ":virt-v2v-image",
+    directory = "/usr/local/bin/",
+    empty_dirs = ["/disks"],
+    entrypoint = ["/usr/local/bin/entrypoint"],
+    env = {"LIBGUESTFS_BACKEND": "direct"},
+    files = ["warm/entrypoint"],
     user = "1001",
     visibility = ["//visibility:public"],
 )

virt-v2v/warm/entrypoint
@@ -1,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "Run virt-v2v with the following input:"
+cat /mnt/v2v/input.xml
+
+virt-v2v -v -x -i libvirtxml -o null --debug-overlays --no-copy --root=first /mnt/v2v/input.xml
+[ $? != 0 ] && exit 1
+
+echo "Conversion successful. Committing all overlays to local disks."
+for OVERLAY in /var/tmp/*.qcow2
+do
+	if ! qemu-img commit -p "$OVERLAY"
+	then
+		echo Failed to commit overlay "$OVERLAY"!
+		echo Unable to complete import!
+		exit 1
+	fi
+done
+
+echo "Commit successful. Cleaning up."
+find /var/tmp -name '*.qcow2' -exec rm -f {} \;
+
+exit 0

commit cc3c0783127211d28556ef869933890a37d38efc

build: fix virt-v2v image builds
The build targets for virt-v2v warm image and operator bundle were
broken after commit 7918bbdb.

Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>

Makefile
@@ -4,7 +4,8 @@ REGISTRY_TAG ?= devel
 CONTROLLER_IMAGE ?= ${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-controller:${REGISTRY_TAG}
 API_IMAGE ?= ${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-api:${REGISTRY_TAG}
 VALIDATION_IMAGE ?= ${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-validation:${REGISTRY_TAG}
-VIRT_V2V_IMAGE ?= ${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-virt-v2v:${REGISTRY_TAG}
+VIRT_V2V_IMAGE_COLD ?= ${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-virt-v2v:${REGISTRY_TAG}
+VIRT_V2V_IMAGE_WARM ?= ${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-virt-v2v-warm:${REGISTRY_TAG}
 OPERATOR_IMAGE ?= ${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-operator:${REGISTRY_TAG}
 OPERATOR_BUNDLE_IMAGE ?= ${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-operator-bundle:${REGISTRY_TAG}
 OPERATOR_INDEX_IMAGE ?= ${REGISTRY}/${REGISTRY_ACCOUNT}/forklift-operator-index:${REGISTRY_TAG}
@@ -102,15 +103,15 @@ build-virt-v2v-image:
 	bazel run virt-v2v:forklift-virt-v2v
 
 push-virt-v2v-image: build-virt-v2v-image
-	$(CONTAINER_CMD) tag virt-v2v:forklift-virt-v2v ${VIRT_V2V_IMAGE}
-	$(CONTAINER_CMD) push ${VIRT_V2V_IMAGE}
+	$(CONTAINER_CMD) tag virt-v2v:forklift-virt-v2v ${VIRT_V2V_IMAGE_COLD}
+	$(CONTAINER_CMD) push ${VIRT_V2V_IMAGE_COLD}
 
 build-virt-v2v-warm-image:
 	bazel run virt-v2v:forklift-virt-v2v-warm
 
 push-virt-v2v-warm-image: build-virt-v2v-warm-image
-	$(CONTAINER_CMD) tag virt-v2v:forklift-virt-v2v-warm ${VIRT_V2V_IMAGE}
-	$(CONTAINER_CMD) push ${VIRT_V2V_IMAGE}
+	$(CONTAINER_CMD) tag virt-v2v:forklift-virt-v2v-warm ${VIRT_V2V_IMAGE_WARM}
+	$(CONTAINER_CMD) push ${VIRT_V2V_IMAGE_WARM}
 
 build-operator-bundle-image:
 	bazel run operator:forklift-operator-bundle-image --action_env CONTROLLER_IMAGE=${CONTROLLER_IMAGE} --action_env VALIDATION_IMAGE=${VALIDATION_IMAGE} --action_env OPERATOR_IMAGE=${OPERATOR_IMAGE} --action_env VIRT_V2V_IMAGE=${VIRT_V2V_IMAGE_COLD} --action_env API_IMAGE=${API_IMAGE}