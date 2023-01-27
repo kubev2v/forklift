a9f978a16f50ec28eb40681b56be7dea10d244a9
Miguel Martín <mmartinv@redhat.com>
2023-01-27 13:07:40 +0100

    Implement missing Finder methods
    Signed-off-by: Miguel Martín <mmartinv@redhat.com>

--- pkg/controller/provider/web/openstack/client.go
+++ pkg/controller/provider/web/openstack/client.go
@@ -70,6 +70,11 @@ func (r *Resolver) Path(resource interface{}, id string) (path string, err error
 		r.ID = id
 		r.Link(provider)
 		path = r.SelfLink
+	case *Workload:
+		r := Workload{}
+		r.ID = id
+		r.Link(provider)
+		path = r.SelfLink
 
 	default:
 		err = liberr.Wrap(
@@ -83,6 +88,17 @@ func (r *Resolver) Path(resource interface{}, id string) (path string, err error
 	return
 }
 
+// Resource finder.
+type Finder struct {
+	base.Client
+}
+
+// With client.
+func (r *Finder) With(client base.Client) base.Finder {
+	r.Client = client
+	return r
+}
+
 // Find a resource by ref.
 // Returns:
 //
@@ -91,7 +107,143 @@ func (r *Resolver) Path(resource interface{}, id string) (path string, err error
 //	NotFoundErr
 //	RefNotUniqueErr
 func (r *Finder) ByRef(resource interface{}, ref base.Ref) (err error) {
-	return nil
+	switch resource.(type) {
+	case *VM:
+		id := ref.ID
+		if id != "" {
+			err = r.Get(resource, id)
+			return
+		}
+		name := ref.Name
+		if name != "" {
+			list := []VM{}
+			err = r.List(
+				&list,
+				base.Param{
+					Key:   DetailParam,
+					Value: "all",
+				},
+				base.Param{
+					Key:   NameParam,
+					Value: name,
+				})
+			if err != nil {
+				break
+			}
+			if len(list) == 0 {
+				err = liberr.Wrap(NotFoundError{Ref: ref})
+				break
+			}
+			if len(list) > 1 {
+				err = liberr.Wrap(RefNotUniqueError{Ref: ref})
+				break
+			}
+			*resource.(*VM) = list[0]
+		}
+	case *Workload:
+		id := ref.ID
+		if id != "" {
+			err = r.Get(resource, id)
+			return
+		}
+		name := ref.Name
+		if name != "" {
+			list := []Workload{}
+			err = r.List(
+				&list,
+				base.Param{
+					Key:   DetailParam,
+					Value: "all",
+				},
+				base.Param{
+					Key:   NameParam,
+					Value: name,
+				})
+			if err != nil {
+				break
+			}
+			if len(list) == 0 {
+				err = liberr.Wrap(NotFoundError{Ref: ref})
+				break
+			}
+			if len(list) > 1 {
+				err = liberr.Wrap(RefNotUniqueError{Ref: ref})
+				break
+			}
+			*resource.(*Workload) = list[0]
+		}
+	case *Network:
+		id := ref.ID
+		if id != "" {
+			err = r.Get(resource, id)
+			return
+		}
+		name := ref.Name
+		if name != "" {
+			list := []Network{}
+			err = r.List(
+				&list,
+				base.Param{
+					Key:   DetailParam,
+					Value: "all",
+				},
+				base.Param{
+					Key:   NameParam,
+					Value: name,
+				})
+			if err != nil {
+				break
+			}
+			if len(list) == 0 {
+				err = liberr.Wrap(NotFoundError{Ref: ref})
+				break
+			}
+			if len(list) > 1 {
+				err = liberr.Wrap(RefNotUniqueError{Ref: ref})
+				break
+			}
+			*resource.(*Network) = list[0]
+		}
+	case *VolumeType:
+		id := ref.ID
+		if id != "" {
+			err = r.Get(resource, id)
+			return
+		}
+		name := ref.Name
+		if name != "" {
+			list := []VolumeType{}
+			err = r.List(
+				&list,
+				base.Param{
+					Key:   DetailParam,
+					Value: "all",
+				},
+				base.Param{
+					Key:   NameParam,
+					Value: name,
+				})
+			if err != nil {
+				break
+			}
+			if len(list) == 0 {
+				err = liberr.Wrap(NotFoundError{Ref: ref})
+				break
+			}
+			if len(list) > 1 {
+				err = liberr.Wrap(RefNotUniqueError{Ref: ref})
+				break
+			}
+			*resource.(*VolumeType) = list[0]
+		}
+	default:
+		err = liberr.Wrap(
+			ResourceNotResolvedError{
+				Object: resource,
+			})
+	}
+
+	return
 }
 
 // Find a VM by ref.
@@ -113,18 +265,7 @@ func (r *Finder) VM(ref *base.Ref) (object interface{}, err error) {
 	return
 }
 
-// Resource finder.
-type Finder struct {
-	base.Client
-}
-
-// With client.
-func (r *Finder) With(client base.Client) base.Finder {
-	r.Client = client
-	return r
-}
-
-// Find workload by ref.
+// Find Workload by ref.
 // Returns the matching resource and:
 //
 //	ProviderNotSupportedErr
@@ -143,6 +284,13 @@ func (r *Finder) Workload(ref *base.Ref) (object interface{}, err error) {
 	return
 }
 
+// Find Network by ref.
+// Returns the matching resource and:
+//
+//	ProviderNotSupportedErr
+//	ProviderNotReadyErr
+//	NotFoundErr
+//	RefNotUniqueErr
 func (r *Finder) Network(ref *base.Ref) (object interface{}, err error) {
 	network := &Network{}
 	err = r.ByRef(network, *ref)
@@ -150,6 +298,25 @@ func (r *Finder) Network(ref *base.Ref) (object interface{}, err error) {
 		ref.ID = network.ID
 		ref.Name = network.Name
 		object = network
+	}
+
+	return
+}
+
+// Find a storage by ref.
+// Returns the matching resource and:
+//
+//	ProviderNotSupportedErr
+//	ProviderNotReadyErr
+//	NotFoundErr
+//	RefNotUniqueErr
+func (r *Finder) Storage(ref *base.Ref) (object interface{}, err error) {
+	volumeType := &VolumeType{}
+	err = r.ByRef(volumeType, *ref)
+	if err == nil {
+		ref.ID = volumeType.ID
+		ref.Name = volumeType.Name
+		object = volumeType
 	}
 
 	return
