c11713ff73a20055a50e306e4c7b0bd74767ce4d
Arik Hadas <ahadas@redhat.com>
2023-02-15 18:29:40 +0200

    Set oVirt populator image using envvar
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- cmd/populator-controller/populator-controller.go
+++ cmd/populator-controller/populator-controller.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"flag"
+	"os"
+
 	"github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1"
 	populator_machinery "github.com/kubev2v/lib-volume-populator/populator-machinery"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/klog/v2"
 )
 
 const (
@@ -23,7 +26,7 @@ const (
 )
 
 func main() {
-	var httpEndpoint, metricsPath, masterURL, kubeconfig, imageName, namespace string
+	var httpEndpoint, metricsPath, masterURL, kubeconfig, namespace string
 
 	// Controller args
 	if f := flag.Lookup("kubeconfig"); f != nil {
@@ -32,7 +35,6 @@ func main() {
 		flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
 	}
 	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
-	flag.StringVar(&imageName, "image-name", "", "Image to use for populating")
 	// Metrics args
 	flag.StringVar(&httpEndpoint, "http-endpoint", "", "The TCP network address where the HTTP server for diagnostics, including metrics and leader election health check, will listen (example: `:8080`). The default is empty string, which means the server is disabled.")
 	flag.StringVar(&metricsPath, "metrics-path", "/metrics", "The HTTP path where prometheus metrics will be exposed. Default is `/metrics`.")
@@ -40,12 +42,15 @@ func main() {
 	flag.StringVar(&namespace, "namespace", "konveyor-forklift", "Namespace to deploy controller")
 	flag.Parse()
 
-	var (
-		gk  = schema.GroupKind{Group: groupName, Kind: kind}
-		gvr = schema.GroupVersionResource{Group: groupName, Version: apiVersion, Resource: resource}
-	)
-	populator_machinery.RunController(masterURL, kubeconfig, imageName, httpEndpoint, metricsPath,
-		namespace, prefix, gk, gvr, mountPath, devicePath, getPopulatorPodArgs)
+	if ovirtImage, ok := os.LookupEnv("OVIRT_POPULATOR_IMAGE"); ok {
+		klog.Info("Tracking ovirt populators")
+		gk := schema.GroupKind{Group: groupName, Kind: kind}
+		gvr := schema.GroupVersionResource{Group: groupName, Version: apiVersion, Resource: resource}
+		populator_machinery.RunController(masterURL, kubeconfig, ovirtImage, httpEndpoint, metricsPath,
+			namespace, prefix, gk, gvr, mountPath, devicePath, getPopulatorPodArgs)
+	} else {
+		klog.Warning("Couldn't find OVIRT_POPULATOR_IMAGE, don't track ovirt populators")
+	}
 }
 
 func getPopulatorPodArgs(rawBlock bool, u *unstructured.Unstructured) ([]string, error) {
