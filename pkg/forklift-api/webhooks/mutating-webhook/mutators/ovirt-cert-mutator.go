74ef0dda2815b6690a877b3078cb1e6e74b343fc
Bella Khizgiyaev <bkhizgiy@redhat.com>
2023-02-19 17:27:12 +0200

    Adding a check that the certificate provided is valid
    Signed-off-by: Bella Khizgiyaev <bkhizgiy@redhat.com>

--- pkg/forklift-api/webhooks/mutating-webhook/mutators/ovirt-cert-mutator.go
+++ pkg/forklift-api/webhooks/mutating-webhook/mutators/ovirt-cert-mutator.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -11,6 +12,7 @@ import (
 	"strings"
 
 	"github.com/konveyor/forklift-controller/pkg/forklift-api/webhooks/util"
+	liberr "github.com/konveyor/forklift-controller/pkg/lib/error"
 	"github.com/konveyor/forklift-controller/pkg/lib/logging"
 	admissionv1 "k8s.io/api/admission/v1beta1"
 	core "k8s.io/api/core/v1"
@@ -28,8 +30,8 @@ func (mutator *OvirtCertMutator) Mutate(ar *admissionv1.AdmissionReview) *admiss
 	secret := &core.Secret{}
 	err := json.Unmarshal(raw, secret)
 	if err != nil {
-		log.Error(err, "mutating webhook error")
-		util.ToAdmissionResponseError(err)
+		log.Error(err, "mutating webhook error, failed to unmarshel certificate")
+		return util.ToAdmissionResponseError(err)
 	}
 
 	if providerType, ok := secret.GetLabels()["createdForProviderType"]; ok && providerType == "ovirt" {
@@ -37,23 +39,34 @@ func (mutator *OvirtCertMutator) Mutate(ar *admissionv1.AdmissionReview) *admiss
 		url, err := url.Parse(string(secret.Data["url"]))
 		if err != nil {
 			log.Error(err, "mutating webhook URL parsing error")
-			util.ToAdmissionResponseError(err)
+			return util.ToAdmissionResponseError(err)
 		}
 
 		certPool := x509.NewCertPool()
-		certPool.AppendCertsFromPEM(secret.Data["cacert"])
+		ok := certPool.AppendCertsFromPEM(secret.Data["cacert"])
+		if !ok {
+			err = liberr.Wrap(errors.New("failed to parse certificate"))
+			log.Error(err, "Certificate is not valid")
+			return &admissionv1.AdmissionResponse{
+				Allowed: false,
+				Result: &metav1.Status{
+					Message: "The certificate is not valid, can't proceed.",
+					Code:    http.StatusBadRequest,
+				},
+			}
+		}
 		certUrl := fmt.Sprint(url.Scheme, "://", url.Host, "/ovirt-engine/services/pki-resource?resource=ca-certificate&format=X509-PEM-CA")
 		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{RootCAs: certPool}
 		response, err := http.Get(certUrl)
 		if err != nil {
-			log.Error(err, "mutating webhook error")
-			util.ToAdmissionResponseError(err)
+			log.Error(err, "mutating webhook error, failed to send request for CA certificate retrieval")
+			return util.ToAdmissionResponseError(err)
 		}
 
 		b, err := io.ReadAll(response.Body)
 		if err != nil {
-			log.Error(err, "mutating webhook error")
-			util.ToAdmissionResponseError(err)
+			log.Error(err, "mutating webhook error, failed to read certificate retrieval response")
+			return util.ToAdmissionResponseError(err)
 		}
 
 		//check if the CA included in the secrete provided by the user and update it if needed
@@ -77,8 +90,8 @@ func (mutator *OvirtCertMutator) Mutate(ar *admissionv1.AdmissionReview) *admiss
 		)
 
 		if err != nil {
-			log.Error(err, "mutating webhook error")
-			util.ToAdmissionResponseError(err)
+			log.Error(err, "mutating webhook error, failed to generete paylod for patch request")
+			return util.ToAdmissionResponseError(err)
 		}
 
 		jsonPatchType := admissionv1.PatchTypeJSONPatch
