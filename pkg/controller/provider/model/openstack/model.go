99aeab96992d2cbd5ea430fe2e631eb4677ca8fe
Miguel Martín <mmartinv@redhat.com>
2023-03-16 13:17:49 +0100

    Retrieve Flavor ExtraSpecs
    Signed-off-by: Miguel Martín <mmartinv@redhat.com>

--- pkg/controller/provider/model/openstack/model.go
+++ pkg/controller/provider/model/openstack/model.go
@@ -84,14 +84,15 @@ type Image struct {
 
 type Flavor struct {
 	Base
-	Disk        int    `sql:""`
-	RAM         int    `sql:""`
-	RxTxFactor  string `sql:""`
-	Swap        int    `sql:""`
-	VCPUs       int    `sql:""`
-	IsPublic    bool   `sql:""`
-	Ephemeral   int    `sql:""`
-	Description string `sql:""`
+	Disk        int               `sql:""`
+	RAM         int               `sql:""`
+	RxTxFactor  string            `sql:""`
+	Swap        int               `sql:""`
+	VCPUs       int               `sql:""`
+	IsPublic    bool              `sql:""`
+	Ephemeral   int               `sql:""`
+	Description string            `sql:""`
+	ExtraSpecs  map[string]string `sql:""`
 }
 
 type AttachedVolume struct {
