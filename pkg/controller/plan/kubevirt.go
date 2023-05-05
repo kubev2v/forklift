2c8b4ed1efa93630cbffb877a5599ec8343742ed
Martin Nečas <necas.marty@gmail.com>
2023-05-05 14:39:32 +0200

    Revert "Use patch instead of update to set PVC owner references"
    This reverts commit fd57723450a805a81ed0fe5e4ea08b91a7ca7565.
    
    Signed-off-by: Martin Nečas <necas.marty@gmail.com>

--- pkg/controller/plan/kubevirt.go
+++ pkg/controller/plan/kubevirt.go
@@ -2,7 +2,6 @@ package plan
 
 import (
 	"context"
-	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"math/rand"
@@ -328,17 +327,8 @@ func (r *KubeVirt) EnsureVM(vm *plan.VMStatus) (err error) {
 
 	for _, pvc := range pvcs {
 		ownerRefs := []meta.OwnerReference{vmOwnerReference(virtualMachine)}
-		patch := map[string]interface{}{
-			"metadata": map[string]interface{}{
-				"ownerReferences": ownerRefs,
-			},
-		}
-		patchBytes, patchErr := json.Marshal(patch)
-		if patchErr != nil {
-			err = liberr.Wrap(patchErr)
-			return
-		}
-		err = r.Client.Patch(context.TODO(), &pvc, client.RawPatch(types.MergePatchType, patchBytes))
+		pvc.SetOwnerReferences(ownerRefs)
+		err = r.Destination.Client.Update(context.TODO(), &pvc)
 		if err != nil {
 			err = liberr.Wrap(err)
 			return
