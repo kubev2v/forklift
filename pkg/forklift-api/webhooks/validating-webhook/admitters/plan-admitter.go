48d2ea46a57a448f433590c55dbfa95ac36ff725
Tomáš Golembiovský <tgolembi@redhat.com>
2023-05-22 10:36:56 +0200

    webhooks: move storage validation to separate function
    Signed-off-by: Tomáš Golembiovský <tgolembi@redhat.com>

--- pkg/forklift-api/webhooks/validating-webhook/admitters/plan-admitter.go
+++ pkg/forklift-api/webhooks/validating-webhook/admitters/plan-admitter.go
@@ -7,11 +7,12 @@ import (
 
 	"encoding/json"
 	"fmt"
+	"net/http"
+
 	admissionv1 "k8s.io/api/admission/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
-	"net/http"
 
 	"github.com/konveyor/forklift-controller/pkg/apis"
 	api "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1"
@@ -19,79 +20,44 @@ import (
 )
 
 type PlanAdmitter struct {
+	client client.Client
+	plan api.Plan
+	sourceProvider api.Provider
+	destinationProvider api.Provider
 }
 
-func (admitter *PlanAdmitter) Admit(ar *admissionv1.AdmissionReview) *admissionv1.AdmissionResponse {
-	log.Info("Plan admitter was called")
-	raw := ar.Request.Object.Raw
+func (admitter *PlanAdmitter) validateStorage() *admissionv1.AdmissionResponse {
 
-	plan := &api.Plan{}
-	err := json.Unmarshal(raw, plan)
-	if err != nil {
-		return util.ToAdmissionResponseError(err)
-	}
-
-	if plan.Spec.Warm {
+	if admitter.plan.Spec.Warm {
 		log.Info("Warm migration supports all storages, passing")
 		return util.ToAdmissionResponseAllow()
 	}
 
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		log.Error(err, "Couldn't get the cluster configuration")
-		return util.ToAdmissionResponseError(err)
-	}
-
-	err = api.SchemeBuilder.AddToScheme(scheme.Scheme)
-	if err != nil {
-		log.Error(err, "Couldn't build the scheme")
-		return util.ToAdmissionResponseError(err)
-	}
-	err = apis.AddToScheme(scheme.Scheme)
-	if err != nil {
-		log.Error(err, "Couldn't add forklift API to the scheme")
-		return util.ToAdmissionResponseError(err)
-	}
-
-	cl, err := client.New(config, client.Options{Scheme: scheme.Scheme})
-	if err != nil {
-		log.Error(err, "Couldn't create a cluster client")
-		return util.ToAdmissionResponseError(err)
-	}
-
-	sourceProvider := api.Provider{}
-	err = cl.Get(context.TODO(), client.ObjectKey{Namespace: plan.Spec.Provider.Source.Namespace, Name: plan.Spec.Provider.Source.Name}, &sourceProvider)
-	if err != nil {
-		log.Error(err, "Couldn't get the source provider")
-		return util.ToAdmissionResponseError(err)
-	}
-
-	if sourceProvider.Type() == api.VSphere {
+	if admitter.sourceProvider.Type() == api.VSphere {
 		log.Info("Provider supports all storages, passing")
 		return util.ToAdmissionResponseAllow()
 	}
 
-	destinationProvider := api.Provider{}
-	err = cl.Get(context.TODO(), client.ObjectKey{Namespace: plan.Spec.Provider.Destination.Namespace, Name: plan.Spec.Provider.Destination.Name}, &destinationProvider)
-	if err != nil {
-		log.Error(err, "Couldn't get the destination provider")
-		return util.ToAdmissionResponseError(err)
-	}
-
-	if !destinationProvider.IsHost() {
+	if !admitter.destinationProvider.IsHost() {
 		log.Info("Migration to a remote provider supports all storages, passing")
 		return util.ToAdmissionResponseAllow()
 	}
 
 	storageClasses := v1.StorageClassList{}
-	err = cl.List(context.TODO(), &storageClasses, &client.ListOptions{})
+	err := admitter.client.List(context.TODO(), &storageClasses, &client.ListOptions{})
 	if err != nil {
 		log.Error(err, "Couldn't get the cluster storage classes")
 		return util.ToAdmissionResponseError(err)
 	}
 
 	storageMap := api.StorageMap{}
-	err = cl.Get(context.TODO(), client.ObjectKey{Namespace: plan.Spec.Map.Storage.Namespace, Name: plan.Spec.Map.Storage.Name}, &storageMap)
+	err = admitter.client.Get(
+		context.TODO(),
+		client.ObjectKey{
+			Namespace: admitter.plan.Spec.Map.Storage.Namespace,
+			Name: admitter.plan.Spec.Map.Storage.Name,
+		},
+		&storageMap)
 	if err != nil {
 		log.Error(err, "Couldn't get the storage map")
 		return util.ToAdmissionResponseError(err)
@@ -116,6 +82,72 @@ func (admitter *PlanAdmitter) Admit(ar *admissionv1.AdmissionReview) *admissionv
 			},
 		}
 	}
+
 	log.Info("Passed storage validation")
+	return util.ToAdmissionResponseAllow()
+}
+
+
+func (admitter *PlanAdmitter) Admit(ar *admissionv1.AdmissionReview) *admissionv1.AdmissionResponse {
+	log.Info("Plan admitter was called")
+	raw := ar.Request.Object.Raw
+
+	err := json.Unmarshal(raw, &admitter.plan)
+	if err != nil {
+		return util.ToAdmissionResponseError(err)
+	}
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Error(err, "Couldn't get the cluster configuration")
+		return util.ToAdmissionResponseError(err)
+	}
+
+	err = api.SchemeBuilder.AddToScheme(scheme.Scheme)
+	if err != nil {
+		log.Error(err, "Couldn't build the scheme")
+		return util.ToAdmissionResponseError(err)
+	}
+	err = apis.AddToScheme(scheme.Scheme)
+	if err != nil {
+		log.Error(err, "Couldn't add forklift API to the scheme")
+		return util.ToAdmissionResponseError(err)
+	}
+
+	admitter.client, err = client.New(config, client.Options{Scheme: scheme.Scheme})
+	if err != nil {
+		log.Error(err, "Couldn't create a cluster client")
+		return util.ToAdmissionResponseError(err)
+	}
+
+	err = admitter.client.Get(
+		context.TODO(),
+		client.ObjectKey{
+			Namespace: admitter.plan.Spec.Provider.Source.Namespace,
+			Name: admitter.plan.Spec.Provider.Source.Name,
+		},
+		&admitter.sourceProvider)
+	if err != nil {
+		log.Error(err, "Couldn't get the source provider")
+		return util.ToAdmissionResponseError(err)
+	}
+
+	err = admitter.client.Get(
+		context.TODO(),
+		client.ObjectKey{
+			Namespace: admitter.plan.Spec.Provider.Destination.Namespace,
+			Name: admitter.plan.Spec.Provider.Destination.Name,
+		},
+		&admitter.destinationProvider)
+	if err != nil {
+		log.Error(err, "Couldn't get the destination provider")
+		return util.ToAdmissionResponseError(err)
+	}
+
+	response := admitter.validateStorage()
+	if !response.Allowed {
+		return response
+	}
+
 	return util.ToAdmissionResponseAllow()
 }
