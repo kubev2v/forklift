5e257bfab90fdce2dd0a697b037c0d8ec3f30d03
Arik Hadas <ahadas@redhat.com>
2023-07-26 19:06:54 +0300

    Make vSphere's Client#Finalize non-empty
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- pkg/controller/plan/adapter/vsphere/client.go
+++ pkg/controller/plan/adapter/vsphere/client.go
@@ -227,7 +227,7 @@ func (r *Client) Close() {
 }
 
 func (c *Client) Finalize(vms []*planapi.VMStatus, planName string) {
-
+	return
 }
 
 func (r *Client) PreTransferActions(vmRef ref.Ref) (ready bool, err error) {
