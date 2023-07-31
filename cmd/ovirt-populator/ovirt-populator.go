11916fa6fa22d0f04826ab8e269636353639fd63
Benny Zlotnik <bzlotnik@redhat.com>
2023-07-31 12:25:43 +0300

    create layers
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- cmd/ovirt-populator/ovirt-populator.go
+++ cmd/ovirt-populator/ovirt-populator.go
@@ -117,7 +117,7 @@ func populate(engineURL, diskID, volPath string) {
 			volPath,
 		}
 	}
-	cmd := exec.Command("ovirt-img", args...)
+	cmd := exec.Command("/usr/local/bin/ovirt-img", args...)
 	r, _ := cmd.StdoutPipe()
 	cmd.Stderr = cmd.Stdout
 	done := make(chan struct{})
