3f2b05402c427922052d29df40564227a9541491
Benny Zlotnik <bzlotnik@redhat.com>
2023-08-01 17:19:55 +0300

    Cleanup bazelified ovirt populator
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- hack/ovirt-populator-rpm-deps.sh
+++ hack/ovirt-populator-rpm-deps.sh
@@ -7,13 +7,19 @@ if [ "${CUSTOM_REPO}" ]; then
     bazeldnf_repos="--repofile ${CUSTOM_REPO} ${bazeldnf_repos}"
 fi
 
-# get latest repo data from repo.yaml
 bazel run \
     //:bazeldnf -- fetch \
     ${bazeldnf_repos}
 
-# These are the packages we really depend on.
-imageio="gcc python3-pip python3-devel libxml2-devel openssl-devel libcurl-devel qemu-img"
+imageio_deps=(
+    "gcc"
+    "python3-pip"
+    "python3-devel"
+    "libxml2-devel"
+    "openssl-devel"
+    "libcurl-devel"
+    "qemu-img"
+)
 
 bazel run \
         //:bazeldnf -- rpmtree \
@@ -23,5 +29,5 @@ bazel run \
         --name deps \
         --basesystem centos-stream-release \
         ${bazeldnf_repos} \
-        $imageio
+        $imageio_deps
 
