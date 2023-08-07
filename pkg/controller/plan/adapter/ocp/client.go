d8b762dee2608be65b8bcf5d5803779772a1fa2d
Benny Zlotnik <bzlotnik@redhat.com>
2023-08-07 15:28:47 +0300

    ocp: make export token TTL configurable
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- pkg/controller/plan/adapter/ocp/client.go
+++ pkg/controller/plan/adapter/ocp/client.go
@@ -2,19 +2,19 @@ package ocp
 
 import (
 	"context"
+	"time"
 
 	planapi "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1/plan"
 	"github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1/ref"
 	plancontext "github.com/konveyor/forklift-controller/pkg/controller/plan/context"
 	liberr "github.com/konveyor/forklift-controller/pkg/lib/error"
+	"github.com/konveyor/forklift-controller/pkg/settings"
 	core "k8s.io/api/core/v1"
 	k8serr "k8s.io/apimachinery/pkg/api/errors"
-	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	cnv "kubevirt.io/api/core/v1"
 	export "kubevirt.io/api/export/v1alpha1"
 	cdi "kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -41,7 +41,7 @@ func (r *Client) CreateSnapshot(vmRef ref.Ref) (string, error) {
 // Finalize implements base.Client
 func (r *Client) Finalize(vms []*planapi.VMStatus, planName string) {
 	for _, vm := range vms {
-		vmExport := &export.VirtualMachineExport{ObjectMeta: v1.ObjectMeta{
+		vmExport := &export.VirtualMachineExport{ObjectMeta: metav1.ObjectMeta{
 			Name:      vm.Name,
 			Namespace: vm.Namespace,
 		}}
@@ -57,7 +57,7 @@ func (r *Client) Finalize(vms []*planapi.VMStatus, planName string) {
 // PowerOff implements base.Client
 func (r *Client) PowerOff(vmRef ref.Ref) error {
 	vm := cnv.VirtualMachine{}
-	err := r.sourceClient.Get(context.TODO(), client.ObjectKey{Namespace: vmRef.Namespace, Name: vmRef.Name}, &vm)
+	err := r.sourceClient.Get(context.TODO(), k8sclient.ObjectKey{Namespace: vmRef.Namespace, Name: vmRef.Name}, &vm)
 	if err != nil {
 		return err
 	}
@@ -75,7 +75,7 @@ func (r *Client) PowerOff(vmRef ref.Ref) error {
 // PowerOn implements base.Client
 func (r *Client) PowerOn(vmRef ref.Ref) error {
 	vm := cnv.VirtualMachine{}
-	err := r.Destination.Client.Get(context.TODO(), client.ObjectKey{Namespace: vmRef.Namespace, Name: vmRef.Name}, &vm)
+	err := r.Destination.Client.Get(context.TODO(), k8sclient.ObjectKey{Namespace: vmRef.Namespace, Name: vmRef.Name}, &vm)
 	if err != nil {
 		return err
 	}
@@ -93,7 +93,7 @@ func (r *Client) PowerOn(vmRef ref.Ref) error {
 // PowerState implements base.Client
 func (r *Client) PowerState(vmRef ref.Ref) (string, error) {
 	vm := cnv.VirtualMachine{}
-	err := r.sourceClient.Get(context.TODO(), client.ObjectKey{Namespace: vmRef.Namespace, Name: vmRef.Name}, &vm)
+	err := r.sourceClient.Get(context.TODO(), k8sclient.ObjectKey{Namespace: vmRef.Namespace, Name: vmRef.Name}, &vm)
 	if err != nil {
 		err = liberr.Wrap(err)
 		return "", err
@@ -109,7 +109,7 @@ func (r *Client) PowerState(vmRef ref.Ref) (string, error) {
 // PoweredOff implements base.Client
 func (r *Client) PoweredOff(vmRef ref.Ref) (bool, error) {
 	vm := cnv.VirtualMachine{}
-	err := r.sourceClient.Get(context.TODO(), client.ObjectKey{Namespace: vmRef.Namespace, Name: vmRef.Name}, &vm)
+	err := r.sourceClient.Get(context.TODO(), k8sclient.ObjectKey{Namespace: vmRef.Namespace, Name: vmRef.Name}, &vm)
 	if err != nil {
 		err = liberr.Wrap(err)
 		return false, err
@@ -143,23 +143,31 @@ func (r *Client) PreTransferActions(vmRef ref.Ref) (ready bool, err error) {
 
 	// Check if VM export exists
 	vmExport := &export.VirtualMachineExport{}
-	err = r.sourceClient.Get(context.Background(), client.ObjectKey{Namespace: vmRef.Namespace, Name: vmRef.Name}, vmExport)
+	err = r.sourceClient.Get(context.Background(), k8sclient.ObjectKey{Namespace: vmRef.Namespace, Name: vmRef.Name}, vmExport)
+
 	if err != nil {
 		if !k8serr.IsNotFound(err) {
 			r.Log.Error(err, "Failed to get VM-export.", "vm", vmRef.Name)
 			return true, liberr.Wrap(err)
 		}
+
+		var tokenTTLDuration *metav1.Duration
+		if settings.Settings.CDIExportTokenTTL > 0 {
+			tokenTTLDuration = &metav1.Duration{Duration: time.Duration(settings.Settings.CDIExportTokenTTL) * time.Minute}
+		}
+
 		// Create VM export
 		vmExport = &export.VirtualMachineExport{
-			TypeMeta: meta.TypeMeta{
+			TypeMeta: metav1.TypeMeta{
 				Kind:       "VirtualMachineExport",
 				APIVersion: "kubevirt.io/v1alpha3",
 			},
-			ObjectMeta: meta.ObjectMeta{
+			ObjectMeta: metav1.ObjectMeta{
 				Name:      vmRef.Name,
 				Namespace: vmRef.Namespace,
 			},
 			Spec: export.VirtualMachineExportSpec{
+				TTLDuration: tokenTTLDuration,
 				Source: core.TypedLocalObjectReference{
 					APIGroup: &apiGroup,
 					Kind:     "VirtualMachine",
@@ -168,7 +176,7 @@ func (r *Client) PreTransferActions(vmRef ref.Ref) (ready bool, err error) {
 			},
 		}
 
-		err = r.sourceClient.Create(context.Background(), vmExport, &client.CreateOptions{})
+		err = r.sourceClient.Create(context.Background(), vmExport, &k8sclient.CreateOptions{})
 		if err != nil {
 			return true, liberr.Wrap(err)
 		}
