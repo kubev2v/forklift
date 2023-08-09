c1623d063b43b6caf4ab42ae5e0cc7c261fe6d11
Arik Hadas <ahadas@redhat.com>
2023-08-09 16:08:10 +0300

    fixing tests for capacity in an ova
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- pkg/controller/plan/adapter/ova/ova_capacity_test.go
+++ pkg/controller/plan/adapter/ova/ova_capacity_test.go
@@ -21,14 +21,14 @@ func TestGetResourceCapacity(t *testing.T) {
 		{"byte units", 20, "byte", 20, false},
 		{"pow 10", 2, "byte * 2^10", 2048, false},
 		{"pow 20", 1, "byte * 2^20", 1048576, false},
-		{"missing pow", 1, "byte * 2", 0, true},
+		{"missing pow", 2, "byte * 1024", 2048, false},
 		{"malformed units", 100, "byte*2^", 0, true},
 		{"no spaces", 1, "byte*2^20", 1048576, false},
 		{"unsupported units", 10, "kilobytes", 0, true},
 		{"unsupported operand", 1, "byte + 2^20", 0, true},
 		{"diffrent base", 1, "byte * 3^10", 59049, false},
 		{"uncommon format", 1, "byte * 2 * 2^10", 2048, false},
-		{"uncommon format no spaces", 1, "byte*3*2^10", 2048, false},
+		{"uncommon format no spaces", 1, "byte*3*2^10", 3072, false},
 	}
 
 	for _, testCase := range tests {
