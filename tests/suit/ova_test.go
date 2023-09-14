9b517c4490815438ebd6af1a94b1f7610f24fb51
Bella Khizgiyaev <bkhizgiy@redhat.com>
2023-09-15 00:41:34 +0300

    Add e2e test for ova provider
    Signed-off-by: Bella Khizgiyaev <bkhizgiy@redhat.com>

---
+++ tests/suit/ova_test.go
@@ -0,0 +1,84 @@
+package suit_test
+
+import (
+	"time"
+
+	forkliftv1 "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1"
+	"github.com/konveyor/forklift-controller/tests/suit/framework"
+	"github.com/konveyor/forklift-controller/tests/suit/utils"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+const (
+	ovaProviderName = "ova-provider"
+	ovaStorageClass = "nfs-csi"
+)
+
+var _ = Describe("[level:component]Migration tests for OVA provider", func() {
+	f := framework.NewFramework("migration-func-test")
+
+	It("[test] should create provider with NetworkMap", func() {
+		namespace := f.Namespace.Name
+
+		By("Load Source VM Details from OVA")
+		vmData, err := f.Clients.OvaClient.LoadSourceDetails()
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Get NFS share for OVA provider")
+		nfs, err := f.Clients.OvaClient.GetNfsServerForOva(f.K8sClient)
+		Expect(err).ToNot(HaveOccurred())
+		By("Create Secret from Definition")
+		secret, err := utils.CreateSecretFromDefinition(f.K8sClient, utils.NewSecretDefinition(
+			map[string]string{
+				"createdForProviderType": "ova",
+			}, nil,
+			map[string][]byte{
+				"url": []byte(nfs),
+			}, namespace, "provider-test-secret"))
+		Expect(err).ToNot(HaveOccurred())
+
+		targetNS, err := f.CreateNamespace("ova-migration-test", map[string]string{})
+		Expect(err).ToNot(HaveOccurred())
+		By("Create target Openshift provider")
+		targetPr := utils.NewProvider(utils.TargetProviderName, forkliftv1.OpenShift, namespace, map[string]string{}, map[string]string{}, "", nil)
+		err = utils.CreateProviderFromDefinition(f.CrClient, targetPr)
+		Expect(err).ToNot(HaveOccurred())
+		_, err = utils.WaitForProviderReadyWithTimeout(f.CrClient, namespace, utils.TargetProviderName, 30*time.Second)
+		Expect(err).ToNot(HaveOccurred())
+		By("Create OVA provider")
+		pr := utils.NewProvider(ovaProviderName, forkliftv1.Ova, namespace, map[string]string{}, map[string]string{}, nfs, secret)
+		err = utils.CreateProviderFromDefinition(f.CrClient, pr)
+		Expect(err).ToNot(HaveOccurred())
+		provider, err := utils.WaitForProviderReadyWithTimeout(f.CrClient, namespace, ovaProviderName, 5*time.Minute)
+		Expect(err).ToNot(HaveOccurred())
+		By("Create Network Map")
+		networkMapDef := utils.NewNetworkMap(namespace, *provider, networkMapName, vmData.GetNetworkId())
+		err = utils.CreateNetworkMapFromDefinition(f.CrClient, networkMapDef)
+		Expect(err).ToNot(HaveOccurred())
+		err = utils.WaitForNetworkMapReadyWithTimeout(f.CrClient, namespace, networkMapName, 30*time.Second)
+		Expect(err).ToNot(HaveOccurred())
+		By("Create Storage Map")
+		storageMapDef := utils.NewStorageMap(namespace, *provider, test_storage_map_name, []string{vmData.GetStorageName()}, ovaStorageClass)
+		err = utils.CreateStorageMapFromDefinition(f.CrClient, storageMapDef)
+		Expect(err).ToNot(HaveOccurred())
+		err = utils.WaitForStorageMapReadyWithTimeout(f.CrClient, namespace, test_storage_map_name, 10*time.Second)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Creating plan")
+		planDef := utils.NewPlanWithVmId(*provider, namespace, test_plan_name, test_storage_map_name, networkMapName, targetNS.Name, []string{vmData.GetVmId()})
+
+		err = utils.CreatePlanFromDefinition(f.CrClient, planDef)
+		Expect(err).ToNot(HaveOccurred())
+		err, _ = utils.WaitForPlanReadyWithTimeout(f.CrClient, namespace, test_plan_name, 15*time.Second)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Creating migration")
+		migrationDef := utils.NewMigration(provider.Namespace, test_migration_name, test_plan_name)
+		err = utils.CreateMigrationFromDefinition(f.CrClient, migrationDef)
+		Expect(err).ToNot(HaveOccurred())
+		err = utils.WaitForMigrationSucceededWithTimeout(f.CrClient, provider.Namespace, test_migration_name, 900*time.Second)
+		Expect(err).ToNot(HaveOccurred())
+
+	})
+})
--- tests/suit/utils/storagemap.go
+++ tests/suit/utils/storagemap.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	api "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1"
 	forkliftv1 "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1"
 	"github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1/provider"
 	"github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1/ref"
@@ -31,11 +32,17 @@ func NewStorageMap(namespace string, providerIdentifier forkliftv1.Provider, sto
 
 	for _, sd := range storageIDs {
 		pair := forkliftv1.StoragePair{
-			Source: ref.Ref{ID: sd},
 			Destination: forkliftv1.DestinationStorage{
 				StorageClass: storageClass,
 			},
 		}
+
+		if providerIdentifier.Type() != api.Ova {
+			pair.Source = ref.Ref{ID: sd}
+		} else {
+			pair.Source = ref.Ref{Name: sd}
+		}
+
 		sdPairs = append(sdPairs, pair)
 	}
 
