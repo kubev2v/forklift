2de6fba7227322cf0fdff8f9b95ae2fc26a6df13
Benny Zlotnik <bzlotnik@redhat.com>
2023-09-18 12:38:17 +0300

    ocp: extract to function and add unit test
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

---
+++ pkg/controller/plan/adapter/ocp/validator_test.go
@@ -0,0 +1,41 @@
+package ocp
+
+import (
+	"testing"
+
+	"github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1/ref"
+)
+
+func TestGetNetworkNameAndNamespace(t *testing.T) {
+	tests := []struct {
+		name         string
+		networkName  string
+		vmRef        *ref.Ref
+		expectedName string
+		expectedNS   string
+	}{
+		{
+			name:         "no slash in network name",
+			networkName:  "network",
+			vmRef:        &ref.Ref{Namespace: "vmNamespace"},
+			expectedName: "network",
+			expectedNS:   "vmNamespace",
+		},
+		{
+			name:         "slash in network name",
+			networkName:  "namespace/network",
+			vmRef:        &ref.Ref{Namespace: "vmNamespace"},
+			expectedName: "network",
+			expectedNS:   "namespace",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actualName, actualNS := getNetworkNameAndNamespace(tt.networkName, &ref.Ref{Namespace: tt.vmRef.Namespace})
+			if actualName != tt.expectedName || actualNS != tt.expectedNS {
+				t.Errorf("got (%s, %s), want (%s, %s)", actualName, actualNS, tt.expectedName, tt.expectedNS)
+			}
+		})
+	}
+}
