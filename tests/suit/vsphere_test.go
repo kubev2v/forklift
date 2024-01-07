2d4c331514835c263b3ec871da9a940d72d34dae
Arik Hadas <ahadas@redhat.com>
2024-01-07 14:17:54 +0200

    Enhance vSphere end-to-end tests
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- tests/suit/vsphere_test.go
+++ tests/suit/vsphere_test.go
@@ -1,6 +1,7 @@
 package suit_test
 
 import (
+	"context"
 	"time"
 
 	forkliftv1 "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1"
@@ -8,6 +9,7 @@ import (
 	"github.com/konveyor/forklift-controller/tests/suit/utils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -15,10 +17,10 @@ const (
 	vsphereStorageClass = "nfs-csi"
 )
 
-var _ = Describe("[level:component]Migration tests for vSphere provider", func() {
+var _ = Describe("vSphere provider", func() {
 	f := framework.NewFramework("migration-func-test")
 
-	It("[test] should create provider with NetworkMap", func() {
+	It("Migrate VM", func() {
 		namespace := f.Namespace.Name
 		By("Create Secret from Definition")
 		s, err := utils.CreateSecretFromDefinition(f.K8sClient, utils.NewSecretDefinition(nil, nil,
@@ -71,5 +73,91 @@ var _ = Describe("[level:component]Migration tests for vSphere provider", func()
 		err = utils.WaitForMigrationSucceededWithTimeout(f.CrClient, provider.Namespace, test_migration_name, 300*time.Second)
 		Expect(err).ToNot(HaveOccurred())
 
+	})
+
+	Context("vCenter SDK", func() {
+		var provider *forkliftv1.Provider
+		var namespace string
+
+		BeforeEach(func() {
+			namespace = f.Namespace.Name
+			By("Create Secret from Definition")
+			s, err := utils.CreateSecretFromDefinition(f.K8sClient, utils.NewSecretDefinition(nil, nil,
+				map[string][]byte{
+					"thumbprint": []byte("52:6C:4E:88:1D:78:AE:12:1C:F3:BB:6C:5B:F4:E2:82:86:A7:08:AF"),
+					"password":   []byte("MTIzNDU2Cg=="),
+					"user":       []byte("YWRtaW5pc3RyYXRvckB2c3BoZXJlLmxvY2Fs"),
+				}, namespace, "vcenter-provider-secret"))
+			Expect(err).ToNot(HaveOccurred())
+			By("Create vCenter provider")
+			pr := utils.NewProvider(vsphereProviderName, forkliftv1.VSphere, namespace, map[string]string{}, map[string]string{forkliftv1.VDDK: "quay.io/kubev2v/vddk-test-vmdk"}, "https://vcsim.konveyor-forklift:8989/sdk", s)
+			err = utils.CreateProviderFromDefinition(f.CrClient, pr)
+			Expect(err).ToNot(HaveOccurred())
+			provider, err = utils.WaitForProviderReadyWithTimeout(f.CrClient, namespace, vsphereProviderName, 30*time.Second)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("Provider should be set with sdkEndpoint=vcenter by default", func() {
+			Expect(provider.Spec.Settings[forkliftv1.SDK]).To(Equal(forkliftv1.VCenter))
+		})
+		It("Host cannot be defined with empty credentials", func() {
+			labels := map[string]string{
+				"createdForResourceType": "hosts",
+				"createdForResource":     "host-21",
+			}
+			_, err := utils.CreateSecretFromDefinition(f.K8sClient, utils.NewSecretDefinition(labels, nil,
+				map[string][]byte{
+					"ip":       []byte("52:6C:4E:88:1D:78:AE:12:1C:F3:BB:6C:5B:F4:E2:82:86:A7:08:AF"),
+					"provider": []byte(vsphereProviderName),
+				}, namespace, "esxi-host-secret"))
+			Expect(err).To(HaveOccurred())
+		})
+	})
+
+	Context("ESXi SDK", func() {
+		var provider *forkliftv1.Provider
+		var namespace string
+
+		BeforeEach(func() {
+			namespace = f.Namespace.Name
+			By("Create Secret from Definition")
+			s, err := utils.CreateSecretFromDefinition(f.K8sClient, utils.NewSecretDefinition(nil, nil,
+				map[string][]byte{
+					"thumbprint": []byte("52:6C:4E:88:1D:78:AE:12:1C:F3:BB:6C:5B:F4:E2:82:86:A7:08:AF"),
+					"password":   []byte("MTIzNDU2Cg=="),
+					"user":       []byte("YWRtaW5pc3RyYXRvckB2c3BoZXJlLmxvY2Fs"),
+				}, namespace, "esxi-provider-secret"))
+			Expect(err).ToNot(HaveOccurred())
+			By("Create ESXi provider")
+			settings := map[string]string{
+				forkliftv1.VDDK: "quay.io/kubev2v/vddk-test-vmdk",
+				forkliftv1.SDK:  forkliftv1.ESXI,
+			}
+			pr := utils.NewProvider(vsphereProviderName, forkliftv1.VSphere, namespace, map[string]string{}, settings, "https://vcsim.konveyor-forklift:8989/sdk", s)
+			err = utils.CreateProviderFromDefinition(f.CrClient, pr)
+			Expect(err).ToNot(HaveOccurred())
+			provider, err = utils.WaitForProviderReadyWithTimeout(f.CrClient, namespace, vsphereProviderName, 30*time.Second)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("Provider should be set with sdkEndpoint=esxi", func() {
+			Expect(provider.Spec.Settings[forkliftv1.SDK]).To(Equal(forkliftv1.ESXI))
+		})
+		It("Credentials are copied from the Provider to the Host", func() {
+			labels := map[string]string{
+				"createdForResourceType": "hosts",
+				"createdForResource":     "host-21",
+			}
+			secret := utils.NewSecretDefinition(labels, nil,
+				map[string][]byte{
+					"ip":       []byte("52:6C:4E:88:1D:78:AE:12:1C:F3:BB:6C:5B:F4:E2:82:86:A7:08:AF"),
+					"provider": []byte(vsphereProviderName),
+				}, namespace, "esxi-host-secret",
+			)
+			secret, err := f.K8sClient.CoreV1().Secrets(secret.Namespace).Create(context.TODO(), secret, metav1.CreateOptions{})
+			Expect(err).ToNot(HaveOccurred())
+			_, found := secret.Data["user"]
+			Expect(found).To(BeTrue())
+		})
 	})
 })
