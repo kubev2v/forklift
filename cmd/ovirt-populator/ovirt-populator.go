93c3d66034c7ad4248da59eb9495ce9b2cc591d8
Benny Zlotnik <bzlotnik@redhat.com>
2024-01-24 15:59:47 +0200

    ovirt: refactor ovirt-populator
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- cmd/ovirt-populator/ovirt-populator.go
+++ cmd/ovirt-populator/ovirt-populator.go
@@ -34,29 +34,77 @@ type TransferProgress struct {
 }
 
 func main() {
-	var engineUrl, secretName, diskID, volPath, crName, crNamespace, namespace, ownerUID string
+	var engineUrl, diskID, volPath, secretName, crName, crNamespace, ownerUID string
 	var pvcSize *int64
 
-	// Populate args
-	flag.StringVar(&engineUrl, "engine-url", "", "ovirt-engine url (https//engine.fqdn)")
-	flag.StringVar(&secretName, "secret-name", "", "secret containing oVirt credentials")
+	flag.StringVar(&engineUrl, "engine-url", "", "ovirt-engine url (https://engine.fqdn)")
 	flag.StringVar(&diskID, "disk-id", "", "ovirt-engine disk id")
 	flag.StringVar(&volPath, "volume-path", "", "Volume path to populate")
-	flag.StringVar(&crNamespace, "cr-namespace", "", "Custom Resource instance namespace")
+	flag.StringVar(&secretName, "secret-name", "", "Name of secret containing ovirt credentials")
 	flag.StringVar(&crName, "cr-name", "", "Custom Resource instance name")
+	flag.StringVar(&crNamespace, "cr-namespace", "", "Custom Resource instance namespace")
 	flag.StringVar(&ownerUID, "owner-uid", "", "Owner UID (usually PVC UID)")
 	pvcSize = flag.Int64("pvc-size", 0, "Size of pvc (in bytes)")
 
-	// Other args
-	flag.StringVar(&namespace, "namespace", "konveyor-forklift", "Namespace to deploy controller")
 	flag.Parse()
 
 	populate(engineUrl, diskID, volPath, ownerUID, *pvcSize)
 }
 
 func populate(engineURL, diskID, volPath, ownerUID string, pvcSize int64) {
+	setupPrometheusMetrics()
+	config := loadEngineConfig(engineURL)
+	prepareCredentials(config)
+	executePopulationProcess(config, diskID, volPath, ownerUID, pvcSize)
+}
+
+func setupPrometheusMetrics() {
 	http.Handle("/metrics", promhttp.Handler())
 	go http.ListenAndServe(":2112", nil)
+	// ... [rest of the Prometheus setup code] ...
+}
+
+func prepareCredentials(config *engineConfig) {
+	writeFile("/tmp/ovirt.pass", config.password, "ovirt.pass")
+	if !config.insecure {
+		writeFile("/tmp/ca.pem", config.cacert, "ca.pem")
+	}
+}
+
+func writeFile(filename, content, logName string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		klog.Fatalf("Failed to create %s: %v", logName, err)
+	}
+	defer file.Close()
+
+	if _, err := file.Write([]byte(content)); err != nil {
+		klog.Fatalf("Failed to write to %s: %v", logName, err)
+	}
+}
+
+func executePopulationProcess(config *engineConfig, diskID, volPath, ownerUID string, pvcSize int64) {
+	args := createCommandArguments(config, diskID, volPath)
+	cmd := exec.Command("ovirt-img", args...)
+	r, _ := cmd.StdoutPipe()
+	cmd.Stderr = cmd.Stdout
+	done := make(chan struct{})
+	scanner := bufio.NewScanner(r)
+	klog.Info(fmt.Sprintf("Running command: %s", cmd.String()))
+
+	go monitorProgress(scanner, ownerUID, pvcSize, done)
+
+	if err := cmd.Start(); err != nil {
+		klog.Fatal(err)
+	}
+
+	<-done
+	if err := cmd.Wait(); err != nil {
+		klog.Fatal(err)
+	}
+}
+
+func monitorProgress(scanner *bufio.Scanner, ownerUID string, pvcSize int64, done chan struct{}) {
 	progressCounter := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "ovirt_progress",
@@ -66,139 +114,77 @@ func populate(engineURL, diskID, volPath, ownerUID string, pvcSize int64) {
 	)
 	if err := prometheus.Register(progressCounter); err != nil {
 		klog.Error("Prometheus progress gauge not registered:", err)
+		return
 	} else {
 		klog.Info("Prometheus progress gauge registered.")
 	}
 
-	engineConfig := loadEngineConfig(engineURL)
+	var currentProgress float64
+	total := pvcSize
+	metric := &dto.Metric{}
 
-	// Write credentials to files
-	ovirtPass, err := os.Create("/tmp/ovirt.pass")
-	if err != nil {
-		klog.Fatalf("Failed to create ovirt.pass %v", err)
-	}
-
-	defer ovirtPass.Close()
-	_, err = ovirtPass.Write([]byte(engineConfig.password))
-	if err != nil {
-		klog.Fatalf("Failed to write password to file: %v", err)
-	}
-
-	args := createCommandArguments(&engineConfig, diskID, volPath)
-	cmd := exec.Command("ovirt-img", args...)
-	r, _ := cmd.StdoutPipe()
-	cmd.Stderr = cmd.Stdout
-	done := make(chan struct{})
-	scanner := bufio.NewScanner(r)
-	klog.Info(fmt.Sprintf("Running command: %s", cmd.String()))
-
-	go func() {
-		var currentProgress float64
-		total := pvcSize
-		metric := &dto.Metric{}
-
-		for scanner.Scan() {
-			progressOutput := TransferProgress{}
-			text := scanner.Text()
-			klog.Info(text)
-			err = json.Unmarshal([]byte(text), &progressOutput)
-			if err != nil {
-				var syntaxError *json.SyntaxError
-				if !errors.As(err, &syntaxError) {
-					klog.Error(err)
-				}
-			}
-			if total > 0 {
-				currentProgress = (float64(progressOutput.Transferred) / float64(total)) * 100
-				err = progressCounter.WithLabelValues(ownerUID).Write(metric)
-				if err != nil {
-					klog.Error(err)
-				} else if currentProgress > metric.Counter.GetValue() {
-					progressCounter.WithLabelValues(ownerUID).Add(currentProgress - metric.Counter.GetValue())
-				}
+	for scanner.Scan() {
+		progressOutput := TransferProgress{}
+		text := scanner.Text()
+		klog.Info(text)
+		if err := json.Unmarshal([]byte(text), &progressOutput); err != nil {
+			var syntaxError *json.SyntaxError
+			if !errors.As(err, &syntaxError) {
+				klog.Error(err)
 			}
 		}
-
-		metric.GetCounter().Value = ptr.To[float64](100)
-		err = progressCounter.WithLabelValues(ownerUID).Write(metric)
-
-		done <- struct{}{}
-	}()
-
-	err = cmd.Start()
-	if err != nil {
-		klog.Fatal(err)
+		if total > 0 {
+			currentProgress = (float64(progressOutput.Transferred) / float64(total)) * 100
+			if err := progressCounter.WithLabelValues(ownerUID).Write(metric); err != nil {
+				klog.Error(err)
+			} else if currentProgress > metric.Counter.GetValue() {
+				progressCounter.WithLabelValues(ownerUID).Add(currentProgress - metric.Counter.GetValue())
+			}
+		}
 	}
 
-	<-done
-	err = cmd.Wait()
-	if err != nil {
-		klog.Fatal(err)
+	metric.GetCounter().Value = ptr.To[float64](100)
+	if err := progressCounter.WithLabelValues(ownerUID).Write(metric); err != nil {
+		klog.Error(err)
 	}
+
+	done <- struct{}{}
 }
 
 func createCommandArguments(config *engineConfig, diskID, volPath string) []string {
+	var args []string
+	args = append(args, "download-disk", "--output", "json", "--engine-url="+config.URL, "--username="+config.username, "--password-file=/tmp/ovirt.pass")
+
 	if config.insecure {
-		return []string{
-			"download-disk",
-			"--output", "json",
-			"--engine-url=" + config.URL,
-			"--username=" + config.username,
-			"--password-file=/tmp/ovirt.pass",
-			"--insecure",
-			"-f", "raw",
-			diskID,
-			volPath,
-		}
+		args = append(args, "--insecure")
 	} else {
-		// for secure connection use the ca cert
-		cert, err := os.Create("/tmp/ca.pem")
-		if err != nil {
-			klog.Fatalf("Failed to create ca.pem %v", err)
-		}
-
-		defer cert.Close()
-		_, err = cert.Write([]byte(config.cacert))
-		if err != nil {
-			klog.Fatalf("Failed to write CA to file: %v", err)
-		}
-
-		return []string{
-			"download-disk",
-			"--output", "json",
-			"--engine-url=" + config.URL,
-			"--username=" + config.username,
-			"--password-file=/tmp/ovirt.pass",
-			"--cafile=" + "/tmp/ca.pem",
-			"-f", "raw",
-			diskID,
-			volPath,
-		}
+		args = append(args, "--cafile=/tmp/ca.pem")
 	}
+
+	args = append(args, "-f", "raw", diskID, volPath)
+	return args
 }
 
-func loadEngineConfig(engineURL string) engineConfig {
-	user := os.Getenv("user")
-	pass := os.Getenv("password")
-
-	insecureSkipVerify, found := os.LookupEnv("insecureSkipVerify")
-	if !found {
-		insecureSkipVerify = "false"
-	}
-
-	insecure, err := strconv.ParseBool(string(insecureSkipVerify))
-	if err != nil {
-		klog.Fatal(err.Error())
-	}
-
-	//If the insecure option is set, the ca file field in the secret is not required.
-	cacert := os.Getenv("cacert")
-
-	return engineConfig{
+func loadEngineConfig(engineURL string) *engineConfig {
+	user, pass := os.Getenv("user"), os.Getenv("password")
+	insecure := getEnvAsBool("insecureSkipVerify", false)
+	return &engineConfig{
 		URL:      engineURL,
 		username: user,
 		password: pass,
-		cacert:   cacert,
+		cacert:   os.Getenv("cacert"),
 		insecure: insecure,
 	}
+}
+
+func getEnvAsBool(key string, defaultVal bool) bool {
+	val, found := os.LookupEnv(key)
+	if !found {
+		return defaultVal
+	}
+	boolVal, err := strconv.ParseBool(val)
+	if err != nil {
+		klog.Fatal("Invalid boolean value for", key, ":", val)
+	}
+	return boolVal
 }
