22c434b73d4731777b86811558478603335eedda
Benny Zlotnik <bzlotnik@redhat.com>
2024-02-13 18:12:41 +0200

    add tests
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

---
+++ pkg/controller/plan/validation_test.go
@@ -0,0 +1,48 @@
+package plan
+
+import (
+	"github.com/konveyor/forklift-controller/pkg/controller/base"
+	ginkgo "github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/version"
+	discovery "k8s.io/client-go/discovery/fake"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+var _ = ginkgo.Describe("Plan Validations", func() {
+	var (
+		fakeClientSet *fake.Clientset
+		reconciler    *Reconciler
+	)
+
+	ginkgo.BeforeEach(func() {
+		reconciler = &Reconciler{
+			base.Reconciler{},
+		}
+		fakeClientSet = fake.NewSimpleClientset()
+	})
+
+	ginkgo.Describe("validateOCPVersion", func() {
+		ginkgo.DescribeTable("should validate OpenShift version correctly",
+			func(major, minor string, shouldError bool) {
+				fakeDiscovery, ok := fakeClientSet.Discovery().(*discovery.FakeDiscovery)
+				gomega.Expect(ok).To(gomega.BeTrue())
+				fakeDiscovery.FakedServerVersion = &version.Info{
+					Major: major, Minor: minor,
+				}
+
+				err := reconciler.checkOCPVersion(fakeClientSet)
+
+				if shouldError {
+					gomega.Expect(err).To(gomega.HaveOccurred())
+				} else {
+					gomega.Expect(err).NotTo(gomega.HaveOccurred())
+				}
+			},
+
+			// Directly declare entries here
+			ginkgo.Entry("when the OpenShift version is supported", "1", "26", false),
+			ginkgo.Entry("when the OpenShift version is not supported", "1", "25", true),
+		)
+	})
+})
