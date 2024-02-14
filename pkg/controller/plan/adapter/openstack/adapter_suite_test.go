a5ba42a8776c103fc5f5b21a5c1ea21a092a6d9f
Liran Rotenberg <lrotenbe@redhat.com>
2024-02-14 13:25:22 +0200

    Enable openstack builder tests
    Forgotten in previous commit, the tests were added but nothing triggers
    them.
    
    Signed-off-by: Liran Rotenberg <lrotenbe@redhat.com>

---
+++ pkg/controller/plan/adapter/openstack/adapter_suite_test.go
@@ -0,0 +1,22 @@
+package openstack
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+// forkliftFailHandler call ginkgo.Fail with printing the additional information
+func forkliftFailHandler(message string, callerSkip ...int) {
+	if len(callerSkip) > 0 {
+		callerSkip[0]++
+	}
+	Fail(message, callerSkip...)
+}
+
+func TestTests(t *testing.T) {
+	defer GinkgoRecover()
+	RegisterFailHandler(forkliftFailHandler)
+	RunSpecs(t, "openstack builder")
+}
