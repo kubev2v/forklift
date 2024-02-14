beb46317f702c33a6e7ab171807155d5959d2c45
Liran Rotenberg <lrotenbe@redhat.com>
2024-02-14 10:46:29 +0200

    Switch builder test to ginkgo v2
    Signed-off-by: Liran Rotenberg <lrotenbe@redhat.com>

--- pkg/controller/plan/adapter/openstack/builder_test.go
+++ pkg/controller/plan/adapter/openstack/builder_test.go
@@ -1,19 +1,18 @@
 package openstack
 
 import (
-	. "github.com/onsi/ginkgo"
-	"github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("vSphere builder", func() {
-	table.DescribeTable("should", func(os, version, distro, matchPreferenceName string) {
+	DescribeTable("should", func(os, version, distro, matchPreferenceName string) {
 		Expect(getPreferenceOs(os, version, distro)).Should(Equal(matchPreferenceName))
 	},
-		table.Entry("rhel9", RHEL, "9", RHEL, "rhel.9"),
-		table.Entry("centos stream 9", CentOS, "9", CentOS, "centos.stream9"),
-		table.Entry("windows 11", Windows, "11", Windows, "windows.11.virtio"),
-		table.Entry("windows2022", Windows, "2022", Windows, "windows.2k22.virtio"),
-		table.Entry("ubuntu 22", Ubuntu, "22.04.3", Ubuntu, "ubuntu"),
+		Entry("rhel9", RHEL, "9", RHEL, "rhel.9"),
+		Entry("centos stream 9", CentOS, "9", CentOS, "centos.stream9"),
+		Entry("windows 11", Windows, "11", Windows, "windows.11.virtio"),
+		Entry("windows2022", Windows, "2022", Windows, "windows.2k22.virtio"),
+		Entry("ubuntu 22", Ubuntu, "22.04.3", Ubuntu, "ubuntu"),
 	)
 })
