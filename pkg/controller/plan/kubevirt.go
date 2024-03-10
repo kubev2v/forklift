00ad03c6d2cb4a7e148b9a1ca589c82bf66464f7
Bella Khizgiyaev <bkhizgiy@redhat.com>
2024-03-10 17:15:16 +0200

    OVA: adjust the controller to use kubevirt yaml for firmware detection
    Signed-off-by: Bella Khizgiyaev <bkhizgiy@redhat.com>

--- pkg/controller/plan/kubevirt.go
+++ pkg/controller/plan/kubevirt.go
@@ -37,6 +37,7 @@ import (
 	"github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1/plan"
 	"github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1/ref"
 	"github.com/konveyor/forklift-controller/pkg/controller/plan/adapter"
+	yamlparser "github.com/konveyor/forklift-controller/pkg/controller/plan/adapter/ova"
 	plancontext "github.com/konveyor/forklift-controller/pkg/controller/plan/context"
 	libcnd "github.com/konveyor/forklift-controller/pkg/lib/condition"
 	liberr "github.com/konveyor/forklift-controller/pkg/lib/error"
@@ -939,7 +940,7 @@ func (r *KubeVirt) UpdateVmByConvertedConfig(vm *plan.VMStatus, pod *core.Pod, s
 		return
 	}
 
-	url := fmt.Sprintf("http://%s:8080/firmware", pod.Status.PodIP)
+	url := fmt.Sprintf("http://%s:8080/vm", pod.Status.PodIP)
 
 	/* Due to the virt-v2v operation, the ovf file is only available after the command's execution,
 	meaning it appears following the copydisks phase.
@@ -958,16 +959,21 @@ func (r *KubeVirt) UpdateVmByConvertedConfig(vm *plan.VMStatus, pod *core.Pod, s
 	defer resp.Body.Close()
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 	vmConfigBytes, err := io.ReadAll(resp.Body)
 =======
 	vmFirmware, err := io.ReadAll(resp.Body)
 >>>>>>> ae0dd5b8 (OVA: workaround for virt-v2v firmware detection)
+=======
+	vmConf, err := io.ReadAll(resp.Body)
+>>>>>>> 867f6944 (OVA: adjust the controller to use kubevirt yaml for firmware detection)
 	if err != nil {
 		err = liberr.Wrap(err)
 		return
 	}
 	vmConfigXML := string(vmConfigBytes)
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 	switch r.Source.Provider.Type() {
 	case api.Ova:
@@ -984,6 +990,14 @@ func (r *KubeVirt) UpdateVmByConvertedConfig(vm *plan.VMStatus, pod *core.Pod, s
 =======
 	vm.Firmware = string(vmFirmware)
 	r.Log.Info("Setting the vm fimware",
+=======
+	vm.Firmware, err = yamlparser.ReadConfFromYaml(vmConf)
+	if err != nil {
+		r.Log.Error(err, "failed to get firmware configuration")
+	}
+
+	r.Log.Info("Setting the vm firmware",
+>>>>>>> 867f6944 (OVA: adjust the controller to use kubevirt yaml for firmware detection)
 		"vm",
 		vm.String())
 >>>>>>> ae0dd5b8 (OVA: workaround for virt-v2v firmware detection)
