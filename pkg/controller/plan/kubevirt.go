fa516fc2336c2853010df66ccc89250cd549d6be
Liran Rotenberg <lrotenbe@redhat.com>
2024-04-01 16:09:41 +0300

    Fix local preference
    We always looked up for cluster wide preference even when we found a
    local one.
    
    Signed-off-by: Liran Rotenberg <lrotenbe@redhat.com>

--- pkg/controller/plan/kubevirt.go
+++ pkg/controller/plan/kubevirt.go
@@ -1310,11 +1310,12 @@ func (r *KubeVirt) getPreference(vm *plan.VMStatus, preferenceName string) (name
 				"vm",
 				vm.String())
 		} else {
-			return
+			r.Log.Error(err, "could not fetch a local instance type preference for destination VM. trying cluster wide",
+				"vm",
+				vm.String())
 		}
+		name, kind, err = r.getVirtualMachineClusterPreference(vm, preferenceName)
 	}
-
-	name, kind, err = r.getVirtualMachineClusterPreference(vm, preferenceName)
 	return
 }
 
