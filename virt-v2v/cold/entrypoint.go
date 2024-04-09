48a3f89723c3808e1c63fc491aa34b7f5b1ebd2e
Liran Rotenberg <lrotenbe@redhat.com>
2024-04-09 10:28:41 +0300

    Map vSphere static IPs
    This patch will get the static IPs configurations from the vSphere
    provider to each guest and will propogate it to virt-v2v to set it up
    while converting the VM.
    
    Signed-off-by: Liran Rotenberg <lrotenbe@redhat.com>

--- virt-v2v/cold/entrypoint.go
+++ virt-v2v/cold/entrypoint.go
@@ -9,6 +9,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strconv"
+	"strings"
 )
 
 const (
@@ -89,6 +90,12 @@ func main() {
 			}
 		}
 		virtV2vArgs = append(virtV2vArgs, "-ip", "/etc/secret/secretKey")
+
+		if envStaticIPs := os.Getenv("V2V_staticIPs"); envStaticIPs != "" {
+			for _, macToIp := range strings.Split(envStaticIPs, "_") {
+				virtV2vArgs = append(virtV2vArgs, "--mac", macToIp)
+			}
+		}
 
 		if info, err := os.Stat(VDDK); err == nil && info.IsDir() {
 			virtV2vArgs = append(virtV2vArgs,
