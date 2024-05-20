30ebd16b038b4723a4d05f1691c96f1bb4eb3ca0
Arik Hadas <ahadas@redhat.com>
2024-05-20 10:02:32 +0300

    change pipe
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- cmd/virt-v2v-monitor/virt-v2v-monitor.go
+++ cmd/virt-v2v-monitor/virt-v2v-monitor.go
@@ -16,7 +16,7 @@ import (
 )
 
 var COPY_DISK_RE = regexp.MustCompile(`^.*Copying disk (\d+)/(\d+)`)
-var DISK_PROGRESS_RE = regexp.MustCompile(`\s+\((\d+).*|.+ (\d+)% \[[*-]+\]`)
+var DISK_PROGRESS_RE = regexp.MustCompile(`.+ (\d+)% \[[*-]+\]`)
 var FINISHED_RE = regexp.MustCompile(`^\[[ .0-9]*\] Finishing off`)
 
 // Here is a scan function that imposes limit on returned line length. virt-v2v
@@ -29,7 +29,7 @@ func LimitedScanLines(data []byte, atEOF bool) (advance int, token []byte, err e
 	if token != nil || err != nil {
 		return
 	}
-	if len(data) == 1*1024*1024 {
+	if len(data) == bufio.MaxScanTokenSize {
 		// Line is too long for the buffer. Trim it.
 		advance = len(data)
 		token = data
@@ -99,7 +99,7 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 	scanner := NewBufferedScanner(reader, 1*1024*1024)
-
+	fmt.Println("Arik")
 	for scanner.Scan() {
 		line := scanner.Bytes()
 		os.Stdout.Write(line)
@@ -109,23 +109,27 @@ func main() {
 			klog.Fatal("Output monitoring failed! ", err)
 		}
 
-		fmt.Println("this is the line we scanning now ", string(line))
+		//fmt.Println("this is the line we scanning now ", string(line))
 
 		if match := COPY_DISK_RE.FindSubmatch(line); match != nil {
 			diskNumber, _ = strconv.ParseUint(string(match[1]), 10, 0)
 			disks, _ = strconv.ParseUint(string(match[2]), 10, 0)
-			klog.Infof("Copying disk %d out of %d", diskNumber, disks)
+			//klog.Infof("Copying disk %d out of %d", diskNumber, disks)
+			fmt.Printf("Copying disk %d out of %d", diskNumber, disks)
 			progress = 0
 			err = updateProgress(progressCounter, diskNumber, progress)
 		} else if match := DISK_PROGRESS_RE.FindSubmatch(line); match != nil {
-			klog.Info("we are here at progress ", line)
+			//klog.Info("we are here at progress ", line)
+			fmt.Printf("we are here at progress line=%s match-0=%s match-1=%s\n", string(line), string(match[0]), string(match[1]))
 			progress, _ = strconv.ParseUint(string(match[1]), 10, 0)
-			klog.Infof("Progress update, completed %d %%", progress)
+			//klog.Infof("Progress update, completed %d %%", progress)
+			fmt.Printf("Progress update, completed %d %%\n", progress)
 			err = updateProgress(progressCounter, diskNumber, progress)
 		} else if match := FINISHED_RE.Find(line); match != nil {
 			// Make sure we flag conversion as finished. This is
 			// just in case we miss the last progress update for some reason.
-			klog.Infof("Finished")
+			//klog.Infof("Finished")
+			fmt.Printf("Finished\n")
 			for disk := uint64(0); disk < disks; disk++ {
 				err = updateProgress(progressCounter, disk, 100)
 			}
