a8a9b13692acc7cd72708cea9cf1cecd566e3865
Liran Rotenberg <lrotenbe@redhat.com>
2024-05-27 11:40:27 +0300

    Revert "Add unit cases for virt-v2v command"
    This reverts commit ee69f9d60bde16d8e13adb6c2cdf1489d7799a49.
    
    Signed-off-by: Liran Rotenberg <lrotenbe@redhat.com>

--- virt-v2v/cold/cold_test.go
+++ virt-v2v/cold/cold_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"strings"
 	"testing"
 )
 
@@ -25,34 +24,6 @@ func TestGenName(t *testing.T) {
 		got := genName(c.diskNum)
 		if got != c.expected {
 			t.Errorf("genName(%d) = %s; want %s", c.diskNum, got, c.expected)
-		}
-	}
-}
-
-func TestStaticIPs(t *testing.T) {
-	t.Setenv("V2V_source", vSphere)
-	t.Setenv("V2V_libvirtURL", "http://fake.com")
-	t.Setenv("V2V_secretKey", "fake")
-	t.Setenv("V2V_vmName", "test")
-
-	cases := []struct {
-		inputConfig string
-		outputArgs  []string
-	}{
-		{"", []string{""}},
-		{"00:50:56:83:25:47:ip:172.29.3.193", []string{"--mac 00:50:56:83:25:47:ip:172.29.3.193"}},
-		{"00:50:56:83:25:47:ip:172.29.3.193_00:50:56:83:25:47:ip:fe80::5da:b7a5:e0a2:a097", []string{"--mac 00:50:56:83:25:47:ip:172.29.3.193", "--mac 00:50:56:83:25:47:ip:fe80::5da:b7a5:e0a2:a097"}},
-	}
-
-	for _, c := range cases {
-		if c.inputConfig != "" {
-			t.Setenv("V2V_staticIPs", c.inputConfig)
-		}
-		command := strings.Join(buildCommand(), " ")
-		for _, outputArg := range c.outputArgs {
-			if !strings.Contains(command, outputArg) {
-				t.Errorf("The command is: %s. Excpected to contain '%s'", command, outputArg)
-			}
 		}
 	}
 }
