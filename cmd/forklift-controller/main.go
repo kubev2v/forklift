10ab24001bb2fff926992a39465954d53f15fcbc
Bella Khizgiyaev <bkhizgiy@redhat.com>
2024-06-10 23:16:17 +0300

    Add recording rules for forklift metrics
    Signed-off-by: Bella Khizgiyaev <bkhizgiy@redhat.com>

--- cmd/forklift-controller/main.go
+++ cmd/forklift-controller/main.go
@@ -17,6 +17,7 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"net/http"
 	"os"
 	"time"
@@ -26,16 +27,20 @@ import (
 	"github.com/konveyor/forklift-controller/pkg/apis"
 	"github.com/konveyor/forklift-controller/pkg/controller"
 	"github.com/konveyor/forklift-controller/pkg/lib/logging"
+	"github.com/konveyor/forklift-controller/pkg/monitoring/rules"
 	"github.com/konveyor/forklift-controller/pkg/settings"
 	"github.com/konveyor/forklift-controller/pkg/webhook"
 	template "github.com/openshift/api/template/v1"
 	"github.com/pkg/profile"
+	promv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"k8s.io/apimachinery/pkg/api/errors"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	cnv "kubevirt.io/api/core/v1"
 	export "kubevirt.io/api/export/v1alpha1"
 	instancetype "kubevirt.io/api/instancetype/v1beta1"
 	cdi "kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -113,12 +118,62 @@ func main() {
 		log.Error(err, "unable to add kubevirt export APIs to scheme")
 		os.Exit(1)
 	}
+	if err := promv1.AddToScheme(mgr.GetScheme()); err != nil {
+		log.Error(err, "unable to add Prometheus APIs to scheme")
+		os.Exit(1)
+	}
 	if err := template.Install(mgr.GetScheme()); err != nil {
 		log.Error(err, "proceeding without optional OpenShift template APIs")
 	}
 	if err := instancetype.AddToScheme(mgr.GetScheme()); err != nil {
 		log.Error(err, "proceeding without optional kubevirt instance type APIs")
 	}
+
+	mgr.Add(manager.RunnableFunc(func(ctx context.Context) error {
+		// Setup Prometheus recording rules
+		log.Info("Setting up Prometheus recording rules")
+		namespace := "konveyor-forklift" // Replace with the appropriate namespace
+		err = rules.SetupRules(namespace)
+		if err != nil {
+			log.Error(err, "unable to set up Prometheus recording rules")
+			return err
+		}
+
+		// Build the PrometheusRule
+		promRule, err := rules.BuildPrometheusRule(namespace)
+		if err != nil {
+			log.Error(err, "unable to build PrometheusRule")
+			return err
+		}
+
+		// Create or Update the PrometheusRule in the Kubernetes cluster
+		existingPromRule := &promv1.PrometheusRule{}
+		err = mgr.GetClient().Get(context.TODO(), client.ObjectKey{
+			Namespace: namespace,
+			Name:      promRule.Name,
+		}, existingPromRule)
+		if err != nil {
+			if errors.IsNotFound(err) {
+				err = mgr.GetClient().Create(context.TODO(), promRule)
+				if err != nil {
+					log.Error(err, "unable to create PrometheusRule")
+					return err
+				}
+			} else {
+				log.Error(err, "unable to get PrometheusRule")
+				return err
+			}
+		} else {
+			promRule.ResourceVersion = existingPromRule.ResourceVersion
+			err = mgr.GetClient().Update(context.TODO(), promRule)
+			if err != nil {
+				log.Error(err, "unable to update PrometheusRule")
+				return err
+			}
+		}
+		return nil
+	}))
+
 	// Setup all Controllers
 	log.Info("Setting up controller")
 	if err := controller.AddToManager(mgr); err != nil {
