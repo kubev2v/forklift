5394fe56ced6ea664d0cdba44e318a76224f9cf3
Bella Khizgiyaev <bkhizgiy@redhat.com>
2024-06-19 19:21:57 +0300

    Change the metrics to atomic values and add additional metrics gathering to migrations.
    Signed-off-by: Bella Khizgiyaev <bkhizgiy@redhat.com>

--- pkg/monitoring/metrics/forklift-controller/metrics.go
+++ pkg/monitoring/metrics/forklift-controller/metrics.go
@@ -6,43 +6,109 @@ import (
 )
 
 const (
-	Succeeded = "succeeded"
-	Failed    = "failed"
-	Executing = "executing"
-	Running   = "running"
-	Pending   = "pending"
-	Canceled  = "canceled"
-	Blocked   = "blocked"
-	Ready     = "ready"
-	Warm      = "warm"
-	Cold      = "cold"
-	Local     = "local"
-	Remote    = "remote"
+	Succeeded = "Succeeded"
+	Failed    = "Failed"
+	Executing = "Executing"
+	Running   = "Running"
+	Pending   = "Pending"
+	Canceled  = "Canceled"
+	Blocked   = "Blocked"
+	Ready     = "Ready"
+	Deleted   = "Deleted"
+	Warm      = "Warm"
+	Cold      = "Cold"
+	Local     = "Local"
+	Remote    = "Remote"
 )
 
 var (
-	// 'status' - [ succeeded, failed ]
+	// 'status' - [ succeeded, failed, Executing, Canceled]
 	// 'provider' - [oVirt, vSphere, Openstack, OVA, openshift]
-	migrationGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "mtv_workload_migrations",
-		Help: "VM Migrations sorted by status and provider type",
+	// 'mode' - [Cold, Warm]
+	// 'target' - [Local, Remote]
+	migratioStatusCounter = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "mtv_migrations_status",
+		Help: "VM Migrations sorted by status status, provider, mode and destination",
 	},
 		[]string{
 			"status",
 			"provider",
+			"mode",
+			"target",
 		},
 	)
 
-	// 'status' - [ succeeded, failed, Executing, Running, Pending, Canceled, Blocked]
+	// 'status' - [ succeeded, failed, Executing, Running, Pending, Canceled, Blocked, Deleted]
 	// 'provider' - [oVirt, vSphere, Openstack, OVA, openshift]
 	// 'mode' - [Cold, Warm]
 	// 'target' - [Local, Remote]
-	planStatusCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "mtv_workload_plans_status_total",
-		Help: "VM migration Plans sorted by status and provider type",
+	planStatusCounter = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "mtv_plans_status",
+		Help: "VM migration Plans sorted by status, provider, mode and destination",
 	},
 		[]string{
 			"status",
+			"provider",
+			"mode",
+			"target",
+		},
+	)
+
+	// 'status' - [ succeeded, failed, Executing, Canceled]
+	// 'provider' - [oVirt, vSphere, Openstack, OVA, openshift]
+	// 'mode' - [Cold, Warm]
+	// 'target' - [Local, Remote]
+	// 'plan' - [Id]
+	migrationDurationGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "mtv_migration_duration_in_seconds",
+		Help: "Duration of VM migrations in seconds",
+	},
+		[]string{"provider", "mode", "target", "plan"},
+	)
+
+	// 'provider' - [oVirt, vSphere, Openstack, OVA, openshift]
+	// 'mode' - [Cold, Warm]
+	// 'target' - [Local, Remote]
+	// 'plan' - [Id]
+	dataTransferredGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "mtv_data_transferred_in_bytes",
+		Help: "Total data transferred during VM migrations in bytes",
+	},
+		[]string{
+			"provider",
+			"mode",
+			"target",
+			"plan",
+		},
+	)
+
+	// 'status' - [ succeeded, failed, Executing, Canceled]
+	// 'provider' - [oVirt, vSphere, Openstack, OVA, openshift]
+	// 'mode' - [Cold, Warm]
+	// 'target' - [Local, Remote]
+	// 'plan' - [Id]
+	migratioPlanCorolationStatusnCounter = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "mtv_workload_migrations_status_plan_correlation",
+		Help: "VM Migrations by status, provider type and plan",
+	},
+		[]string{
+			"status",
+			"provider",
+			"mode",
+			"target",
+			"plan",
+		},
+	)
+
+	// 'provider' - [oVirt, vSphere, Openstack, OVA, openshift]
+	// 'mode' - [Cold, Warm]
+	// 'target' - [Local, Remote]
+	migrationDurationHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "mtv_migration_duration_in_seconds_bucket",
+		Help:    "Histogram of VM migrations duration in seconds",
+		Buckets: []float64{1 * 3600, 2 * 3600, 5 * 3600, 10 * 3600, 24 * 3600, 48 * 3600}, // 1, 2, 5, 10, 24, 48 hours in seconds
+	},
+		[]string{
 			"provider",
 			"mode",
 			"target",
