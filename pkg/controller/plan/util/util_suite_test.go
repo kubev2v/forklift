efb64bc671cf1ed3e5ec689f8db6e559dd4ee7e7
Arik Hadas <ahadas@redhat.com>
2024-06-19 19:40:53 +0300

    move the tests of GetDeviceNumber to the right package
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- pkg/controller/plan/util/util_suite_test.go
+++ pkg/controller/plan/util/util_suite_test.go
@@ -1,4 +1,4 @@
-package vsphere
+package util
 
 import (
 	"testing"
@@ -7,7 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func TestVsphere(t *testing.T) {
+func TestUtil(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "vSphere Suite")
+	RunSpecs(t, "plan/util Suite")
 }
