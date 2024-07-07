182feac7d7ec71e0c047ccc8a96d4dc789f7e0ac
yaacov <kobi.zamir@gmail.com>
2024-07-07 10:08:01 +0300

    Parse defaultTransferNetwork anotation whith namespace
    Signed-off-by: yaacov <kobi.zamir@gmail.com>

--- tests/suit/forklift_test.go
+++ tests/suit/forklift_test.go
@@ -140,4 +140,75 @@ var _ = Describe("Forklift", func() {
 			Expect(plan.Annotations[AnnPopulatorLabels]).To(Equal("True"))
 		})
 	})
+
+	Context("Plan with provider that is set with transfer network that includes namespace", func() {
+		providerNetworkNamespace := "default"
+		providerNetworkName := "my-network"
+		providerNetwork := providerNetworkNamespace + "/" + providerNetworkName
+
+		var provider *forkliftv1.Provider
+		var targetNS *v1.Namespace
+		var err error
+
+		BeforeEach(func() {
+			targetNS, err = f.CreateNamespace("target", map[string]string{})
+			Expect(err).ToNot(HaveOccurred())
+			targetNS, err = f.CreateNamespace(providerNetworkNamespace, map[string]string{})
+			Expect(err).ToNot(HaveOccurred())
+			By("Create target Openshift provider")
+			annotations := map[string]string{"forklift.konveyor.io/defaultTransferNetwork": providerNetwork}
+			target := utils.NewProvider(utils.TargetProviderName, forkliftv1.OpenShift, namespace, annotations, map[string]string{}, "", nil)
+			err = utils.CreateProviderFromDefinition(f.CrClient, target)
+			Expect(err).ToNot(HaveOccurred())
+			_, err = utils.WaitForProviderReadyWithTimeout(f.CrClient, namespace, utils.TargetProviderName, 30*time.Second)
+			Expect(err).ToNot(HaveOccurred())
+			By("Create oVirt provider")
+			pr := utils.NewProvider(ovirtProviderName, forkliftv1.OVirt, namespace, map[string]string{}, map[string]string{}, f.OvirtClient.OvirtURL, secret)
+			err = utils.CreateProviderFromDefinition(f.CrClient, pr)
+			Expect(err).ToNot(HaveOccurred())
+			provider, err = utils.WaitForProviderReadyWithTimeout(f.CrClient, namespace, ovirtProviderName, 30*time.Second)
+			Expect(err).ToNot(HaveOccurred())
+			By("Create Network Map")
+			networkMapDef := utils.NewNetworkMap(namespace, *provider, networkMapName, vmData.GetVMNics()[0])
+			err = utils.CreateNetworkMapFromDefinition(f.CrClient, networkMapDef)
+			Expect(err).ToNot(HaveOccurred())
+			err = utils.WaitForNetworkMapReadyWithTimeout(f.CrClient, namespace, networkMapName, 30*time.Second)
+			Expect(err).ToNot(HaveOccurred())
+			By("Create Storage Map")
+			storageMapDef := utils.NewStorageMap(namespace, *provider, test_storage_map_name, vmData.GetVMSDs(), ovirtStorageClass)
+			err = utils.CreateStorageMapFromDefinition(f.CrClient, storageMapDef)
+			Expect(err).ToNot(HaveOccurred())
+			err = utils.WaitForStorageMapReadyWithTimeout(f.CrClient, namespace, test_storage_map_name, 30*time.Second)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("Namespaced transfer network should not be set to that of the Provider when it doesn't exist", func() {
+			By("Create plan for provider with defaultTransferNetwork")
+			planDef := utils.NewPlanWithVmId(*provider, namespace, test_plan_name, test_storage_map_name, networkMapName, targetNS.Name, []string{vmData.GetTestVMId()})
+			err = utils.CreatePlanFromDefinition(f.CrClient, planDef)
+			Expect(err).ToNot(HaveOccurred())
+			err, plan := utils.WaitForPlanReadyWithTimeout(f.CrClient, namespace, planDef.Name, 15*time.Second)
+			Expect(err).ToNot(HaveOccurred())
+			By("Verify created plan")
+			Expect(plan).ToNot(BeNil())
+			Expect(plan.Spec.TransferNetwork).To(BeNil())
+		})
+
+		It("Namespaced transfer network should be set to that of the Provider when it exists", func() {
+			By("Create namespaced Network Attachment Definition")
+			err, _ = utils.CreateNetworkAttachmentDefinition(f.CrClient, providerNetworkName, providerNetworkNamespace)
+			Expect(err).ToNot(HaveOccurred())
+			By("Create plan for provider with defaultTransferNetwork")
+			planDef := utils.NewPlanWithVmId(*provider, namespace, test_plan_name, test_storage_map_name, networkMapName, targetNS.Name, []string{vmData.GetTestVMId()})
+			err = utils.CreatePlanFromDefinition(f.CrClient, planDef)
+			Expect(err).ToNot(HaveOccurred())
+			err, plan := utils.WaitForPlanReadyWithTimeout(f.CrClient, namespace, planDef.Name, 15*time.Second)
+			Expect(err).ToNot(HaveOccurred())
+			By("Verify created plan for provider with defaultTransferNetwork")
+			Expect(plan).ToNot(BeNil())
+			Expect(plan.Spec.TransferNetwork).ToNot(BeNil())
+			Expect(plan.Spec.TransferNetwork.Name).To(Equal(providerNetworkName))
+			Expect(plan.Spec.TransferNetwork.Namespace).To(Equal(providerNetworkNamespace))
+		})
+	})
 })
