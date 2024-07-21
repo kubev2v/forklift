b8a3eb55f1d247b4cda16659fc39b1cfee43ee01
Bella Khizgiyaev <bkhizgiy@redhat.com>
2024-07-21 17:43:23 +0300

    Change the migration metrics type to counter to obtain data about the total migrations in the system.
    Signed-off-by: Bella Khizgiyaev <bkhizgiy@redhat.com>

--- pkg/monitoring/metrics/forklift-controller/migration_metrics.go
+++ pkg/monitoring/metrics/forklift-controller/migration_metrics.go
@@ -3,7 +3,6 @@ package forklift_controller
 import (
 	"context"
 	"fmt"
-	"strings"
 	"time"
 
 	api "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1"
@@ -12,6 +11,8 @@ import (
 )
 
 var processedSucceededMigrations = make(map[string]struct{})
+var processedFailedMigrations = make(map[string]struct{})
+var processedCanceledMigrations = make(map[string]struct{})
 
 // Calculate Migrations metrics every 10 seconds
 func RecordMigrationMetrics(c client.Client) {
@@ -28,9 +29,6 @@ func RecordMigrationMetrics(c client.Client) {
 				fmt.Printf("Metrics Migrations list error: %v\n", err)
 				continue
 			}
-
-			// Initialize or reset the counter map at the beginning of each iteration
-			counterMap := make(map[string]float64)
 
 			for _, m := range migrations.Items {
 				plan := api.Plan{}
@@ -54,7 +52,7 @@ func RecordMigrationMetrics(c client.Client) {
 				isLocal := destProvider.Spec.URL == ""
 				isWarm := plan.Spec.Warm
 
-				var target, mode, key string
+				var target, mode string
 				if isLocal {
 					target = Local
 				} else {
@@ -68,51 +66,51 @@ func RecordMigrationMetrics(c client.Client) {
 
 				provider := sourceProvider.Type().String()
 
-				if m.Status.HasCondition(Succeeded) {
-					key = fmt.Sprintf("%s|%s|%s|%s|%s", Succeeded, provider, mode, target, string(plan.UID))
-					counterMap[key]++
+				statusList := []string{Succeeded, Failed, Executing, Canceled}
+				for _, status := range statusList {
+					if m.Status.HasCondition(status) {
+						switch status {
+						case Succeeded:
+							if _, exists := processedSucceededMigrations[string(m.UID)]; !exists {
+								migrationStatusCounter.With(prometheus.Labels{"status": Succeeded, "provider": provider, "mode": mode, "target": target}).Inc()
+								migrationPlanCorrelationStatuCounter.With(prometheus.Labels{"status": Succeeded, "provider": provider, "mode": mode, "target": target, "plan": string(plan.UID)}).Inc()
 
-					startTime := m.Status.Started.Time
-					endTime := m.Status.Completed.Time
-					duration := endTime.Sub(startTime).Seconds()
+								startTime := m.Status.Started.Time
+								endTime := m.Status.Completed.Time
+								duration := endTime.Sub(startTime).Seconds()
 
-					var totalDataTransferred float64
-					for _, vm := range m.Status.VMs {
-						for _, step := range vm.Pipeline {
-							if step.Name == "DiskTransferV2v" || step.Name == "DiskTransfer" {
-								for _, task := range step.Tasks {
-									totalDataTransferred += float64(task.Progress.Completed) * 1024 * 1024 // convert to Bytes
+								var totalDataTransferred float64
+								for _, vm := range m.Status.VMs {
+									for _, step := range vm.Pipeline {
+										if step.Name == "DiskTransferV2v" || step.Name == "DiskTransfer" {
+											for _, task := range step.Tasks {
+												totalDataTransferred += float64(task.Progress.Completed) * 1024 * 1024 // convert to Bytes
+											}
+										}
+									}
 								}
+
+								migrationDurationGauge.With(prometheus.Labels{"provider": provider, "mode": mode, "target": target, "plan": string(plan.UID)}).Set(duration)
+								migrationDurationHistogram.With(prometheus.Labels{"provider": provider, "mode": mode, "target": target}).Observe(duration)
+								dataTransferredGauge.With(prometheus.Labels{"provider": provider, "mode": mode, "target": target, "plan": string(plan.UID)}).Set(totalDataTransferred)
+
+								processedSucceededMigrations[string(m.UID)] = struct{}{}
+							}
+						case Failed:
+							if _, exists := processedFailedMigrations[string(m.UID)]; !exists {
+								migrationStatusCounter.With(prometheus.Labels{"status": Failed, "provider": provider, "mode": mode, "target": target}).Inc()
+								migrationPlanCorrelationStatuCounter.With(prometheus.Labels{"status": Failed, "provider": provider, "mode": mode, "target": target, "plan": string(plan.UID)}).Inc()
+								processedFailedMigrations[string(m.UID)] = struct{}{}
+							}
+						case Canceled:
+							if _, exists := processedCanceledMigrations[string(m.UID)]; !exists {
+								migrationStatusCounter.With(prometheus.Labels{"status": Canceled, "provider": provider, "mode": mode, "target": target}).Inc()
+								migrationPlanCorrelationStatuCounter.With(prometheus.Labels{"status": Canceled, "provider": provider, "mode": mode, "target": target, "plan": string(plan.UID)}).Inc()
+								processedCanceledMigrations[string(m.UID)] = struct{}{}
 							}
 						}
 					}
-
-					// Set the metrics for duration and data transferred and update the map for scaned migration
-					if _, exists := processedSucceededMigrations[string(m.UID)]; !exists {
-						migrationDurationGauge.With(prometheus.Labels{"provider": provider, "mode": mode, "target": target, "plan": string(plan.UID)}).Set(duration)
-						migrationDurationHistogram.With(prometheus.Labels{"provider": provider, "mode": mode, "target": target}).Observe(duration)
-						dataTransferredGauge.With(prometheus.Labels{"provider": provider, "mode": mode, "target": target, "plan": string(plan.UID)}).Set(totalDataTransferred)
-						processedSucceededMigrations[string(m.UID)] = struct{}{}
-					}
 				}
-				if m.Status.HasCondition(Failed) {
-					key = fmt.Sprintf("%s|%s|%s|%s|%s", Failed, provider, mode, target, string(plan.UID))
-					counterMap[key]++
-				}
-				if m.Status.HasCondition(Executing) {
-					key = fmt.Sprintf("%s|%s|%s|%s|%s", Executing, provider, mode, target, string(plan.UID))
-					counterMap[key]++
-				}
-				if m.Status.HasCondition(Canceled) {
-					key = fmt.Sprintf("%s|%s|%s|%s|%s", Canceled, provider, mode, target, string(plan.UID))
-					counterMap[key]++
-				}
-			}
-
-			for key, value := range counterMap {
-				parts := strings.Split(key, "|")
-				migrationStatusGauge.With(prometheus.Labels{"status": parts[0], "provider": parts[1], "mode": parts[2], "target": parts[3]}).Set(value)
-				migrationPlanCorrelationStatusGauge.With(prometheus.Labels{"status": parts[0], "provider": parts[1], "mode": parts[2], "target": parts[3], "plan": parts[4]}).Set(value)
 			}
 		}
 	}()
