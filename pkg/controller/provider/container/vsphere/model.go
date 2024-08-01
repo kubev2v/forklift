1821051844840b8c5463ea0b3f08c7f2b5ed5fda
Liran Rotenberg <lrotenbe@redhat.com>
2024-08-01 14:11:01 +0300

    Add gateway and DNS to migrated vNIC
    Gather the ipStack from vSphere and add the vNIC gateway and DNS when
    the IP preservation is on.
    
    Signed-off-by: Liran Rotenberg <lrotenbe@redhat.com>

--- pkg/controller/provider/container/vsphere/model.go
+++ pkg/controller/provider/container/vsphere/model.go
@@ -1,7 +1,6 @@
 package vsphere
 
 import (
-	"fmt"
 	"net/url"
 	"sort"
 	"strings"
@@ -622,22 +621,45 @@ func (v *VmAdapter) Apply(u types.ObjectUpdate) {
 							continue
 						}
 						for _, ip := range info.IpConfig.IpAddress {
-							//info.DnsConfig
+							var dnsList []string
+							if info.DnsConfig != nil {
+								dnsList = info.DnsConfig.IpAddress
+							}
 							guestNetworksList = append(guestNetworksList, model.GuestNetwork{
 								MAC:          info.MacAddress,
 								IP:           ip.IpAddress,
 								Origin:       ip.Origin,
 								PrefixLength: ip.PrefixLength,
+								DeviceId:     info.DeviceConfigId,
+								DNS:          dnsList,
 							})
 						}
 					}
 					// when the vm goes down, we get an update with empty values - the following check keeps the previously reported data.
 					if len(guestNetworksList) > 0 {
 						v.model.GuestNetworks = guestNetworksList
-						fmt.Println(fmt.Sprintf("guest networks: %v", guestNetworksList))
 					}
 				}
-			//case fGuestIpAddress:
+			case fGuestIpStack:
+				if ipas, cast := p.Val.(types.ArrayOfGuestStackInfo); cast {
+					guestIpStackList := []model.GuestIpStack{}
+					for _, ipa := range ipas.GuestStackInfo {
+						routes := ipa.IpRouteConfig.IpRoute
+						for _, route := range routes {
+							if len(route.Gateway.IpAddress) > 0 {
+								guestIpStackList = append(guestIpStackList, model.GuestIpStack{
+									DeviceId: route.Gateway.Device,
+									Gateway:  route.Gateway.IpAddress,
+									DNS:      ipa.DnsConfig.IpAddress,
+								})
+							}
+						}
+					}
+					// when the vm goes down, we get an update with empty values - the following check keeps the previously reported data.
+					if len(guestIpStackList) > 0 {
+						v.model.GuestIpStacks = guestIpStackList
+					}
+				}
 			case fDevices:
 				if devArray, cast := p.Val.(types.ArrayOfVirtualDevice); cast {
 					devList := []model.Device{}
