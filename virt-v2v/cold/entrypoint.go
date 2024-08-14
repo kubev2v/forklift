f89c6902355867a486c3dfff03ebdcff1637a546
yaacov <kobi.zamir@gmail.com>
2024-08-14 13:14:23 +0300

    Add customize disk image step to virt-v2v pod
    Signed-off-by: yaacov <kobi.zamir@gmail.com>

--- virt-v2v/cold/entrypoint.go
+++ virt-v2v/cold/entrypoint.go
@@ -63,6 +63,12 @@ func main() {
 		fmt.Println("Error getting XML file:", err)
 		os.Exit(1)
 	}
+	// If needed, customize root disk image
+	err = customizeRootDiskImage(source)
+	if err != nil {
+		fmt.Println("Error customizing root disk:", err)
+		os.Exit(1)
+	}
 
 	http.HandleFunc("/vm", vmHandler)
 	http.HandleFunc("/shutdown", shutdownHandler)
@@ -73,6 +79,43 @@ func main() {
 		fmt.Printf("Error starting server: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func customizeRootDiskImage(source string) error {
+	xmlData, err := ReadXMLFile(xmlFilePath)
+	if err != nil {
+		fmt.Printf("Error read XML: %v\n", err)
+		return err
+	}
+
+	operatingSystem, err := GetOperationSystemFromConfig(xmlData)
+	if err != nil {
+		fmt.Printf("Error getting OS ID: %v\n", err)
+	} else {
+		fmt.Printf("Operating System ID: %s\n", operatingSystem)
+	}
+
+	// If user selected 'preserveStaticIPs' and it's a RHEL machine, customize root disk image
+	// and add a first boot script to copy ipv4 and ipv6 settings to new network device.
+	preserveStaticIPs := os.Getenv("V2V_preserveStaticIPs")
+	shouldCustomizeImage := preserveStaticIPs == "true" && source == vSphere && strings.Contains(operatingSystem, "rhel")
+	if shouldCustomizeImage {
+		rootDiskPath, err := FindRootDiskImage(DIR)
+		if err != nil {
+			fmt.Println("Error looking for root disk path:", err)
+			os.Exit(1)
+		} else {
+			fmt.Printf("Root disk path: %s\n", rootDiskPath)
+		}
+
+		err = CustomizeImage(DIR, rootDiskPath)
+		if err != nil {
+			fmt.Println("Error customizing disk image:", err)
+			return err
+		}
+	}
+
+	return nil
 }
 
 func buildCommand() []string {
@@ -294,7 +337,7 @@ func getXMLFile(dir, fileExtension string) (string, error) {
 	if len(files) > 0 {
 		return files[0], nil
 	}
-	return "", fmt.Errorf("XML file was not found.")
+	return "", fmt.Errorf("XML file was not found")
 }
 
 func vmHandler(w http.ResponseWriter, r *http.Request) {
@@ -312,8 +355,8 @@ func vmHandler(w http.ResponseWriter, r *http.Request) {
 
 	xmlData, err := os.ReadFile(xmlFilePath)
 	if err != nil {
-		fmt.Printf("Error reading XML file: %v\n", err)
-		http.Error(w, "Error reading XML file", http.StatusInternalServerError)
+		fmt.Printf("Error: %v\n", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
