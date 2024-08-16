1837c735a6e5cb3f9e8079bcbc06a82dff96c61e
Jonathon Jongsma <jjongsma@redhat.com>
2024-08-16 18:18:16 -0400

    Remove vddk init image requirement
    Since we're now the using el9 image for all conversions, the vddk image
    is not technically required anymore. Remove the check and the error condition.
    
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>

--- pkg/forklift-api/webhooks/validating-webhook/admitters/provider-admitter.go
+++ pkg/forklift-api/webhooks/validating-webhook/admitters/provider-admitter.go
@@ -1,7 +1,6 @@
 package admitters
 
 import (
-	"context"
 	"encoding/json"
 
 	api "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1"
@@ -14,77 +13,6 @@ import (
 type ProviderAdmitter struct {
 	Client   client.Client
 	provider api.Provider
-}
-
-func (admitter *ProviderAdmitter) validateVDDK() error {
-	if admitter.provider.Type() != api.VSphere {
-		log.Info("Provider of this type does not require VDDK, passing", "type", admitter.provider.Type())
-		return nil
-	}
-
-	if err := admitter.validateVddkImage(); err != nil {
-		return err
-	}
-
-	if _, found := admitter.provider.Spec.Settings[api.VDDK]; found {
-		return nil
-	}
-
-	plans := api.PlanList{}
-	err := admitter.Client.List(context.TODO(), &plans, &client.ListOptions{})
-	if err != nil {
-		log.Error(err, "Couldn't get all plans", "namespace", admitter.provider.Namespace)
-		return err
-	}
-
-	for _, plan := range plans.Items {
-		if plan.Spec.Provider.Source.Namespace != admitter.provider.Namespace ||
-			plan.Spec.Provider.Source.Name != admitter.provider.Name {
-			log.V(1).Info("Plan not associated to provider, skipping",
-				"plan", plan.Name,
-				"namespace", plan.Namespace)
-			continue
-		}
-		if plan.Spec.Archived {
-			log.V(1).Info("Plan is archived, skipping",
-				"plan", plan.Name,
-				"namespace", plan.Namespace)
-			continue
-		}
-
-		var destinationProvider api.Provider
-		err = admitter.Client.Get(
-			context.TODO(),
-			client.ObjectKey{
-				Namespace: plan.Spec.Provider.Destination.Namespace,
-				Name:      plan.Spec.Provider.Destination.Name,
-			},
-			&destinationProvider)
-		if err != nil {
-			log.Error(err, "Couldn't get the destination provider for plan, skipping unwillingly",
-				"plan", plan.Name,
-				"namespace", plan.Namespace)
-			continue
-		}
-		plan.Referenced.Provider.Source = &admitter.provider
-		plan.Referenced.Provider.Destination = &destinationProvider
-
-		el9, el9Err := plan.VSphereUsesEl9VirtV2v()
-		if el9Err != nil {
-			log.Error(el9Err, "Could not analyze plan, skipping unwillingly",
-				"plan", plan.Name,
-				"namespace", plan.Namespace)
-			continue
-		}
-		if !el9 {
-			err := liberr.New("Plans requiring VDDK are associated with this provider")
-			log.Error(err, "Plans requiring VDDK are associated with this provider, failing",
-				"plan", plan.Name,
-				"namespace", plan.Namespace)
-			return err
-		}
-	}
-	return nil
 }
 
 func (admitter *ProviderAdmitter) validateVddkImage() error {
@@ -113,10 +41,6 @@ func (admitter *ProviderAdmitter) Admit(ar *admissionv1.AdmissionReview) *admiss
 	raw := ar.Request.Object.Raw
 
 	if err := json.Unmarshal(raw, &admitter.provider); err != nil {
-		return util.ToAdmissionResponseError(err)
-	}
-
-	if err := admitter.validateVDDK(); err != nil {
 		return util.ToAdmissionResponseError(err)
 	}
 
