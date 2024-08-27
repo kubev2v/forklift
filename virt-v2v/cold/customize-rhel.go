d189a6efa4edc94480cb534d8c7f3d00ef67ca10
Yaacov Zamir <kobi.zamir@gmail.com>
2024-08-27 15:08:23 +0300

    [MTV-595] Add customize disk image step to virt-v2v pod (#983)
    Ref: https://issues.redhat.com/browse/MTV-595
    
    **Issue:**
    When migrating a simple RHEL8 VM from VMware 7 to OCPv 4.13 the name of
    the network interfaces changes and the static IP configuration for the
    VM no longer works
    
    **What this pull request do:**
    
    a. If the `os` is RHEL and user opted to `preserveStaticIPs` and the
    source is `vcenter` :
    
    we look for network manager configuration files:
    `/etc/sysconfig/network-scripts/`   - for sysconfig scripts
    `/etc/NetworkManager/system-connections/` - for network connection files
    
    b. If we find configuration files that can connect `ip address` to
    `interface name` we map `interface name` to mac and address.
    
    c. we create a `/etc/udev/rules.d/70-persistent-net.rules` file to pin
    the `interface names` to `mac addresses`
    
    **Limitations:**
    
    Only work for network manager
    Require the source Virtual machine to run while MTV is collecting
    inventory to create the mac to ip mapping
     
    **Screenshot**
    
    ![screenshot-console-openshift-console apps mtv82 rhos-psi cnv-qe rhood
    us-2024 08
    27-13_47_24](https://github.com/user-attachments/assets/4563eee3-54fd-4db1-80c3-6bc47eb084d8)
    
    ---------
    
    Signed-off-by: yaacov <kobi.zamir@gmail.com>

---
+++ virt-v2v/cold/customize-rhel.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type FileSystemTool interface {
+	CreateFilesFromFS(dstDir string) error
+}
+
+type DomainExecFunc func(args ...string) error
+
+func CustomizeRHEL(execFunc DomainExecFunc, disks []string, dir string, t FileSystemTool) error {
+	fmt.Printf("Customizing disks '%v'\n", disks)
+
+	var extraArgs []string
+
+	// Step 1: Create files from the filesystem
+	if err := t.CreateFilesFromFS(dir); err != nil {
+		return fmt.Errorf("failed to create files from filesystem: %w", err)
+	}
+
+	// Step 2: Handle static IP configuration
+	if err := handleStaticIPConfiguration(&extraArgs, dir); err != nil {
+		return err
+	}
+
+	// Step 3: Add scripts
+	if err := addRunScripts(&extraArgs, dir); err != nil {
+		return err
+	}
+	if err := addFirstbootScripts(&extraArgs, dir); err != nil {
+		return err
+	}
+
+	// Step 4: Add the disks to customize
+	addDisksToCustomize(&extraArgs, disks)
+
+	// Step 5: Adds LUKS keys, if they exist
+	if err := addLuksKeysToCustomize(&extraArgs); err != nil {
+		return err
+	}
+
+	// Step 6: Execute the customization with the collected arguments
+	if err := execFunc(extraArgs...); err != nil {
+		return fmt.Errorf("failed to execute domain customization: %w", err)
+	}
+
+	return nil
+}
+
+// handleStaticIPConfiguration processes the static IP configuration and returns the initial extraArgs
+func handleStaticIPConfiguration(extraArgs *[]string, dir string) error {
+	envStaticIPs := os.Getenv("V2V_staticIPs")
+	if envStaticIPs != "" {
+		macToIPFilePath := filepath.Join(dir, "macToIP")
+		macToIPFileContent := strings.ReplaceAll(envStaticIPs, "_", "\n") + "\n"
+
+		if err := os.WriteFile(macToIPFilePath, []byte(macToIPFileContent), 0755); err != nil {
+			return fmt.Errorf("failed to write MAC to IP mapping file: %w", err)
+		}
+
+		*extraArgs = append(*extraArgs, "--upload", macToIPFilePath+":/tmp/macToIP")
+	}
+
+	return nil
+}
+
+// addFirstbootScripts appends firstboot script arguments to extraArgs
+func addFirstbootScripts(extraArgs *[]string, dir string) error {
+	firstbootScriptsPath := filepath.Join(dir, "scripts", "rhel", "firstboot")
+
+	firstBootScripts, err := getScripts(firstbootScriptsPath)
+	if err != nil {
+		return err
+	}
+
+	if len(firstBootScripts) == 0 {
+		fmt.Println("No run scripts found in directory:", firstbootScriptsPath)
+		return nil
+	}
+
+	*extraArgs = append(*extraArgs, getScriptArgs("firstboot", firstBootScripts...)...)
+	return nil
+}
+
+// addRunScripts appends run script arguments to extraArgs
+func addRunScripts(extraArgs *[]string, dir string) error {
+	runScriptsPath := filepath.Join(dir, "scripts", "rhel", "run")
+
+	runScripts, err := getScripts(runScriptsPath)
+	if err != nil {
+		return err
+	}
+
+	if len(runScripts) == 0 {
+		fmt.Println("No run scripts found in directory:", runScriptsPath)
+		return nil
+	}
+
+	*extraArgs = append(*extraArgs, getScriptArgs("run", runScripts...)...)
+	return nil
+}
+
+// getScripts retrieves all .sh scripts from the specified directory
+func getScripts(directory string) ([]string, error) {
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read firstboot scripts directory: %w", err)
+	}
+
+	var scripts []string
+	for _, file := range files {
+		if !file.IsDir() && strings.HasSuffix(file.Name(), ".sh") {
+			scriptPath := filepath.Join(directory, file.Name())
+			scripts = append(scripts, scriptPath)
+		}
+	}
+
+	return scripts, nil
+}
+
+// addDisksToCustomize appends disk arguments to extraArgs
+func addDisksToCustomize(extraArgs *[]string, disks []string) {
+	*extraArgs = append(*extraArgs, getScriptArgs("add", disks...)...)
+}
+
+// addLuksKeysToCustomize appends key arguments to extraArgs
+func addLuksKeysToCustomize(extraArgs *[]string) error {
+	luksArgs, err := addLUKSKeys()
+	if err != nil {
+		return fmt.Errorf("error adding LUKS kyes: %w", err)
+	}
+	*extraArgs = append(*extraArgs, luksArgs...)
+
+	return nil
+}
