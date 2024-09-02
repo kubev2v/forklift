a516d20268bf982b7a70541df0ecc51f725296f6
Fabian Deutsch <fabiand@fedoraproject.org>
2024-09-02 16:39:44 +0200

    more dashify and nm
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- virt-v2v/cold/scripts/linux/run/keep_ifnames.sh
+++ virt-v2v/cold/scripts/linux/run/keep_ifnames.sh
@@ -10,12 +10,16 @@ exit_err_with() { echo "ERR Exit: $@" ; exit 1 ; }
 
 # Check if udev rules file exists
 if [ -f "$UDEV_RULES_FILE" ]; then
-    exit_with "File $UDEV_RULES_FILE already exists. Exiting."
+    exit_err_with "File $UDEV_RULES_FILE already exists. Exiting."
 fi
 
 # _udev_rule HWADDR IFNAME
 :> /var/tmp/70.stage
-stage_udev_rule() { echo "SUBSYSTEM==\"net\",ACTION==\"add\",ATTR{address}==\"$1\",NAME=\"$2\"" >> /var/tmp/70.stage ; }
+stage_udev_rule() {
+    local HWADDR="$1"
+    local IFNAME="$2"
+    echo "SUBSYSTEM==\"net\",ACTION==\"add\",ATTR{address}==\"$HWADDR\",NAME=\"$IFNAME\"" >> /var/tmp/70.stage
+}
 cat_staged_udev_rules() { cat /var/tmp/70.stage ; }
 write_udev_rules() { 
     echo "Old rules:"
@@ -25,6 +29,17 @@ write_udev_rules() {
     mv -v /var/tmp/70.stage $UDEV_RULES_FILE ;
 }
 
+# FN HWADDR IFNAME
+try_stage_udev_rule() {
+    local FN="$1"
+    local FNKEY="$2"
+    local HWADDR="$3"
+    local IFNAME="$4"
+
+    [ -z "$HWADDR" ] && { echo "'$FNKEY' not set in '$FN', unable to match against an interface. Continuing" ; return 1 ; }
+    [ -n "$IFNAME" ] && stage_udev_rule $HWADDR $IFNAME
+}
+
 # Create udev rules based on the macToip mapping + ifcfg network scripts
 udev_from_ifcfg() {
     # Check if the network scripts directory exists
@@ -32,25 +47,35 @@ udev_from_ifcfg() {
 
     for IFCFG_FN in $NETWORK_SCRIPTS_DIR/*;
     do
-        FN_PARSED_IFNAME=${IFCFG_FN/*ifcfg-/}
+        FN_PARSED_IFNAME=${IFCFG_FN#$NETWORK_SCRIPTS_DIR/ifcfg-}
         source $IFCFG_FN
         
-        [ -z "$HWADDR" ] && { echo "HWADDR not set for $IFCFG_FN, unable to match, continuing" ; continue ; }
-
         IFNAME=${DEVICE:-$FN_PARSED_IFNAME}
-        [ -n "$IFNAME" ] && _udev_rule $HWADDR $IFNAME
+        try_stage_udev_rule "$IFCFG_FN" "HWADDR" "$HWADDR" "$IFNAME"
     done
 }
 
+iniToEnv() {
+    _sani() { tr -d "[]" | tr "-" "_" ; }
+    local SECT=""
+    while read LINE ;
+    do
+        [ -z "$LINE" ] && continue ; 
+        SANI_LINE=$(echo "$LINE" | _sani) ;
+        echo "$LINE" | grep -q -E "^\[" && { SECT=$SANI_LINE ; continue ; }
+        echo "${SECT}__${SANI_LINE}"
+    done
+}
 # Create udev rules based on the macToip mapping + network manager connections
 udev_from_nm() {
     # Check if the network connections directory exists
-    if [ ! -d "$NETWORK_CONNECTIONS_DIR" ]; then
-        echo "Warning: Directory $NETWORK_CONNECTIONS_DIR does not exist."
-        return 0
-    fi
+    [ ! -d "$NETWORK_CONNECTIONS_DIR" ] || ret_with "Warning: Directory $NETWORK_CONNECTIONS_DIR does not exist."
 
-   ret_with "NM: Unsupported right now" 
+    for NM_FN in $NETWORK_CONNECTIONS_DIR/*;
+    do
+        eval $(cat $NM_FN | iniToEnv)
+        try_stage_udev_rule "$NM_FN" "ethernet.mac-address" "$ethernet__mac_address" "$connection__interface_name"
+    done
 }
 
 # Create udev rules check for duplicates and write them to udev file
