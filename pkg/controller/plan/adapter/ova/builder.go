216783b48d4ed8ef4188d5d094d8a287ba6ce07d
Martin Necas <mnecas@redhat.com>
2025-01-06 14:41:10 +0100

    MTV-1865 | Remove the q35 machine type
    Issue:
    VMs imported from vSphere always have the q35 machine type set. but the machine type is "floating", and thus the VM abi can change between reboots (i.e. update from rhel 8 to rhel9 or 10 on the host side).
    
    Fix:
    Remove the q35 machine type.
    
    Ref: https://issues.redhat.com/browse/MTV-1865
    
    Signed-off-by: Martin Necas <mnecas@redhat.com>

--- pkg/controller/plan/adapter/ova/builder.go
+++ pkg/controller/plan/adapter/ova/builder.go
@@ -228,7 +228,6 @@ func (r *Builder) VirtualMachine(vmRef ref.Ref, object *cnv.VirtualMachineSpec, 
 	}
 	r.mapDisks(vm, persistentVolumeClaims, object)
 	r.mapFirmware(vm, vmRef, object)
-	r.setMachine(object)
 	r.mapInput(object)
 	if !usesInstanceType {
 		r.mapCPU(vm, object)
@@ -318,10 +317,6 @@ func (r *Builder) mapMemory(vm *model.VM, object *cnv.VirtualMachineSpec) error 
 	reservation := resource.NewQuantity(memoryBytes, resource.BinarySI)
 	object.Template.Spec.Domain.Memory = &cnv.Memory{Guest: reservation}
 	return nil
-}
-
-func (r *Builder) setMachine(object *cnv.VirtualMachineSpec) {
-	object.Template.Spec.Domain.Machine = &cnv.Machine{Type: "q35"}
 }
 
 func (r *Builder) mapCPU(vm *model.VM, object *cnv.VirtualMachineSpec) {
