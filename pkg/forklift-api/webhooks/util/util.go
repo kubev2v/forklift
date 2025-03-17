c96fef3714939d5a28fe92a02835abd36109f099
Martin Necas <mnecas@redhat.com>
2025-03-17 12:26:03 +0100

    Remove unsed code from admission webhooks
    Signed-off-by: Martin Necas <mnecas@redhat.com>

--- pkg/forklift-api/webhooks/util/util.go
+++ pkg/forklift-api/webhooks/util/util.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"net/http"
 
-	api "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1"
 	liberr "github.com/konveyor/forklift-controller/pkg/lib/error"
 	admissionv1 "k8s.io/api/admission/v1beta1"
 	authz "k8s.io/api/authorization/v1"
@@ -41,37 +40,8 @@ func GetAdmissionReview(r *http.Request) (*admissionv1.AdmissionReview, error) {
 	return ar, err
 }
 
-func GetProviderFromAdmissionReview(ar *admissionv1.AdmissionReview) (new *api.Provider, old *api.Provider, err error) {
-
-	/*if !ValidateRequestResource(ar.Request.Resource, webhooks.VirtualMachineInstanceGroupVersionResource.Group, webhooks.VirtualMachineInstanceGroupVersionResource.Resource) {
-		return nil, nil, fmt.Errorf("expect resource to be '%s'", webhooks.VirtualMachineInstanceGroupVersionResource.Resource)
-	}*/
-
-	raw := ar.Request.Object.Raw
-	newProvider := api.Provider{}
-
-	err = json.Unmarshal(raw, &newProvider)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	/*if ar.Request.Operation == admissionv1.Update {
-		raw := ar.Request.OldObject.Raw
-		oldVMI := v12.VirtualMachineInstance{}
-		err = json.Unmarshal(raw, &oldVMI)
-		if err != nil {
-			return nil, nil, err
-		}
-		return &newVMI, &oldVMI, nil
-	}*/
-
-	return &newProvider, nil, nil
-}
-
 // ToAdmissionResponseError
 func ToAdmissionResponseError(err error) *admissionv1.AdmissionResponse {
-	//log.Log.Reason(err).Error("admission generic error")
-
 	return &admissionv1.AdmissionResponse{
 		Allowed: false,
 		Result: &v1.Status{
