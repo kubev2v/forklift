6332637bc4eb69b31107ef965044cfc82eed6331
Martin Necas <mnecas@redhat.com>
2025-04-02 15:31:56 +0200

    Refactor v2v
    - [x] Add abstraction on top of CommandBuilder, CommandExecutor, FileSystem, EmbedFileSystem, which will allow us to inject the mocked dependecies for the testing
    - [x] Add mockgen to create a mocked interfaces https://github.com/uber-go/mock
    - [x] Rewrite the tests using those mocked interfaces
    - [x] Add args, so we don't need to use only env vars
    - [x] Move all const dirs to configurable AppConfig
    - [x] Add settings to contain all env variables, move all settings variables to the settings, add all discovery to the init
    - [x] Add disk struct with a type (filesystem or block), add to that struct links to those disks
    - [x] Refactor v2v to use those disks
    - [x] Create a Conversion, Customize structs containing needed information in argument instead of passing them.
    - [x] Unify the v2v args/env var format -> Create CommandBuilder
    - [x] Add validation step of the args
    
    Signed-off-by: Martin Necas <mnecas@redhat.com>

--- pkg/virt-v2v/utils/command.go
+++ pkg/virt-v2v/utils/command.go
@@ -2,160 +2,115 @@ package utils
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
-
-	"github.com/konveyor/forklift-controller/pkg/virt-v2v/global"
+	"io"
+	"os/exec"
 )
 
-func CheckEnvVariablesSet(envVars ...string) bool {
-	for _, v := range envVars {
-		if os.Getenv(v) == "" {
-			return false
+//go:generate mockgen -source=command.go -package=utils -destination=mock_command.go
+type CommandExecutor interface {
+	Run() error
+	Start() error
+	Wait() error
+	SetStdout(io.Writer)
+	SetStderr(io.Writer)
+	SetStdin(read io.Reader)
+}
+
+// RealCommand wraps exec.Cmd.
+type Command struct {
+	cmd *exec.Cmd
+}
+
+// Run executes the command.
+func (r *Command) Run() error {
+	return r.cmd.Run()
+}
+
+// Start starts the specified command but does not wait for it to complete.
+func (r *Command) Start() error {
+	return r.cmd.Start()
+}
+
+// Wait waits for the command to exit and waits for any copying to
+// stdin or copying from stdout or stderr to complete.
+func (r *Command) Wait() error {
+	return r.cmd.Wait()
+}
+
+// SetStdout sets the Stdout field of exec.Cmd.
+func (r *Command) SetStdout(w io.Writer) {
+	r.cmd.Stdout = w
+}
+
+// SetStderr sets the Stderr field of exec.Cmd.
+func (r *Command) SetStderr(w io.Writer) {
+	r.cmd.Stderr = w
+}
+
+// SetStdin sets the Stderr field of exec.Cmd.
+func (r *Command) SetStdin(read io.Reader) {
+	r.cmd.Stdin = read
+}
+
+//go:generate mockgen -source=command.go -package=utils -destination=mock_command.go
+type CommandBuilder interface {
+	New(cmd string) CommandBuilder
+	AddArg(flag string, value string) CommandBuilder
+	AddArgs(flag string, values ...string) CommandBuilder
+	AddFlag(flag string) CommandBuilder
+	AddPositional(value string) CommandBuilder
+	AddExtraArgs(values ...string) CommandBuilder
+	Build() CommandExecutor
+}
+
+type CommandBuilderImpl struct {
+	BaseCommand string
+	Args        []string
+	// Executor
+	Executor CommandExecutor
+}
+
+func (cb *CommandBuilderImpl) New(cmd string) CommandBuilder {
+	cb.BaseCommand = cmd
+	cb.Args = []string{}
+	return cb
+}
+
+func (cb *CommandBuilderImpl) AddArg(flag string, value string) CommandBuilder {
+	if value != "" {
+		cb.Args = append(cb.Args, flag, value)
+	}
+	return cb
+}
+
+func (cb *CommandBuilderImpl) AddArgs(flag string, values ...string) CommandBuilder {
+	for _, value := range values {
+		if value != "" {
+			cb.Args = append(cb.Args, flag, value)
 		}
 	}
-	return true
+	return cb
 }
 
-// GetScriptArgs generates a list of arguments.
-//
-// Arguments:
-//   - argName (string): Argument name which should be used for all the values
-//   - values (...string): The list of values which should be joined with argument names.
-//
-// Returns:
-//   - []string: List of arguments
-//
-// Example:
-//   - getScriptArgs("firstboot", boot1, boot2) => ["--firstboot", boot1, "--firstboot", boot2]
-func GetScriptArgs(argName string, values ...string) []string {
-	var args []string
-	for _, val := range values {
-		args = append(args, fmt.Sprintf("--%s", argName), val)
-	}
-	return args
+func (cb *CommandBuilderImpl) AddFlag(flag string) CommandBuilder {
+	cb.Args = append(cb.Args, flag)
+	return cb
 }
 
-// AddLUKSKeys checks the LUKS directory for key files and returns the appropriate
-// arguments for a 'virt-' command to add these keys.
-//
-// Returns a slice of strings representing the LUKS key arguments, or an error if
-// there's an issue accessing the directory or reading the files.
-func AddLUKSKeys() ([]string, error) {
-	var luksArgs []string
-
-	if _, err := os.Stat(global.LUKSDIR); err == nil {
-		files, err := GetFilesInPath(global.LUKSDIR)
-		if err != nil {
-			return nil, fmt.Errorf("Error reading files in LUKS directory: %v", err)
-		}
-
-		var luksFiles []string
-		for _, file := range files {
-			luksFiles = append(luksFiles, fmt.Sprintf("all:file:%s", file))
-		}
-
-		luksArgs = append(luksArgs, GetScriptArgs("key", luksFiles...)...)
-	} else if !os.IsNotExist(err) {
-		return nil, fmt.Errorf("Error accessing the LUKS directory: %v", err)
+// Add a single parameter that is NOT a flag (e.g., a file path or a command action)
+func (cb *CommandBuilderImpl) AddPositional(value string) CommandBuilder {
+	if value != "" {
+		cb.Args = append(cb.Args, value)
 	}
-
-	return luksArgs, nil
+	return cb
 }
 
-func GetFilesInPath(rootPath string) (paths []string, err error) {
-	files, err := os.ReadDir(rootPath)
-	if err != nil {
-		fmt.Println("Error reading the files in the directory ", err)
-		return
-	}
-	for _, file := range files {
-		if !file.IsDir() && !strings.HasPrefix(file.Name(), "..") {
-			paths = append(paths, fmt.Sprintf("%s/%s", rootPath, file.Name()))
-		}
-	}
-	return
+func (cb *CommandBuilderImpl) AddExtraArgs(values ...string) CommandBuilder {
+	cb.Args = append(cb.Args, values...)
+	return cb
 }
 
-func genName(diskNum int) string {
-	if diskNum <= 0 {
-		return ""
-	}
-
-	index := (diskNum - 1) % global.LETTERS_LENGTH
-	cycles := (diskNum - 1) / global.LETTERS_LENGTH
-
-	return genName(cycles) + string(global.LETTERS[index])
-}
-
-func getDiskNumber(kind global.MountPath, disk string) (int, error) {
-	switch kind {
-	case global.FS:
-		return strconv.Atoi(disk[15:])
-	case global.BLOCK:
-		return strconv.Atoi(disk[10:])
-	default:
-		return 0, fmt.Errorf("wrong kind when specifying")
-	}
-}
-
-func GetDiskName() string {
-	if name := os.Getenv("V2V_NewName"); name != "" {
-		return name
-	}
-	return os.Getenv("V2V_vmName")
-}
-
-func getDiskLink(kind global.MountPath, disk string) (string, error) {
-	diskNum, err := getDiskNumber(kind, disk)
-	if err != nil {
-		fmt.Println("Error getting disks names ", err)
-		return "", err
-	}
-	return filepath.Join(
-		global.DIR,
-		fmt.Sprintf("%s-sd%s", GetDiskName(), genName(diskNum+1)),
-	), nil
-}
-
-func GetLinkedDisks() ([]string, error) {
-	disks, err := filepath.Glob(
-		filepath.Join(
-			global.DIR,
-			fmt.Sprintf("%s-sd*", GetDiskName()),
-		),
-	)
-	if err != nil {
-		return nil, err
-	}
-	if len(disks) != 0 {
-		return disks, nil
-	}
-	return nil, fmt.Errorf("no disks founds")
-}
-
-func LinkDisks(path global.MountPath) (err error) {
-	disks, err := filepath.Glob(string(path))
-	if err != nil {
-		fmt.Println("Error getting disks ", err)
-		return
-	}
-
-	for _, disk := range disks {
-		diskLink, err := getDiskLink(path, disk)
-		if err != nil {
-			fmt.Println("Error getting disks names ", err)
-			return err
-		}
-		if path == global.FS {
-			disk = fmt.Sprintf("%s/disk.img", disk)
-		}
-		if err = os.Symlink(disk, diskLink); err != nil {
-			fmt.Println("Error creating disk link ", err)
-			return err
-		}
-	}
-	return
+func (cb *CommandBuilderImpl) Build() CommandExecutor {
+	fmt.Print("Building command:", cb.BaseCommand, cb.Args)
+	return &Command{cmd: exec.Command(cb.BaseCommand, cb.Args...)}
 }
