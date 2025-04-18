35d388687ade7aa0e81dda51ba4b5d08b380e332
Jonathon Jongsma <jjongsma@redhat.com>
2025-04-18 13:49:18 -0500

    virt-v2v: use `read -r` to avoid escaping backslashes
    Be defensive -- if any of the lines we're reading have backslashes,
    `read` will treat them as escape sequences. use `-r` to avoid this.
    
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>

--- pkg/virt-v2v/customize/scripts/rhel/run/network_config_util.sh
+++ pkg/virt-v2v/customize/scripts/rhel/run/network_config_util.sh
@@ -86,7 +86,7 @@ udev_from_ifcfg() {
     fi
 
     # Read the mapping file line by line
-    cat "$V2V_MAP_FILE" | while read line;
+    cat "$V2V_MAP_FILE" | while read -r line;
     do
         # Extract S_HW and S_IP
         extract_mac_ip "$line"
@@ -124,7 +124,7 @@ udev_from_nm() {
     fi
 
     # Read the mapping file line by line
-    cat "$V2V_MAP_FILE" | while read line;
+    cat "$V2V_MAP_FILE" | while read -r line;
     do
         # Extract S_HW and S_IP
         extract_mac_ip "$line"
@@ -355,7 +355,7 @@ udev_from_netplan() {
         target_ip="$1"
         if netplan_supports_get; then
           # Loop through all interfaces and check for the given IP address
-          netplan_get ethernets | grep -Eo "^[^[:space:]]+[^:]" | while read IFNAME; do
+          netplan_get ethernets | grep -Eo "^[^[:space:]]+[^:]" | while read -r IFNAME; do
               if netplan_get ethernets."$IFNAME".addresses | grep -q "$target_ip"; then
                   echo "$IFNAME"
                   return
@@ -382,7 +382,7 @@ udev_from_netplan() {
     }
 
     # Read the mapping file line by line
-    cat "$V2V_MAP_FILE" | while read line;
+    cat "$V2V_MAP_FILE" | while read -r line;
     do
         # Extract S_HW and S_IP from the current line in the mapping file
         extract_mac_ip "$line"
@@ -424,7 +424,7 @@ udev_from_ifquery() {
     find_interface_by_ip() {
         target_ip="$1"
         # Loop through all interfaces and check for the given IP address
-        ifquery_get -l | while read IFNAME; do
+        ifquery_get -l | while read -r IFNAME; do
             if ifquery_get $IFNAME | grep -q "$target_ip"; then
                 echo "$IFNAME"
                 return
@@ -433,7 +433,7 @@ udev_from_ifquery() {
     }
 
     # Read the mapping file line by line
-    cat "$V2V_MAP_FILE" | while read line;
+    cat "$V2V_MAP_FILE" | while read -r line;
     do
         # Extract S_HW and S_IP from the current line in the mapping file
         extract_mac_ip "$line"
