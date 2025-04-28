6a8690f946556f752cfb42471f7c532c3a13513d
Martin Necas <mnecas@redhat.com>
2025-04-28 15:23:08 +0200

    Fix the vsphere-xcopy-volume-populator vendors and github build
    `make build-vsphere-xcopy-volume-populator-image` fails with:
    
    ```
    go: google.golang.org/genproto/googleapis/rpc@v0.0.0-20250425173222-7b384671a197: module google.golang.org/genproto/googleapis/rpc@v0.0.0-20250425173222-7b384671a197 requires go >= 1.23.0 (running go 1.22.9; GOTOOLCHAIN=local)
    Error: building at STEP "RUN go mod download && go mod verify": while running runtime: exit status 1
    make: *** [Makefile:265: build-vsphere-xcopy-volume-populator-image] Error 1
    ```
    
    Signed-off-by: Martin Necas <mnecas@redhat.com>

--- cmd/vsphere-xcopy-volume-populator/go.mod
+++ cmd/vsphere-xcopy-volume-populator/go.mod
@@ -31,6 +31,9 @@ require (
 	golang.org/x/crypto v0.28.0 // indirect
 	golang.org/x/oauth2 v0.22.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240509183442-62759503f434 // indirect
+	google.golang.org/grpc v1.64.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	k8s.io/api v0.28.10 // indirect
@@ -99,9 +102,6 @@ require (
 	golang.org/x/term v0.25.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/tools v0.24.0 // indirect
-	google.golang.org/genproto/googleapis/rpc 7b384671a197 // indirect
-	google.golang.org/grpc v1.67.1 // indirect
-	google.golang.org/protobuf v1.35.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	howett.net/plist v0.0.0-20181124034731-591f970eefbb // indirect
 	k8s.io/mount-utils v0.31.1 // indirect
--- cmd/vsphere-xcopy-volume-populator/vendor/google.golang.org/grpc/dialoptions.go
+++ cmd/vsphere-xcopy-volume-populator/vendor/google.golang.org/grpc/dialoptions.go
@@ -21,7 +21,6 @@ package grpc
 import (
 	"context"
 	"net"
-	"net/url"
 	"time"
 
 	"google.golang.org/grpc/backoff"
@@ -33,14 +32,8 @@ import (
 	"google.golang.org/grpc/internal/binarylog"
 	"google.golang.org/grpc/internal/transport"
 	"google.golang.org/grpc/keepalive"
-	"google.golang.org/grpc/mem"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/stats"
-)
-
-const (
-	// https://github.com/grpc/proposal/blob/master/A6-client-retries.md#limits-on-retries-and-hedges
-	defaultMaxCallAttempts = 5
 )
 
 func init() {
@@ -50,18 +43,10 @@ func init() {
 	internal.ClearGlobalDialOptions = func() {
 		globalDialOptions = nil
 	}
-	internal.AddGlobalPerTargetDialOptions = func(opt any) {
-		if ptdo, ok := opt.(perTargetDialOption); ok {
-			globalPerTargetDialOptions = append(globalPerTargetDialOptions, ptdo)
-		}
-	}
-	internal.ClearGlobalPerTargetDialOptions = func() {
-		globalPerTargetDialOptions = nil
-	}
 	internal.WithBinaryLogger = withBinaryLogger
 	internal.JoinDialOptions = newJoinDialOption
 	internal.DisableGlobalDialOptions = newDisableGlobalDialOptions
-	internal.WithBufferPool = withBufferPool
+	internal.WithRecvBufferPool = withRecvBufferPool
 }
 
 // dialOptions configure a Dial call. dialOptions are set by the DialOption
@@ -93,8 +78,8 @@ type dialOptions struct {
 	defaultServiceConfigRawJSON *string
 	resolvers                   []resolver.Builder
 	idleTimeout                 time.Duration
+	recvBufferPool              SharedBufferPool
 	defaultScheme               string
-	maxCallAttempts             int
 }
 
 // DialOption configures how we set up the connection.
@@ -103,19 +88,6 @@ type DialOption interface {
 }
 
 var globalDialOptions []DialOption
-
-// perTargetDialOption takes a parsed target and returns a dial option to apply.
-//
-// This gets called after NewClient() parses the target, and allows per target
-// configuration set through a returned DialOption. The DialOption will not take
-// effect if specifies a resolver builder, as that Dial Option is factored in
-// while parsing target.
-type perTargetDialOption interface {
-	// DialOption returns a Dial Option to apply.
-	DialOptionForTarget(parsedTarget url.URL) DialOption
-}
-
-var globalPerTargetDialOptions []perTargetDialOption
 
 // EmptyDialOption does not alter the dial configuration. It can be embedded in
 // another structure to build custom dial options.
@@ -518,8 +490,6 @@ func WithUserAgent(s string) DialOption {
 
 // WithKeepaliveParams returns a DialOption that specifies keepalive parameters
 // for the client transport.
-//
-// Keepalive is disabled by default.
 func WithKeepaliveParams(kp keepalive.ClientParameters) DialOption {
 	if kp.Time < internal.KeepaliveMinPingTime {
 		logger.Warningf("Adjusting keepalive ping interval to minimum period of %v", internal.KeepaliveMinPingTime)
@@ -679,13 +649,12 @@ func defaultDialOptions() dialOptions {
 			WriteBufferSize: defaultWriteBufSize,
 			UseProxy:        true,
 			UserAgent:       grpcUA,
-			BufferPool:      mem.DefaultBufferPool(),
 		},
 		bs:              internalbackoff.DefaultExponential,
 		healthCheckFunc: internal.HealthCheckFunc,
 		idleTimeout:     30 * time.Minute,
+		recvBufferPool:  nopBufferPool{},
 		defaultScheme:   "dns",
-		maxCallAttempts: defaultMaxCallAttempts,
 	}
 }
 
@@ -743,25 +712,25 @@ func WithIdleTimeout(d time.Duration) DialOption {
 	})
 }
 
-// WithMaxCallAttempts returns a DialOption that configures the maximum number
-// of attempts per call (including retries and hedging) using the channel.
-// Service owners may specify a higher value for these parameters, but higher
-// values will be treated as equal to the maximum value by the client
-// implementation. This mitigates security concerns related to the service
-// config being transferred to the client via DNS.
+// WithRecvBufferPool returns a DialOption that configures the ClientConn
+// to use the provided shared buffer pool for parsing incoming messages. Depending
+// on the application's workload, this could result in reduced memory allocation.
 //
-// A value of 5 will be used if this dial option is not set or n < 2.
-func WithMaxCallAttempts(n int) DialOption {
-	return newFuncDialOption(func(o *dialOptions) {
-		if n < 2 {
-			n = defaultMaxCallAttempts
-		}
-		o.maxCallAttempts = n
-	})
+// If you are unsure about how to implement a memory pool but want to utilize one,
+// begin with grpc.NewSharedBufferPool.
+//
+// Note: The shared buffer pool feature will not be active if any of the following
+// options are used: WithStatsHandler, EnableTracing, or binary logging. In such
+// cases, the shared buffer pool will be ignored.
+//
+// Deprecated: use experimental.WithRecvBufferPool instead.  Will be deleted in
+// v1.60.0 or later.
+func WithRecvBufferPool(bufferPool SharedBufferPool) DialOption {
+	return withRecvBufferPool(bufferPool)
 }
 
-func withBufferPool(bufferPool mem.BufferPool) DialOption {
+func withRecvBufferPool(bufferPool SharedBufferPool) DialOption {
 	return newFuncDialOption(func(o *dialOptions) {
-		o.copts.BufferPool = bufferPool
+		o.recvBufferPool = bufferPool
 	})
 }
