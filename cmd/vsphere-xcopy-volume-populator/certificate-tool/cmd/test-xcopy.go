85c76a6fd7e2ce80dd36f446ab4a5c5b137b6830
borod108 <boris.od@gmail.com>
2025-05-14 16:15:56 +0300

    changes by Orr

--- cmd/vsphere-xcopy-volume-populator/certificate-tool/cmd/test-xcopy.go
+++ cmd/vsphere-xcopy-volume-populator/certificate-tool/cmd/test-xcopy.go
@@ -2,17 +2,19 @@ package cmd
 
 import (
 	"certificate-tool/internal/k8s"
-	"certificate-tool/internal/utils/yaml"
+	// "certificate-tool/internal/utils/yaml"
+	"context"
 	"fmt"
 	"github.com/spf13/cobra"
 	corev1 "k8s.io/api/core/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 var (
 	pvcYamlPath   string
-	crYamlPath    string
+	populatorYamlPath    string
 	vmdkPath      string
 	storageVendor string
 )
@@ -31,10 +33,11 @@ var createTestCmd = &cobra.Command{
 			panic(err)
 		}
 		params := &k8s.TemplateParams{
-			VmdkPath:         vmdkPath,
-			StorageVendor:    storageVendor,
-			PodNamespace:     podNamespace,
-			StorageClassName: storageClassName,
+			VmdkPath:        	 	vmdkPath,
+			StorageVendor: 			storageVendor,
+			TestPopulatorImage:     testPopulatorImage,
+			PodNamespace:     		podNamespace,
+			StorageClassName: 		storageClassName,
 		}
 
 		cobra.CheckErr(k8s.ApplyResource[corev1.PersistentVolumeClaim](
@@ -42,16 +45,20 @@ var createTestCmd = &cobra.Command{
 			k8s.EnsurePersistentVolumeClaim, clientset, podNamespace,
 		))
 
-		vars := params.ToMap()
+		cobra.CheckErr(k8s.ApplyResource[corev1.Pod](
+			populatorYamlPath, params, "${", "}",
+			ensurePod, clientset, podNamespace,
+		))
+		// vars := params.ToMap()
 
-		cobra.CheckErr(
-			yaml.ApplyTemplatedYAML(
-				kubeconfigPath,
-				crYamlPath,
-				vars,
-				"${", "}",
-			),
-		)
+		// cobra.CheckErr(
+		// 	yaml.ApplyTemplatedYAML(
+		// 		kubeconfigPath,
+		// 		populatorYamlPath,
+		// 		vars,
+		// 		"${", "}",
+		// 	),
+		// )
 		fmt.Println("cr and pvc created successfully.")
 	},
 }
@@ -59,7 +66,23 @@ var createTestCmd = &cobra.Command{
 func init() {
 	RootCmd.AddCommand(createTestCmd)
 	createTestCmd.Flags().StringVar(&pvcYamlPath, "pvc-yaml", "assets/manifests/xcopy-setup/xcopy-pvc.yaml", "Path to the PVC YAML file")
-	createTestCmd.Flags().StringVar(&crYamlPath, "cr-yaml", "assets/manifests/xcopy-setup/cr-test-xcopy.yaml", "Path to the CR instance YAML file")
+	createTestCmd.Flags().StringVar(&populatorYamlPath, "populator-yaml", "assets/manifests/xcopy-setup/populator.yaml", "Path to the CR instance YAML file")
 	createTestCmd.Flags().StringVar(&vmdkPath, "vmdk-path", "", "Vmdk path in vsphere")
 	createTestCmd.Flags().StringVar(&storageVendor, "storage-vendor", "cr.yaml", "Name of storage vendor to use")
+	createTestCmd.Flags().StringVar(&testPopulatorImage, "test-populator-image", "quay.io/rh-ee-obengali/vsphere-xcopy-volume-populator", "Name of storage vendor to use")
+}
+
+func ensurePod(clientset *kubernetes.Clientset, namespace string, obj *corev1.Pod) error {
+	existing, err := clientset.CoreV1().Pods(namespace).Get(context.TODO(), obj.Name, metav1.GetOptions{})
+	if err == nil {
+		// Pod exists – maybe update it (though updates to pod.spec are limited)
+		// For demo: delete and recreate (not ideal for production)
+		a := clientset.CoreV1().Pods(namespace).Delete(context.TODO(), obj.Name, metav1.DeleteOptions{})
+		fmt.Println(a)
+	}
+
+	b, err := clientset.CoreV1().Pods(namespace).Create(context.TODO(), obj, metav1.CreateOptions{})
+	fmt.Println(existing)
+	fmt.Println(b)
+	return err
 }
