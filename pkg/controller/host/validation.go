216a4f5b2a9c5585b56d6b7eba90663398256ce9
Martin Necas <mnecas@redhat.com>
2025-05-29 17:16:05 +0200

    MTV-2625 | Fix insecure for vSphere hosts
    Signed-off-by: Martin Necas <mnecas@redhat.com>

--- pkg/controller/host/validation.go
+++ pkg/controller/host/validation.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	api "github.com/konveyor/forklift-controller/pkg/apis/forklift/v1beta1"
+	"github.com/konveyor/forklift-controller/pkg/controller/base"
 	adapter "github.com/konveyor/forklift-controller/pkg/controller/plan/adapter/vsphere"
 	"github.com/konveyor/forklift-controller/pkg/controller/provider/web"
 	"github.com/konveyor/forklift-controller/pkg/controller/provider/web/vsphere"
@@ -236,12 +237,13 @@ func (r *Reconciler) validateSecret(host *api.Host) (err error) {
 				"user",
 				"password",
 			}
-
-			_, err := r.VerifyTLSConnection(host.Spec.IpAddress, secret)
-			if err != nil {
-				cnd.Message = err.Error()
-				cnd.Reason = DataErr
-				host.Status.SetCondition(cnd)
+			if base.GetInsecureSkipVerifyFlag(secret) {
+				_, err := r.VerifyTLSConnection(host.Spec.IpAddress, secret)
+				if err != nil {
+					cnd.Message = err.Error()
+					cnd.Reason = DataErr
+					host.Status.SetCondition(cnd)
+				}
 			}
 		}
 	}
