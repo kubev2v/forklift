3694d281100a87c654cec79eaacdcc72bfc5fdfd
Yaacov Zamir <kobi.zamir@gmail.com>
2025-06-29 13:42:54 +0300

    Keep plus and spaces in vm name and convert to dash (#2101)
    Ref: https://issues.redhat.com/browse/MTV-2805
    
    Plan with "+" within VM name replaced with " " as space in the Inventory
    
    Issue:
    We use URL decode on vSphere names, but we should use Path decode that
    preserve + and other URL signs
    
    Before:
    ```
    $ oc mtv inventory vms vsphere -q "where name ~= 'ameen'"
    NAME                       ID          POWER       CPU         MEMORY      DISK USAGE  GUEST OS    CONCERNS (C/W/I)
    mtv-func-rhel8-ameen//  a  vm-7415     Off         2           4.0 GB      24.0 GB                 0/2/0
    ```
    
    After:
    ```
    $ oc mtv inventory vms vsphere -q "where name ~= 'ameen'"
    NAME                       ID          POWER       CPU         MEMORY      DISK USAGE  GUEST OS    CONCERNS (C/W/I)
    mtv-func-rhel8-ameen//++a  vm-7415     Off         2           4.0 GB      24.0 GB                 0/2/0
    ```
    
    Signed-off-by: yaacov <kobi.zamir@gmail.com>

--- pkg/controller/plan/vm_name_handler_test.go
+++ pkg/controller/plan/vm_name_handler_test.go
@@ -17,7 +17,7 @@ func TestVmNameHandler(t *testing.T) {
 
 	//Test all cases in name adjustments
 	originalVmName := "----------------Vm!@#$%^&*()_+-Name/.is,';[]-CorREct-<>123----------------------"
-	newVmName := "vm--name.is-correct-123"
+	newVmName := "vm-name.is-correct-123"
 	changedName := changeVmName(originalVmName)
 	g.Expect(changedName).To(gomega.Equal(newVmName))
 	g.Expect(validateVmName(changedName)).To(gomega.BeTrue(), "Changed name should match DNS1123 subdomain format")
@@ -41,4 +41,39 @@ func TestVmNameHandler(t *testing.T) {
 	changedMultiDotName2 := changeVmName(multiDotVM2)
 	g.Expect(changedMultiDotName2).To(gomega.Equal(expectedMultiDotResult2))
 	g.Expect(validateVmName(changedMultiDotName2)).To(gomega.BeTrue(), "Changed name with multiple leading dots should match DNS1123 subdomain format")
+
+	//Test conversion of spaces to dashes
+	spaceVM := "vm with spaces in name"
+	expectedSpaceResult := "vm-with-spaces-in-name"
+	changedSpaceName := changeVmName(spaceVM)
+	g.Expect(changedSpaceName).To(gomega.Equal(expectedSpaceResult))
+	g.Expect(validateVmName(changedSpaceName)).To(gomega.BeTrue(), "Changed name with spaces should match DNS1123 subdomain format")
+
+	//Test conversion of + signs to dashes
+	plusVM := "vm+with+plus+signs"
+	expectedPlusResult := "vm-with-plus-signs"
+	changedPlusName := changeVmName(plusVM)
+	g.Expect(changedPlusName).To(gomega.Equal(expectedPlusResult))
+	g.Expect(validateVmName(changedPlusName)).To(gomega.BeTrue(), "Changed name with plus signs should match DNS1123 subdomain format")
+
+	//Test removal of multiple consecutive dashes
+	multipleDashVM := "vm---with----multiple-----dashes"
+	expectedMultipleDashResult := "vm-with-multiple-dashes"
+	changedMultipleDashName := changeVmName(multipleDashVM)
+	g.Expect(changedMultipleDashName).To(gomega.Equal(expectedMultipleDashResult))
+	g.Expect(validateVmName(changedMultipleDashName)).To(gomega.BeTrue(), "Changed name with multiple dashes should match DNS1123 subdomain format")
+
+	//Test complex case with spaces, plus signs, and multiple dashes
+	complexVM := "vm   +++with   ---mixed+++   ---characters"
+	expectedComplexResult := "vm-with-mixed-characters"
+	changedComplexName := changeVmName(complexVM)
+	g.Expect(changedComplexName).To(gomega.Equal(expectedComplexResult))
+	g.Expect(validateVmName(changedComplexName)).To(gomega.BeTrue(), "Changed name with mixed special characters should match DNS1123 subdomain format")
+
+	//Test conversion of * (asterisk) to dashes
+	asteriskVM := "vm*with*asterisk*characters"
+	expectedAsteriskResult := "vm-with-asterisk-characters"
+	changedAsteriskName := changeVmName(asteriskVM)
+	g.Expect(changedAsteriskName).To(gomega.Equal(expectedAsteriskResult))
+	g.Expect(validateVmName(changedAsteriskName)).To(gomega.BeTrue(), "Changed name with asterisk should match DNS1123 subdomain format")
 }
