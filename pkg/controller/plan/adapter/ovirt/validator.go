cc615b3b99c3561c53d67215e02b0203bdd8324e
Elad Hazan <ehazan@redhat.com>
2025-07-16 15:32:47 +0300

    MTV-2810 | Migrate VM from OCP to OCP hang
    Issue:
    OCP vm name with dots causes the migration to hang.
    
    Fix:
    Add additional validations to inform users during plan creation  that OCP VMs names with dots are not supported.
    
    Ref: https://issues.redhat.com/browse/MTV-2810
    Signed-off-by: Elad Hazan <ehazan@redhat.com>

--- pkg/controller/plan/adapter/ovirt/validator.go
+++ pkg/controller/plan/adapter/ovirt/validator.go
@@ -10,6 +10,7 @@ import (
 	model "github.com/kubev2v/forklift/pkg/controller/provider/web/ovirt"
 	liberr "github.com/kubev2v/forklift/pkg/lib/error"
 	"github.com/kubev2v/forklift/pkg/settings"
+	k8svalidation "k8s.io/apimachinery/pkg/util/validation"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -45,6 +46,16 @@ func (r *Validator) MigrationType() bool {
 	default:
 		return false
 	}
+}
+
+func (r *Validator) ValidVmName(vmRef ref.Ref) (ok bool, err error) {
+	// Check if the VM reference name is a valid DNS1123 subdomain
+	if len(k8svalidation.IsDNS1123Subdomain(vmRef.Name)) > 0 {
+		// Not valid
+		return false, nil
+	}
+	// Valid
+	return true, nil
 }
 
 // Validate that a VM's networks have been mapped.
