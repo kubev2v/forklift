56b574ec83c48e9f7db3bc8b717cd3717b7d6e2c
borod108 <boris.od@gmail.com>
2025-07-23 14:34:01 +0300

    more

--- cmd/vsphere-xcopy-volume-populator/internal/pure/flashArray.go
+++ cmd/vsphere-xcopy-volume-populator/internal/pure/flashArray.go
@@ -147,7 +147,7 @@ func (f *FlashArrayClonner) ResolvePVToLUN(pv populator.PersistentVolume) (popul
 	return l, nil
 }
 
-// CopyWithVSphere performs a direct copy operation using vSphere API to discover source volume
+// VvolCopy performs a direct copy operation using vSphere API to discover source volume
 func (f *FlashArrayClonner) VvolCopy(vsphereClient vmware.Client, vmId string, sourceVMDKFile string, persistentVolume populator.PersistentVolume, progress chan<- uint) error {
 	klog.Infof("Starting VVol copy operation for VM %s", vmId)
 
