b586c841272d42758630dc9600b5837ed9449c1c
Matthew Arnold <marnold@redhat.com>
2025-09-15 11:24:10 -0400

    Apply snapshot deltas to populator PVC.
    After the DataVolume snapshot list has been updated, adjust the
    populator PVC annotations to match. Reset the pod phase to Unknown so
    that CDI starts an importer pod with the new snapshot pair.
    
    Signed-off-by: Matthew Arnold <marnold@redhat.com>

--- pkg/controller/plan/migration.go
+++ pkg/controller/plan/migration.go
@@ -1061,6 +1061,24 @@ func (r *Migration) execute(vm *plan.VMStatus) (err error) {
 				break
 			}
 
+			pvcs, err := r.kubevirt.getPVCs(vm.Ref)
+			if err != nil {
+				break
+			}
+			for _, pvc := range pvcs {
+				n := len(vm.Warm.Precopies)
+				pvc.Annotations[base.AnnPodPhase] = "Unknown" // Signal CDI to work on this PVC
+				pvc.Annotations[base.AnnCurrentCheckpoint] = vm.Warm.Precopies[n-1].Snapshot
+				backingFile := pvc.Annotations[base.AnnImportBackingFile]
+				pvc.Annotations[base.AnnPreviousCheckpoint] = vm.Warm.Precopies[n-2].DeltaMap()[backingFile]
+				err = r.Destination.Client.Update(context.TODO(), pvc)
+				if err != nil {
+					step.AddError(err.Error())
+					err = nil
+					break
+				}
+			}
+
 			switch vm.Phase {
 			case api.PhaseAddCheckpoint:
 				vm.Phase = api.PhaseWaitForDataVolumesStatus
