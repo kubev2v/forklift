2256ab3b7a7e4318c85cb9ac6bb38ce682b61fd9
Stefan Olenocin <solenoci@redhat.com>
2025-12-09 16:24:11 +0100

    fix(btrfs): Add btrfs support (#3783)
    Signed-off-by: Stefan Olenocin <solenoci@redhat.com>

--- update-tekton.sh
+++ update-tekton.sh
@@ -3,26 +3,42 @@
 # Use this script to update the Tekton Task Bundle references used in a Pipeline or a PipelineRun.
 # update-tekton-task-bundles.sh .tekton/*.yaml
 
-set -euo pipefail
+set +e -uo pipefail
 
 FILES=$@
 
 # Find existing image references
-OLD_REFS="$(\
-    yq '... | select(has("resolver")) | .params // [] | .[] | select(.name == "bundle") | .value'  $FILES | \
-    grep -v -- '---' | \
-    sort -u \
+OLD_REFS="$(
+  yq '... | select(has("resolver")) | .params // [] | .[] | select(.name == "bundle") | .value' $FILES |
+    grep -v -- '---' |
+    sort -u
 )"
 
 # Find updates for image references
 for old_ref in ${OLD_REFS}; do
-    repo_tag="${old_ref%@*}"
+  repo_tag="${old_ref%@*}"
+  task_ver=${repo_tag#*:}
+  task_x=${task_ver%.*}
+  task_y=$((${task_ver#*.} + 1))
+  task_name=${repo_tag%:*}
+  new_digest=$(skopeo inspect --no-tags docker://${task_name}:${task_x}.${task_y} 2>/dev/null)
+  if [[ $? -eq 0 ]]; then
+    new_digest=$(echo $new_digest | yq '.Digest')
+    new_ref="${task_name}:${task_x}.${task_y}@${new_digest}"
+    echo "New digest and version found! $new_ref"
+    if [[ $SKIP_UPDATE == "true" ]]; then continue; fi
+    for file in $FILES; do
+      sed -i -e "s!${old_ref}!${new_ref}!g" $file
+    done
+    continue
+  else
     new_digest="$(skopeo inspect --no-tags docker://${repo_tag} | yq '.Digest')"
     new_ref="${repo_tag}@${new_digest}"
     [[ $new_ref == $old_ref ]] && continue
     echo "New digest found! $new_ref"
-    if [[ $SKIP_UPDATE == "true" ]]; then continue; fi;
+    if [[ $SKIP_UPDATE == "true" ]]; then continue; fi
     for file in $FILES; do
-        sed -i -e "s!${old_ref}!${new_ref}!g" $file
+      sed -i -e "s!${old_ref}!${new_ref}!g" $file
     done
+  fi
 done
