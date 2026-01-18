a0308e42c9b8210803d5c6390dc2871815d6c96e
Yaacov Zamir <kobi.zamir@gmail.com>
2026-01-18 11:01:58 +0200

    MTV-4349 | Add interface for EC2 client calls (#4346)
    Issue:
    Using EC2 client without an interface makes the code less testable
    
    Fix:
    Add interface between us and the EC2 client lib
    
    Ref: https://issues.redhat.com/browse/MTV-4349
    
    Signed-off-by: yaacov <yzamir@redhat.com>

---
+++ pkg/provider/ec2/controller/client/ec2api.go
@@ -0,0 +1,31 @@
+package client
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+)
+
+// EC2API defines the EC2 operations used by the migration client.
+// This interface allows for mocking AWS EC2 calls in unit tests.
+// The real AWS SDK ec2.Client implements this interface.
+type EC2API interface {
+	// Instance operations
+	DescribeInstances(ctx context.Context, params *ec2.DescribeInstancesInput, optFns ...func(*ec2.Options)) (*ec2.DescribeInstancesOutput, error)
+	StopInstances(ctx context.Context, params *ec2.StopInstancesInput, optFns ...func(*ec2.Options)) (*ec2.StopInstancesOutput, error)
+	StartInstances(ctx context.Context, params *ec2.StartInstancesInput, optFns ...func(*ec2.Options)) (*ec2.StartInstancesOutput, error)
+
+	// Snapshot operations
+	CreateSnapshot(ctx context.Context, params *ec2.CreateSnapshotInput, optFns ...func(*ec2.Options)) (*ec2.CreateSnapshotOutput, error)
+	DeleteSnapshot(ctx context.Context, params *ec2.DeleteSnapshotInput, optFns ...func(*ec2.Options)) (*ec2.DeleteSnapshotOutput, error)
+	DescribeSnapshots(ctx context.Context, params *ec2.DescribeSnapshotsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeSnapshotsOutput, error)
+	ModifySnapshotAttribute(ctx context.Context, params *ec2.ModifySnapshotAttributeInput, optFns ...func(*ec2.Options)) (*ec2.ModifySnapshotAttributeOutput, error)
+
+	// Volume operations
+	DescribeVolumes(ctx context.Context, params *ec2.DescribeVolumesInput, optFns ...func(*ec2.Options)) (*ec2.DescribeVolumesOutput, error)
+	CreateVolume(ctx context.Context, params *ec2.CreateVolumeInput, optFns ...func(*ec2.Options)) (*ec2.CreateVolumeOutput, error)
+	DeleteVolume(ctx context.Context, params *ec2.DeleteVolumeInput, optFns ...func(*ec2.Options)) (*ec2.DeleteVolumeOutput, error)
+}
+
+// Compile-time check to ensure *ec2.Client implements EC2API
+var _ EC2API = (*ec2.Client)(nil)
