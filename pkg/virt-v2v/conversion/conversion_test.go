361b3b57940111f139769a44f8ab0c7a7b9608c4
Martin Necas <mnecas@redhat.com>
2026-02-12 20:37:34 +0900

    MTV-4548 | Borken disabled shared disk conversion
    Resolves: MTV-4548
    
    Issue:
    In the v2v pod we get the libvirt domain directly from hyperv libvirt
    connection. We update that libvirt domain with available disks inside
    the guest covnersion pod.
    ```
        <disk type="file" device="disk">
          <source file="/var/tmp/v2v/msafra-rhel8-shared-disks-2-sda"></source>
          <target dev="sda" bus="scsi"></target>
          <address type="drive" controller="0" bus="0" target="0" unit="0"></address>
        </disk>
        <disk type="file" device="disk">
          <source file="[datastore2] msafra-RHEL8-shared-disks-1/msafra-RHEL8-shared-disks-1.vmdk"></source>
          <target dev="sdp" bus="scsi"></target>
          <address type="drive" controller="0" bus="0" target="0" unit="16"></address>
        </disk>
    ```
    
    Fix:
    Ommit the missing disks
    
    Signed-off-by: Martin Necas <mnecas@redhat.com>

--- pkg/virt-v2v/conversion/conversion_test.go
+++ pkg/virt-v2v/conversion/conversion_test.go
@@ -95,7 +95,274 @@ var _ = Describe("Conversion", func() {
 			}
 			conversion.AppConfig = &appConfig
 
-			mockCommandBuilder.EXPECT().AddArg("--root", "/dev/sda").Return(mockCommandBuilder)
+			luksFiles := utils.ConvertMockDirEntryToOs([]utils.MockDirEntry{
+				{FileName: "key1", FileIsDir: false},
+				{FileName: "key2", FileIsDir: false},
+			})
+
+			mockFileSystem.EXPECT().Stat(luksDir).Return(nil, nil)
+			mockFileSystem.EXPECT().ReadDir(luksDir).Return(luksFiles, nil)
+
+			mockCommandBuilder.EXPECT().AddArg("--root", "first").Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArgs("--key", "all:file:/etc/luks/key1", "all:file:/etc/luks/key2").Return(mockCommandBuilder)
+
+			err := conversion.addCommonArgs(mockCommandBuilder)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("returns error when LUKS directory read fails", func() {
+			luksDir := "/etc/luks"
+			appConfig := config.AppConfig{
+				Luksdir: luksDir,
+			}
+			conversion.AppConfig = &appConfig
+
+			mockFileSystem.EXPECT().Stat(luksDir).Return(nil, nil)
+			mockFileSystem.EXPECT().ReadDir(luksDir).Return(nil, errors.New("permission denied"))
+
+			mockCommandBuilder.EXPECT().AddArg("--root", "first").Return(mockCommandBuilder)
+
+			err := conversion.addCommonArgs(mockCommandBuilder)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("error adding LUKS keys"))
+		})
+	})
+
+	Describe("addVirtV2vRemoteInspectionArgs with multiple disks", func() {
+		It("adds all remote inspection disk args", func() {
+			appConfig.RemoteInspectionDisks = []string{
+				"[datastore1] vm/disk1.vmdk",
+				"[datastore1] vm/disk2.vmdk",
+				"[datastore2] vm/disk3.vmdk",
+			}
+
+			mockCommandBuilder.EXPECT().AddArg("-io", "vddk-file=[datastore1] vm/disk1.vmdk").Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("-io", "vddk-file=[datastore1] vm/disk2.vmdk").Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("-io", "vddk-file=[datastore2] vm/disk3.vmdk").Return(mockCommandBuilder)
+
+			err := conversion.addVirtV2vRemoteInspectionArgs(mockCommandBuilder)
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+
+	Describe("updateDiskPaths", func() {
+		It("updates file source disk paths in domain XML",
+			func() {
+				conversion.Disks = []*Disk{
+					{Link: "/var/tmp/v2v/vm-sda"},
+				}
+
+				domainXML := `<domain type='kvm'>
+  <name>test-vm</name>
+  <devices>
+    <disk type='file' device='disk'>
+      <source file='/original/path/disk.vmdk'/>
+      <target dev='sda' bus='scsi'/>
+    </disk>
+  </devices>
+</domain>`
+
+				result, err := conversion.updateDiskPaths(domainXML)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(result).To(ContainSubstring("/var/tmp/v2v/vm-sda"))
+				Expect(result).ToNot(ContainSubstring("/original/path/disk.vmdk"))
+			},
+		)
+
+		It("updates block source disk paths in domain XML",
+			func() {
+				conversion.Disks = []*Disk{
+					{Link: "/var/tmp/v2v/vm-sda"},
+				}
+
+				domainXML := `<domain type='kvm'>
+  <name>test-vm</name>
+  <devices>
+    <disk type='block' device='disk'>
+      <source dev='/dev/original-block'/>
+      <target dev='sda' bus='scsi'/>
+    </disk>
+  </devices>
+</domain>`
+
+				result, err := conversion.updateDiskPaths(domainXML)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(result).To(ContainSubstring("/var/tmp/v2v/vm-sda"))
+				Expect(result).ToNot(ContainSubstring("/dev/original-block"))
+			},
+		)
+
+		It("handles multiple disks",
+			func() {
+				conversion.Disks = []*Disk{
+					{Link: "/var/tmp/v2v/vm-sda"},
+					{Link: "/var/tmp/v2v/vm-sdb"},
+				}
+
+				domainXML := `<domain type='kvm'>
+  <name>test-vm</name>
+  <devices>
+    <disk type='file' device='disk'>
+      <source file='/original/path/disk1.vmdk'/>
+      <target dev='sda' bus='scsi'/>
+    </disk>
+    <disk type='file' device='disk'>
+      <source file='/original/path/disk2.vmdk'/>
+      <target dev='sdb' bus='scsi'/>
+    </disk>
+  </devices>
+</domain>`
+
+				result, err := conversion.updateDiskPaths(domainXML)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(result).To(ContainSubstring("/var/tmp/v2v/vm-sda"))
+				Expect(result).To(ContainSubstring("/var/tmp/v2v/vm-sdb"))
+			},
+		)
+
+		It("handles empty devices section",
+			func() {
+				conversion.Disks = []*Disk{
+					{Link: "/var/tmp/v2v/vm-sda"},
+				}
+
+				domainXML := `<domain type='kvm'>
+  <name>test-vm</name>
+  <devices>
+  </devices>
+</domain>`
+
+				result, err := conversion.updateDiskPaths(domainXML)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(result).ToNot(ContainSubstring("/var/tmp/v2v/vm-sda"))
+			},
+		)
+
+		It("returns error for invalid XML",
+			func() {
+				conversion.Disks = []*Disk{
+					{Link: "/var/tmp/v2v/vm-sda"},
+				}
+
+				invalidXML := `<domain type='kvm'>
+  <name>test-vm</name>
+  <devices>
+    <disk type='file' device='disk'>
+      <source file='/original/path/disk.vmdk'/>
+    <!-- Missing closing tags -->`
+
+				_, err := conversion.updateDiskPaths(invalidXML)
+				Expect(err).To(HaveOccurred())
+			},
+		)
+
+		It("handles more disks in XML than available",
+			func() {
+				conversion.Disks = []*Disk{
+					{Link: "/var/tmp/v2v/vm-sda"},
+				}
+
+				domainXML := `<domain type='kvm'>
+  <name>test-vm</name>
+  <devices>
+    <disk type='file' device='disk'>
+      <source file='/original/path/disk1.vmdk'/>
+      <target dev='sda' bus='scsi'/>
+    </disk>
+    <disk type='file' device='disk'>
+      <source file='/original/path/disk2.vmdk'/>
+      <target dev='sdb' bus='scsi'/>
+    </disk>
+  </devices>
+</domain>`
+
+				result, err := conversion.updateDiskPaths(domainXML)
+				Expect(err).ToNot(HaveOccurred())
+				// First disk should be updated
+				Expect(result).To(ContainSubstring("/var/tmp/v2v/vm-sda"))
+				// Extra disks beyond available are removed from output
+				Expect(result).ToNot(ContainSubstring("/original/path/disk2.vmdk"))
+				Expect(result).ToNot(ContainSubstring("sdb"))
+			},
+		)
+	})
+
+	Describe("addVirtV2vVsphereArgsForInspection", func() {
+		// Auto generated tests for addVirtV2vVsphereArgsForInspection
+		// Generated by: claude-4-5-opus
+		// Reviewed and uploaded by: @yaacov
+
+		It("adds basic vSphere args without VDDK", func() {
+			appConfig.LibvirtUrl = "vpx://user@vcenter.example.com/Datacenter/Cluster/esxi-host?no_verify=1"
+			appConfig.SecretKey = "/etc/secret/secretKey"
+			appConfig.HostName = "vcenter.example.com"
+			appConfig.VmName = "test-vm"
+			// VddkLibDir is empty, so VDDK args should not be added
+
+			mockCommandBuilder.EXPECT().AddArg("-i", "libvirt").Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("-ic", appConfig.LibvirtUrl).Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("-ip", appConfig.SecretKey).Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("--hostname", appConfig.HostName).Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("--root", "first").Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddPositional("--").Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddPositional("test-vm").Return(mockCommandBuilder)
+
+			err := conversion.addVirtV2vVsphereArgsForInspection(mockCommandBuilder)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("does NOT add conversion extra args (critical separation test)", func() {
+			appConfig.LibvirtUrl = "vpx://user@vcenter.example.com/Datacenter/Cluster/esxi-host?no_verify=1"
+			appConfig.SecretKey = "/etc/secret/secretKey"
+			appConfig.HostName = "vcenter.example.com"
+			appConfig.VmName = "test-vm"
+			// Set ExtraArgs - these should NOT be applied for inspection
+			appConfig.ExtraArgs = []string{"--parallel", "4", "--custom-flag"}
+
+			mockCommandBuilder.EXPECT().AddArg("-i", "libvirt").Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("-ic", appConfig.LibvirtUrl).Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("-ip", appConfig.SecretKey).Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("--hostname", appConfig.HostName).Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("--root", "first").Return(mockCommandBuilder)
+			// Note: NO AddExtraArgs expectation - this is the critical test
+			mockCommandBuilder.EXPECT().AddPositional("--").Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddPositional("test-vm").Return(mockCommandBuilder)
+
+			err := conversion.addVirtV2vVsphereArgsForInspection(mockCommandBuilder)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("adds vSphere args with custom root disk", func() {
+			appConfig.LibvirtUrl = "vpx://user@vcenter.example.com/Datacenter/Cluster/esxi-host?no_verify=1"
+			appConfig.SecretKey = "/etc/secret/secretKey"
+			appConfig.HostName = "vcenter.example.com"
+			appConfig.VmName = "test-vm"
+			appConfig.RootDisk = "/dev/sdb"
+
+			mockCommandBuilder.EXPECT().AddArg("-i", "libvirt").Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("-ic", appConfig.LibvirtUrl).Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("-ip", appConfig.SecretKey).Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("--hostname", appConfig.HostName).Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("--root", "/dev/sdb").Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddPositional("--").Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddPositional("test-vm").Return(mockCommandBuilder)
+
+			err := conversion.addVirtV2vVsphereArgsForInspection(mockCommandBuilder)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("adds vSphere args with static IPs", func() {
+			appConfig.LibvirtUrl = "vpx://user@vcenter.example.com/Datacenter/Cluster/esxi-host?no_verify=1"
+			appConfig.SecretKey = "/etc/secret/secretKey"
+			appConfig.HostName = "vcenter.example.com"
+			appConfig.VmName = "test-vm"
+			appConfig.StaticIPs = "00:11:22:33:44:55:ip:192.168.1.100"
+
+			mockCommandBuilder.EXPECT().AddArg("-i", "libvirt").Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("-ic", appConfig.LibvirtUrl).Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("-ip", appConfig.SecretKey).Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("--hostname", appConfig.HostName).Return(mockCommandBuilder)
+			mockCommandBuilder.EXPECT().AddArg("--root", "first").Return(mockCommandBuilder)
 			mockCommandBuilder.EXPECT().AddArg("--mac", "00:11:22:33:44:55:ip:192.168.1.100").Return(mockCommandBuilder)
 			mockCommandBuilder.EXPECT().AddArg("--mac", "00:11:22:33:44:56:ip:192.168.1.101").Return(mockCommandBuilder)
 
