eab8a7d6d7aa30ecc1dc4679e783a15a4b54fff9
Martin Necas <mnecas@redhat.com>
2026-03-09 18:32:33 +0900

    MTV-4681 | Add label to shared disks with storage offload
    Resolve: MTV-4681
    
    Issue:
    When migrating shared disks with storage offload they are not getting
    attached to the other VMs with care migrated with shared=false.
    
    Fix:
    Add label to the PVC created for storage offload. The builder phase will
    be able to see these disks and reattach them to the VMs.
    
    Signed-off-by: Martin Necas <mnecas@redhat.com>

--- pkg/controller/plan/adapter/vsphere/builder.go
+++ pkg/controller/plan/adapter/vsphere/builder.go
@@ -1363,6 +1363,9 @@ func (r *Builder) PopulatorVolumes(vmRef ref.Ref, annotations map[string]string,
 					labels[TemplateNAALabel] = naa
 				}
 
+				if disk.Shared {
+					labels[Shareable] = "true"
+				}
 				r.Log.Info("target namespace for migration", "namespace", namespace)
 				pvc := core.PersistentVolumeClaim{
 					ObjectMeta: metav1.ObjectMeta{
