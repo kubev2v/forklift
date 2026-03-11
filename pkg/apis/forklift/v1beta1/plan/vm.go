435fd38713f3a0851b4a709fd1b0134550ab5b26
Martin Necas <mnecas@redhat.com>
2026-03-11 16:59:21 +0900

    MTV-4701 | Add rdmAsLun to plan
    Issue:
    When migrating RDM disks they are not treated as LUN but disks. This
    causes that the VM can't send SGIO comnads.
    
    Fix:
    Add option to enable the RDM disks as LUN.
    
    Ref: https://issues.redhat.com/browse/MTV-4701
    
    Resolves: MTV-4701
    
    Signed-off-by: Martin Necas <mnecas@redhat.com>

--- pkg/apis/forklift/v1beta1/plan/vm.go
+++ pkg/apis/forklift/v1beta1/plan/vm.go
@@ -159,6 +159,14 @@ type VM struct {
 	// - false: Force disable nested virtualization on the target VM regardless of source settings
 	// +optional
 	EnableNestedVirtualization *bool `json:"enableNestedVirtualization,omitempty"`
+	// RDMAsLun controls whether RDM (Raw Device Mapping) disks from VMware should be
+	// mapped as LUN devices in the target KubeVirt VM instead of regular disk devices.
+	// When nil (default), the plan-level rdmAsLun value is used.
+	// When explicitly set to true, RDM disks are attached using lun: {} for this VM.
+	// When explicitly set to false, RDM disks are attached as regular disks for this VM.
+	//
+	// +optional
+	RDMAsLun *bool `json:"rdmAsLun,omitempty"`
 }
 
 // Find a Hook for the specified step.
