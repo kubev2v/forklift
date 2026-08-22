422a41237707e3dfb83853de2647e9cf0175af17
red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>
2026-08-22 06:46:58 +0000

    fix(deps): update all go non-major dependencies
    Signed-off-by: red-hat-konflux <126015336+red-hat-konflux[bot]@users.noreply.github.com>

--- cmd/vsphere-xcopy-volume-populator/vendor/kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1/types.go
+++ cmd/vsphere-xcopy-volume-populator/vendor/kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1/types.go
@@ -56,6 +56,9 @@ type DataVolumeSpec struct {
 	Storage *StorageSpec `json:"storage,omitempty"`
 	//PriorityClassName for Importer, Cloner and Uploader pod
 	PriorityClassName string `json:"priorityClassName,omitempty"`
+	// ServiceAccountName for Importer and Uploader pod
+	// +optional
+	ServiceAccountName string `json:"serviceAccountName,omitempty"`
 	//DataVolumeContentType options: "kubevirt", "archive"
 	// +kubebuilder:validation:Enum="kubevirt";"archive"
 	ContentType DataVolumeContentType `json:"contentType,omitempty"`
@@ -156,6 +159,16 @@ type DataVolumeSourceSnapshot struct {
 	Name string `json:"name"`
 }
 
+// DataSourceRefSourceDataSource serves as a reference to another DataSource
+// Can be resolved into a DataVolumeSourcePVC or a DataVolumeSourceSnapshot
+// The maximum depth of a reference chain may not exceed 1.
+type DataSourceRefSourceDataSource struct {
+	// The namespace of the source DataSource
+	Namespace string `json:"namespace"`
+	// The name of the source DataSource
+	Name string `json:"name"`
+}
+
 // DataVolumeBlankImage provides the parameters to create a new raw blank image for the PVC
 type DataVolumeBlankImage struct{}
 
@@ -199,6 +212,15 @@ type DataVolumeSourceRegistry struct {
 	//CertConfigMap provides a reference to the Registry certs
 	// +optional
 	CertConfigMap *string `json:"certConfigMap,omitempty"`
+	//Platform describes the minimum runtime requirements of the image
+	// +optional
+	Platform *PlatformOptions `json:"platform,omitempty"`
+}
+
+type PlatformOptions struct {
+	//Architecture specifies the image target CPU architecture
+	// +optional
+	Architecture string `json:"architecture,omitempty"`
 }
 
 const (
@@ -234,6 +256,14 @@ type DataVolumeSourceHTTP struct {
 	// SecretExtraHeaders is a list of Secret references, each containing an extra HTTP header that may include sensitive information
 	// +optional
 	SecretExtraHeaders []string `json:"secretExtraHeaders,omitempty"`
+	// Checksum is the expected checksum of the file. Format: "algorithm:hash", e.g., "sha256:1234abcd..." or "md5:5678efgh..."
+	// Supported algorithms: md5, sha1, sha256, sha512
+	// If specified, the importer will verify the downloaded content matches this checksum
+	// +optional
+	Checksum string `json:"checksum,omitempty"`
+	// InsecureSkipVerify is a flag to skip certificate verification for the HTTP endpoint
+	// +optional
+	InsecureSkipVerify *bool `json:"insecureSkipVerify,omitempty"`
 }
 
 // DataVolumeSourceImageIO provides the parameters to create a Data Volume from an imageio source
@@ -246,6 +276,8 @@ type DataVolumeSourceImageIO struct {
 	SecretRef string `json:"secretRef,omitempty"`
 	//CertConfigMap provides a reference to the CA cert
 	CertConfigMap string `json:"certConfigMap,omitempty"`
+	// InsecureSkipVerify is a flag to skip certificate verification
+	InsecureSkipVerify *bool `json:"insecureSkipVerify,omitempty"`
 }
 
 // DataVolumeSourceVDDK provides the parameters to create a Data Volume from a Vmware source
@@ -260,8 +292,13 @@ type DataVolumeSourceVDDK struct {
 	Thumbprint string `json:"thumbprint,omitempty"`
 	// SecretRef provides a reference to a secret containing the username and password needed to access the vCenter or ESXi host
 	SecretRef string `json:"secretRef,omitempty"`
+	// CertConfigMap provides a reference to a ConfigMap containing the certificate authority (CA) certificate for the vCenter or ESXi host
+	// +optional
+	CertConfigMap string `json:"certConfigMap,omitempty"`
 	// InitImageURL is an optional URL to an image containing an extracted VDDK library, overrides v2v-vmware config map
 	InitImageURL string `json:"initImageURL,omitempty"`
+	// ExtraArgs is a reference to a ConfigMap containing extra arguments to pass directly to the VDDK library
+	ExtraArgs string `json:"extraArgs,omitempty"`
 }
 
 // DataVolumeSourceRef defines an indirect reference to the source of data for the DataVolume
@@ -407,6 +444,7 @@ const DataVolumeCloneSourceSubresource = "source"
 // +kubebuilder:object:root=true
 // +kubebuilder:storageversion
 // +kubebuilder:resource:scope=Cluster
+// +kubebuilder:subresource:status
 type StorageProfile struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -443,7 +481,23 @@ type StorageProfileStatus struct {
 	DataImportCronSourceFormat *DataImportCronSourceFormat `json:"dataImportCronSourceFormat,omitempty"`
 	// SnapshotClass is optional specific VolumeSnapshotClass for CloneStrategySnapshot. If not set, a VolumeSnapshotClass is chosen according to the provisioner.
 	SnapshotClass *string `json:"snapshotClass,omitempty"`
+	// Conditions contains the current conditions observed for the StorageProfile
+	Conditions []StorageProfileCondition `json:"conditions,omitempty" optional:"true"`
 }
+
+// StorageProfileCondition represents the state of a storage profile condition
+type StorageProfileCondition struct {
+	Type           StorageProfileConditionType `json:"type" description:"type of condition ie. Recognized"`
+	ConditionState `json:",inline"`
+}
+
+// StorageProfileConditionType is the string representation of known condition types
+type StorageProfileConditionType string
+
+const (
+	// StorageProfileRecognized is the condition that indicates if the storage class provisioner and parameters are recognized by CDI
+	StorageProfileRecognized StorageProfileConditionType = "Recognized"
+)
 
 // ClaimPropertySet is a set of properties applicable to PVC
 type ClaimPropertySet struct {
@@ -494,6 +548,8 @@ type DataSourceSource struct {
 	PVC *DataVolumeSourcePVC `json:"pvc,omitempty"`
 	// +optional
 	Snapshot *DataVolumeSourceSnapshot `json:"snapshot,omitempty"`
+	// +optional
+	DataSource *DataSourceRefSourceDataSource `json:"dataSource,omitempty"`
 }
 
 // DataSourceStatus provides the most recently observed status of the DataSource
@@ -567,9 +623,13 @@ type DataImportCronSpec struct {
 	// ManagedDataSource specifies the name of the corresponding DataSource this cron will manage.
 	// DataSource has to be in the same namespace.
 	ManagedDataSource string `json:"managedDataSource"`
-	// RetentionPolicy specifies whether the created DataVolumes and DataSources are retained when their DataImportCron is deleted. Default is RatainAll.
+	// RetentionPolicy specifies whether the created DataVolumes and DataSources are retained when their DataImportCron is deleted. Default is RetainAll.
 	// +optional
 	RetentionPolicy *DataImportCronRetentionPolicy `json:"retentionPolicy,omitempty"`
+	// ServiceAccountName is the name of the ServiceAccount for creating DataVolumes.
+	// +optional
+	// +kubebuilder:validation:MinLength=1
+	ServiceAccountName *string `json:"serviceAccountName,omitempty"`
 }
 
 // DataImportCronGarbageCollect represents the DataImportCron garbage collection mode
@@ -731,7 +791,7 @@ type VolumeUploadSourceList struct {
 	metav1.ListMeta `json:"metadata"`
 
 	// Items provides a list of DataSources
-	Items []VolumeImportSource `json:"items"`
+	Items []VolumeUploadSource `json:"items"`
 }
 
 const (
@@ -990,6 +1050,17 @@ type FilesystemOverhead struct {
 	StorageClass map[string]Percent `json:"storageClass,omitempty"`
 }
 
+// WebhookPvcRenderingPolicy defines the policy for PVC mutating webhook rendering
+type WebhookPvcRenderingPolicy string
+
+const (
+	// WebhookPvcRenderingEnabled means the PVC mutating webhook is active and completes PVC specs from StorageProfiles
+	WebhookPvcRenderingEnabled WebhookPvcRenderingPolicy = "Enabled"
+	// WebhookPvcRenderingDisabled means the PVC mutating webhook is not active;
+	// only PVCs created via DataVolumes will be rendered by the controller
+	WebhookPvcRenderingDisabled WebhookPvcRenderingPolicy = "Disabled"
+)
+
 // CDIConfigSpec defines specification for user configuration
 type CDIConfigSpec struct {
 	// Override the URL used when uploading to a DataVolume
@@ -1003,13 +1074,19 @@ type CDIConfigSpec struct {
 	PodResourceRequirements *corev1.ResourceRequirements `json:"podResourceRequirements,omitempty"`
 	// FeatureGates are a list of specific enabled feature gates
 	FeatureGates []string `json:"featureGates,omitempty"`
-	// FilesystemOverhead describes the space reserved for overhead when using Filesystem volumes. A value is between 0 and 1, if not defined it is 0.055 (5.5% overhead)
+	// WebhookPvcRendering controls whether the PVC mutating webhook that completes
+	// PVC specs from StorageProfiles is enabled or disabled
+	// Allowed values are "Enabled" (default) and "Disabled"
+	// +optional
+	WebhookPvcRendering WebhookPvcRenderingPolicy `json:"webhookPvcRendering,omitempty"`
+	// FilesystemOverhead describes the space reserved for overhead when using Filesystem volumes. A value is between 0 and 1, if not defined it is 0.06 (6% overhead)
 	FilesystemOverhead *FilesystemOverhead `json:"filesystemOverhead,omitempty"`
 	// Preallocation controls whether storage for DataVolumes should be allocated in advance.
 	Preallocation *bool `json:"preallocation,omitempty"`
 	// InsecureRegistries is a list of TLS disabled registries
 	InsecureRegistries []string `json:"insecureRegistries,omitempty"`
 	// DataVolumeTTLSeconds is the time in seconds after DataVolume completion it can be garbage collected. Disabled by default.
+	// Deprecated: Removed in v1.62.
 	// +optional
 	DataVolumeTTLSeconds *int32 `json:"dataVolumeTTLSeconds,omitempty"`
 	// TLSSecurityProfile is used by operators to apply cluster-wide TLS security settings to operands.
