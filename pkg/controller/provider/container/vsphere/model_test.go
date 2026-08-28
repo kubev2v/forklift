kubev2v/forklift

commit 87de40160dfbbf47c5d1680b85c2abcbc20fd634

MTV-6511 | Split Custom Field Definitions from VM table in inventory to dedicated table (#8250)
An issue was found in using forklift to move VMs from vSphere where
there was a large amount of custom value definitions. It resulted in a
very large inventory database as well as very high memory consumption of
the inventory container when the plans were reconciling.

This was due to the entire custom value definition being added to each
VM in the VM table. In the test case there were over 1200 custom value
definitions where all were used, but most VMs only used a few each. In
that case about 95% of the entire inventory DB was the custom value
definitions.

Originally, I thought to make the changes entirely within the inventory
so the consumer did not need to be changed where at the time of the data
being committed to the database, the custom value definitions were for
each VM would be pruned to only the values that VM had defined. But when
an incremental change is made where a value is added to a VM, the
definitions are not resent and it would have resulted in annotations
being excluded until the next time the pod was restarted.

Instead, I opted to move the custom value definitions into a separate
database table and endpoint and have the consumer merge the data
together.

This allows for the data in the DB to be reduced from n (VMs) to 1 as
well as the amount of data passed by each of the calls to the inventory
will be reduced by the size of the custom value definition at the
expense of 1 additional call per VM to collect the custom value
definitions.

Ref: https://redhat.atlassian.net/browse/MTV-6511

Signed-off-by: Tyler Muir <tylergmuir@gmail.com>

pkg/controller/provider/container/vsphere/collector.go
@@ -1282,6 +1282,10 @@ func (r Collector) applyEnter(tx *libmodel.Tx, u types.ObjectUpdate) error {
 	if err != nil {
 		return liberr.Wrap(err)
 	}
+	err = r.upsertCustomFieldDefs(tx, u)
+	if err != nil {
+		return liberr.Wrap(err)
+	}
 
 	return nil
 }
@@ -1301,6 +1305,40 @@ func (r Collector) applyModify(tx *libmodel.Tx, u types.ObjectUpdate) error {
 	err = tx.Update(m)
 	if err != nil {
 		return liberr.Wrap(err)
+	}
+	err = r.upsertCustomFieldDefs(tx, u)
+	if err != nil {
+		return liberr.Wrap(err)
+	}
+
+	return nil
+}
+
+// Upsert the custom field definitions reported on a VirtualMachine update
+// into the provider-global CustomFieldDef table. The same global definition
+// set is reported per-VM, so inserting by key naturally de-duplicates it.
+func (r Collector) upsertCustomFieldDefs(tx *libmodel.Tx, u types.ObjectUpdate) (err error) {
+	if u.Obj.Type != VirtualMachine {
+		return nil
+	}
+	for _, p := range u.ChangeSet {
+		if p.Name != fAvailableField {
+			continue
+		}
+		customFields, ok := p.Val.(types.ArrayOfCustomFieldDef)
+		if !ok {
+			continue
+		}
+		for _, f := range customFields.CustomFieldDef {
+			def := &model.CustomFieldDef{
+				Name:              f.Name,
+				Key:               f.Key,
+				ManagedObjectType: f.ManagedObjectType,
+			}
+			if err = tx.Insert(def); err != nil {
+				return liberr.Wrap(err)
+			}
+		}
 	}
 
 	return nil

pkg/controller/provider/container/vsphere/model_test.go
@@ -1,10 +1,12 @@
 package vsphere
 
 import (
+	"path/filepath"
 	"reflect"
 	"testing"
 
 	model "github.com/kubev2v/forklift/pkg/controller/provider/model/vsphere"
+	libmodel "github.com/kubev2v/forklift/pkg/lib/inventory/model"
 	"github.com/vmware/govmomi/vim25/types"
 )
 
@@ -626,21 +628,12 @@ func TestVmAdapter_getDiskGuestInfo(t *testing.T) {
 func TestVmAdapter_Apply_CustomFields(t *testing.T) {
 	tests := []struct {
 		name              string
-		preExistingDef    []model.CustomFieldDef
 		preExistingValues []model.CustomFieldValue
-		availableFieldVal interface{}
 		customValueVal    interface{}
-		expectedDef       []model.CustomFieldDef
 		expectedValues    []model.CustomFieldValue
 	}{
 		{
-			name: "populates CustomDef and CustomValues from scratch",
-			availableFieldVal: types.ArrayOfCustomFieldDef{
-				CustomFieldDef: []types.CustomFieldDef{
-					{Name: "owner", Key: 100, ManagedObjectType: "VirtualMachine"},
-					{Name: "env", Key: 200, ManagedObjectType: ""},
-				},
-			},
+			name: "populates CustomValues from scratch",
 			customValueVal: types.ArrayOfCustomFieldValue{
 				CustomFieldValue: []types.BaseCustomFieldValue{
 					&types.CustomFieldStringValue{
@@ -653,24 +646,10 @@ func TestVmAdapter_Apply_CustomFields(t *testing.T) {
 					},
 				},
 			},
-			expectedDef: []model.CustomFieldDef{
-				{Name: "owner", Key: 100, ManagedObjectType: "VirtualMachine"},
-				{Name: "env", Key: 200, ManagedObjectType: ""},
-			},
 			expectedValues: []model.CustomFieldValue{
 				{Key: 100, Value: "alice"},
 				{Key: 200, Value: "production"},
 			},
-		},
-		{
-			name: "clears CustomDef when vSphere reports empty availableField",
-			preExistingDef: []model.CustomFieldDef{
-				{Name: "stale", Key: 999},
-			},
-			availableFieldVal: types.ArrayOfCustomFieldDef{
-				CustomFieldDef: []types.CustomFieldDef{},
-			},
-			expectedDef: []model.CustomFieldDef{},
 		},
 		{
 			name: "clears CustomValues when vSphere reports empty customValue (ArrayOf)",
@@ -708,19 +687,11 @@ func TestVmAdapter_Apply_CustomFields(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			v := &VmAdapter{
 				model: model.VM{
-					CustomDef:    tt.preExistingDef,
 					CustomValues: tt.preExistingValues,
 				},
 			}
 
 			changeSet := []types.PropertyChange{}
-			if tt.availableFieldVal != nil {
-				changeSet = append(changeSet, types.PropertyChange{
-					Op:   Assign,
-					Name: fAvailableField,
-					Val:  tt.availableFieldVal,
-				})
-			}
 			if tt.customValueVal != nil {
 				changeSet = append(changeSet, types.PropertyChange{
 					Op:   Assign,
@@ -733,17 +704,101 @@ func TestVmAdapter_Apply_CustomFields(t *testing.T) {
 				ChangeSet: changeSet,
 			})
 
-			if tt.expectedDef != nil {
-				if !reflect.DeepEqual(v.model.CustomDef, tt.expectedDef) {
-					t.Errorf("CustomDef mismatch\ngot:  %+v\nwant: %+v", v.model.CustomDef, tt.expectedDef)
-				}
-			}
 			if tt.expectedValues != nil {
 				if !reflect.DeepEqual(v.model.CustomValues, tt.expectedValues) {
 					t.Errorf("CustomValues mismatch\ngot:  %+v\nwant: %+v", v.model.CustomValues, tt.expectedValues)
 				}
 			}
 		})
+	}
+}
+
+func newTestInventoryDB(t *testing.T) libmodel.DB {
+	t.Helper()
+	db := libmodel.New(filepath.Join(t.TempDir(), "test.db"), &model.CustomFieldDef{})
+	if err := db.Open(true); err != nil {
+		t.Fatalf("Open returned error: %v", err)
+	}
+	t.Cleanup(func() { _ = db.Close(true) })
+	return db
+}
+
+func TestUpsertCustomFieldDefs(t *testing.T) {
+	adapter := Collector{}
+	db := newTestInventoryDB(t)
+	defs := []types.CustomFieldDef{
+		{Name: "owner", Key: 100, ManagedObjectType: "VirtualMachine"},
+		{Name: "env", Key: 200, ManagedObjectType: ""},
+	}
+	u := types.ObjectUpdate{
+		Obj: types.ManagedObjectReference{Type: "VirtualMachine"},
+		ChangeSet: []types.PropertyChange{
+			{
+				Op:   Assign,
+				Name: fAvailableField,
+				Val: types.ArrayOfCustomFieldDef{
+					CustomFieldDef: defs,
+				},
+			},
+		},
+	}
+
+	err := db.With(func(tx *libmodel.Tx) error {
+		return adapter.upsertCustomFieldDefs(tx, u)
+	})
+	if err != nil {
+		t.Fatalf("upsertCustomFieldDefs returned error: %v", err)
+	}
+
+	list := []model.CustomFieldDef{}
+	err = db.List(&list, libmodel.ListOptions{Detail: libmodel.MaxDetail})
+	if err != nil {
+		t.Fatalf("List returned error: %v", err)
+	}
+	if len(list) != len(defs) {
+		t.Fatalf("expected %d defs upserted, got %d", len(defs), len(list))
+	}
+	byKey := make(map[int32]model.CustomFieldDef, len(list))
+	for _, d := range list {
+		byKey[d.Key] = d
+	}
+	for _, f := range defs {
+		got, ok := byKey[f.Key]
+		if !ok {
+			t.Errorf("expected def with key %d to be present", f.Key)
+			continue
+		}
+		expected := model.CustomFieldDef{
+			Name:              f.Name,
+			Key:               f.Key,
+			ManagedObjectType: f.ManagedObjectType,
+		}
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("def %d mismatch\ngot:  %+v\nwant: %+v", f.Key, got, expected)
+		}
+	}
+}
+
+func TestUpsertCustomFieldDefsSkipsNonVM(t *testing.T) {
+	adapter := Collector{}
+	db := newTestInventoryDB(t)
+
+	err := db.With(func(tx *libmodel.Tx) error {
+		return adapter.upsertCustomFieldDefs(tx, types.ObjectUpdate{
+			Obj: types.ManagedObjectReference{Type: "HostSystem"},
+		})
+	})
+	if err != nil {
+		t.Fatalf("upsertCustomFieldDefs returned unexpected error: %v", err)
+	}
+
+	list := []model.CustomFieldDef{}
+	err = db.List(&list, libmodel.ListOptions{Detail: libmodel.MaxDetail})
+	if err != nil {
+		t.Fatalf("List returned error: %v", err)
+	}
+	if len(list) != 0 {
+		t.Errorf("expected no defs for non-VM object, got %d", len(list))
 	}
 }
 

pkg/controller/plan/adapter/vsphere/builder.go
@@ -2880,32 +2880,44 @@ func (r *Builder) SourceVMLabelsAndAnnotations(vmRef ref.Ref, tagMapping *api.Ta
 
 	// Custom attributes to annotations
 	annotationOriginalKeys := make(map[string]string)
+	customDefs := []model.CustomFieldDef{}
+	err = r.Source.Inventory.List(&customDefs, web.Param{
+		Key:   web.DetailParam,
+		Value: "all",
+	})
+	if err != nil {
+		err = liberr.Wrap(err, "custom field definitions")
+		return
+	}
+	customDefByKey := make(map[int32]model.CustomFieldDef, len(customDefs))
+	for _, def := range customDefs {
+		customDefByKey[def.Key] = def
+	}
 	for _, cv := range vm.CustomValues {
-		for _, def := range vm.CustomDef {
-			if def.Key == cv.Key {
-				originalName := def.Name
-				sanitizedName := sanitizeForK8sMetadata(originalName)
-				if sanitizedName == "" {
-					break
-				}
-				if sanitizedName != originalName {
-					sanitizationReport[fmt.Sprintf("customAttribute.name.%s", originalName)] = sanitizedName
-				}
-
-				key := fmt.Sprintf("vsphere.forklift.konveyor.io/%s", sanitizedName)
-				if existingOriginal, exists := annotationOriginalKeys[key]; exists {
-					r.Log.Info("Custom attribute key collision, later attribute overwrites earlier",
-						"sanitizedKey", key,
-						"previousAttribute", existingOriginal,
-						"currentAttribute", originalName)
-					sanitizationReport[fmt.Sprintf("customAttribute.collision.%s", sanitizedName)] = fmt.Sprintf("%s overwrites %s", originalName, existingOriginal)
-				}
-				annotationOriginalKeys[key] = originalName
-
-				annotations[key] = cv.Value
-				break
-			}
+		def, ok := customDefByKey[cv.Key]
+		if !ok {
+			continue
 		}
+		originalName := def.Name
+		sanitizedName := sanitizeForK8sMetadata(originalName)
+		if sanitizedName == "" {
+			continue
+		}
+		if sanitizedName != originalName {
+			sanitizationReport[fmt.Sprintf("customAttribute.name.%s", originalName)] = sanitizedName
+		}
+
+		key := fmt.Sprintf("vsphere.forklift.konveyor.io/%s", sanitizedName)
+		if existingOriginal, exists := annotationOriginalKeys[key]; exists {
+			r.Log.Info("Custom attribute key collision, later attribute overwrites earlier",
+				"sanitizedKey", key,
+				"previousAttribute", existingOriginal,
+				"currentAttribute", originalName)
+			sanitizationReport[fmt.Sprintf("customAttribute.collision.%s", sanitizedName)] = fmt.Sprintf("%s overwrites %s", originalName, existingOriginal)
+		}
+		annotationOriginalKeys[key] = originalName
+
+		annotations[key] = cv.Value
 	}
 
 	return

pkg/controller/plan/adapter/vsphere/builder_test.go
@@ -541,16 +541,18 @@ var _ = Describe("vSphere builder", func() {
 				VM1: model.VM1{
 					VM0: model.VM0{ID: "vm-1", Name: "test-vm"},
 				},
-				CustomDef: []vsphere.CustomFieldDef{
-					{Key: 100, Name: "app-name"},
-					{Key: 101, Name: "app-version"},
-				},
 				CustomValues: []vsphere.CustomFieldValue{
 					{Key: 100, Value: "my-application"},
 					{Key: 101, Value: "v1.2.3"},
 				},
 			}
-			builder.Source.Inventory = &mockInventory{vm: vm}
+			builder.Source.Inventory = &mockInventory{
+				vm: vm,
+				customDefs: []model.CustomFieldDef{
+					{Key: 100, Name: "app-name"},
+					{Key: 101, Name: "app-version"},
+				},
+			}
 
 			labels, annotations, _, err := builder.SourceVMLabelsAndAnnotations(ref.Ref{ID: "vm-1"}, nil)
 
@@ -614,14 +616,16 @@ var _ = Describe("vSphere builder", func() {
 				Tags: []vsphere.Tag{
 					{Name: "owner", Description: "platform-team"},
 				},
-				CustomDef: []vsphere.CustomFieldDef{
-					{Key: 100, Name: "app-name"},
-				},
 				CustomValues: []vsphere.CustomFieldValue{
 					{Key: 100, Value: "my-application"},
 				},
 			}
-			builder.Source.Inventory = &mockInventory{vm: vm}
+			builder.Source.Inventory = &mockInventory{
+				vm: vm,
+				customDefs: []model.CustomFieldDef{
+					{Key: 100, Name: "app-name"},
+				},
+			}
 
 			labels, annotations, _, err := builder.SourceVMLabelsAndAnnotations(ref.Ref{ID: "vm-1"}, nil)
 
@@ -642,14 +646,16 @@ var _ = Describe("vSphere builder", func() {
 					{Name: "owner", Description: "platform-team"},
 					{Name: "environment", Description: "production"},
 				},
-				CustomDef: []vsphere.CustomFieldDef{
-					{Key: 100, Name: "app-name"},
-				},
 				CustomValues: []vsphere.CustomFieldValue{
 					{Key: 100, Value: "my-application"},
 				},
 			}
-			builder.Source.Inventory = &mockInventory{vm: vm}
+			builder.Source.Inventory = &mockInventory{
+				vm: vm,
+				customDefs: []model.CustomFieldDef{
+					{Key: 100, Name: "app-name"},
+				},
+			}
 
 			tagMapping := &v1beta1.TagMapping{
 				Disabled: true,
@@ -715,14 +721,16 @@ var _ = Describe("vSphere builder", func() {
 				VM1: model.VM1{
 					VM0: model.VM0{ID: "vm-1", Name: "test-vm"},
 				},
-				CustomDef: []vsphere.CustomFieldDef{
-					{Key: 100, Name: "invalid attr name"},
-				},
 				CustomValues: []vsphere.CustomFieldValue{
 					{Key: 100, Value: "some-value"},
 				},
 			}
-			builder.Source.Inventory = &mockInventory{vm: vm}
+			builder.Source.Inventory = &mockInventory{
+				vm: vm,
+				customDefs: []model.CustomFieldDef{
+					{Key: 100, Name: "invalid attr name"},
+				},
+			}
 
 			_, annotations, sanitizationReport, err := builder.SourceVMLabelsAndAnnotations(ref.Ref{ID: "vm-1"}, nil)
 
@@ -739,16 +747,18 @@ var _ = Describe("vSphere builder", func() {
 				VM1: model.VM1{
 					VM0: model.VM0{ID: "vm-1", Name: "test-vm"},
 				},
-				CustomDef: []vsphere.CustomFieldDef{
-					{Key: 100, Name: "!@#$"},
-					{Key: 101, Name: "valid-attr"},
-				},
 				CustomValues: []vsphere.CustomFieldValue{
 					{Key: 100, Value: "should-be-skipped"},
 					{Key: 101, Value: "kept"},
 				},
 			}
-			builder.Source.Inventory = &mockInventory{vm: vm}
+			builder.Source.Inventory = &mockInventory{
+				vm: vm,
+				customDefs: []model.CustomFieldDef{
+					{Key: 100, Name: "!@#$"},
+					{Key: 101, Name: "valid-attr"},
+				},
+			}
 
 			_, annotations, _, err := builder.SourceVMLabelsAndAnnotations(ref.Ref{ID: "vm-1"}, nil)
 

pkg/controller/plan/adapter/vsphere/validator_test.go
@@ -23,9 +23,10 @@ var ErrNotImplemented = errors.New("not implemented")
 
 // Mock inventory struct and methods for testing
 type mockInventory struct {
-	ds       model.Datastore
-	vm       model.VM
-	networks map[string]model.Network // keyed by ID
+	ds         model.Datastore
+	vm         model.VM
+	networks   map[string]model.Network // keyed by ID
+	customDefs []model.CustomFieldDef   // global custom field definitions
 }
 
 // defaultVM returns a VM with sensible defaults for testing
@@ -131,6 +132,10 @@ func (m *mockInventory) Host(ref *ref.Ref) (interface{}, error) {
 }
 
 func (m *mockInventory) List(list interface{}, param ...web.Param) error {
+	switch v := list.(type) {
+	case *[]model.CustomFieldDef:
+		*v = m.customDefs
+	}
 	return nil
 }
 

pkg/controller/provider/container/vsphere/model.go
@@ -1098,19 +1098,6 @@ func (v *VmAdapter) Apply(u types.ObjectUpdate) {
 						SortNICsByGuestNetworkOrder(&v.model)
 					}
 				}
-			case fAvailableField:
-				if customFields, cast := p.Val.(types.ArrayOfCustomFieldDef); cast {
-					customDef := []model.CustomFieldDef{}
-					for _, f := range customFields.CustomFieldDef {
-						customDef = append(customDef, model.CustomFieldDef{
-							Name:              f.Name,
-							Key:               f.Key,
-							ManagedObjectType: f.ManagedObjectType,
-						})
-					}
-
-					v.model.CustomDef = customDef
-				}
 			case fCustomValue:
 				customValues := []model.CustomFieldValue{}
 				switch cv := p.Val.(type) {

pkg/controller/provider/model/vsphere/doc.go
@@ -15,6 +15,7 @@ func All() []interface{} {
 		&Network{},
 		&Datastore{},
 		&Host{},
+		&CustomFieldDef{},
 		&VM{},
 	}
 }

pkg/controller/provider/model/vsphere/model.go
@@ -1,6 +1,8 @@
 package vsphere
 
 import (
+	"strconv"
+
 	"github.com/kubev2v/forklift/pkg/controller/provider/model/base"
 	libmodel "github.com/kubev2v/forklift/pkg/lib/inventory/model"
 )
@@ -360,7 +362,6 @@ type VM struct {
 	ToolsVersionStatus       string             `sql:""`
 	DiskEnableUuid           bool               `sql:""`
 	NestedHVEnabled          bool               `sql:""`
-	CustomDef                []CustomFieldDef   `sql:""`
 	CustomValues             []CustomFieldValue `sql:""`
 	Tags                     []Tag              `sql:""`
 	ConsolidationNeeded      bool               `sql:""`
@@ -479,8 +480,13 @@ type DiskMountPoint struct {
 
 type CustomFieldDef struct {
 	Name              string `json:"name"`
-	Key               int32  `json:"key"`
+	Key               int32  `json:"key" sql:"pk"`
 	ManagedObjectType string `json:"managedObjectType,omitempty"`
+}
+
+// Get the primary key.
+func (m *CustomFieldDef) Pk() string {
+	return strconv.Itoa(int(m.Key))
 }
 
 type CustomFieldValue struct {

commit c3e6eec49d510c9db040798599e6eaf6bee9248c

MTV-6511 | Fixes performance regression from #8250 (#8367)
With the PR #8250 there was a performance regression that caused the
initial inventory parsing to take 3-4 times longer due to heavier
reliance on the database to process updates instead of only inserts.

This change caches the custom def fields into memory in the inventory
application and then does a compare against that before attempting the
database insert/update. This is especially impactful on the initial
startup which attempts to insert/update the same custom def fields for
each VM.

Resolves: MTV-6511

Signed-off-by: Tyler Muir <tylergmuir@gmail.com>

pkg/controller/provider/container/vsphere/collector.go
@@ -304,6 +304,10 @@ type Collector struct {
 	// Cached missing privileges from the last check.
 	missingPrivs   *privilegeCheckResult
 	missingPrivsMu *sync.RWMutex
+	// Committed provider-global custom field definitions (keyed by field key)
+	// so repeated per-VM upserts can be skipped via in-memory comparison.
+	customFieldDefs   map[int32]model.CustomFieldDef
+	customFieldDefsMu *sync.RWMutex
 }
 
 // New collector.
@@ -314,12 +318,14 @@ func New(db libmodel.DB, provider *api.Provider, secret *core.Secret) *Collector
 			provider.GetNamespace(),
 			provider.GetName()))
 	return &Collector{
-		url:            provider.Spec.URL,
-		provider:       provider,
-		secret:         secret,
-		db:             db,
-		log:            nlog,
-		missingPrivsMu: &sync.RWMutex{},
+		url:               provider.Spec.URL,
+		provider:          provider,
+		secret:            secret,
+		db:                db,
+		log:               nlog,
+		missingPrivsMu:    &sync.RWMutex{},
+		customFieldDefs:   map[int32]model.CustomFieldDef{},
+		customFieldDefsMu: &sync.RWMutex{},
 	}
 }
 
@@ -551,11 +557,13 @@ func (r *Collector) getUpdates(ctx context.Context) error {
 			err = tx.Commit()
 			if err != nil {
 				r.log.Error(err, "tx commit failed.")
+				r.resetCustomFieldDefs()
 			}
 		} else {
 			if endErr := tx.End(); endErr != nil {
 				r.log.Error(endErr, "tx rollback failed.")
 			}
+			r.resetCustomFieldDefs()
 		}
 		if err == nil && (updateSet.Truncated == nil || !*updateSet.Truncated) {
 			if !r.parity {
@@ -1271,7 +1279,7 @@ func (r *Collector) selectAdapter(u types.ObjectUpdate) (Adapter, bool) {
 }
 
 // Object created.
-func (r Collector) applyEnter(tx *libmodel.Tx, u types.ObjectUpdate) error {
+func (r *Collector) applyEnter(tx *libmodel.Tx, u types.ObjectUpdate) error {
 	adapter, selected := r.selectAdapter(u)
 	if !selected {
 		return nil
@@ -1291,7 +1299,7 @@ func (r Collector) applyEnter(tx *libmodel.Tx, u types.ObjectUpdate) error {
 }
 
 // Object modified.
-func (r Collector) applyModify(tx *libmodel.Tx, u types.ObjectUpdate) error {
+func (r *Collector) applyModify(tx *libmodel.Tx, u types.ObjectUpdate) error {
 	adapter, selected := r.selectAdapter(u)
 	if !selected {
 		return nil
@@ -1317,31 +1325,88 @@ func (r Collector) applyModify(tx *libmodel.Tx, u types.ObjectUpdate) error {
 // Upsert the custom field definitions reported on a VirtualMachine update
 // into the provider-global CustomFieldDef table. The same global definition
 // set is reported per-VM, so inserting by key naturally de-duplicates it.
-func (r Collector) upsertCustomFieldDefs(tx *libmodel.Tx, u types.ObjectUpdate) (err error) {
+//
+// The set of definitions is provider-global and identical for every VM, so it
+// is cached in memory. When the reported set matches the cache, the DB
+// upserts are skipped entirely, avoiding repeated inserts on every VM update.
+func (r *Collector) upsertCustomFieldDefs(tx *libmodel.Tx, u types.ObjectUpdate) (err error) {
 	if u.Obj.Type != VirtualMachine {
 		return nil
 	}
+	var reported []types.CustomFieldDef
 	for _, p := range u.ChangeSet {
 		if p.Name != fAvailableField {
 			continue
 		}
 		customFields, ok := p.Val.(types.ArrayOfCustomFieldDef)
 		if !ok {
-			continue
+			return nil
 		}
-		for _, f := range customFields.CustomFieldDef {
-			def := &model.CustomFieldDef{
-				Name:              f.Name,
-				Key:               f.Key,
-				ManagedObjectType: f.ManagedObjectType,
-			}
-			if err = tx.Insert(def); err != nil {
-				return liberr.Wrap(err)
-			}
+		reported = customFields.CustomFieldDef
+	}
+	if len(reported) == 0 {
+		return nil
+	}
+
+	incoming := make(map[int32]model.CustomFieldDef, len(reported))
+	for _, f := range reported {
+		incoming[f.Key] = model.CustomFieldDef{
+			Name:              f.Name,
+			Key:               f.Key,
+			ManagedObjectType: f.ManagedObjectType,
 		}
 	}
 
+	r.customFieldDefsMu.RLock()
+	equal := equalCustomFieldDefs(r.customFieldDefs, incoming)
+	r.customFieldDefsMu.RUnlock()
+	if equal {
+		return nil
+	}
+
+	for _, f := range reported {
+		def := &model.CustomFieldDef{
+			Name:              f.Name,
+			Key:               f.Key,
+			ManagedObjectType: f.ManagedObjectType,
+		}
+		if err = tx.Insert(def); err != nil {
+			return liberr.Wrap(err)
+		}
+	}
+
+	// The cache is updated in place. If the surrounding transaction is rolled
+	// back, resetCustomFieldDefs drops it so a later retry reinserts the defs
+	// rather than skipping them on a stale cache.
+	r.customFieldDefsMu.Lock()
+	r.customFieldDefs = incoming
+	r.customFieldDefsMu.Unlock()
+
 	return nil
+}
+
+// resetCustomFieldDefs drops the cached custom field definitions. It is called
+// when the transaction that updated them is rolled back, so that the cache
+// never reflects definitions that were not committed to the DB.
+func (r *Collector) resetCustomFieldDefs() {
+	r.customFieldDefsMu.Lock()
+	r.customFieldDefs = map[int32]model.CustomFieldDef{}
+	r.customFieldDefsMu.Unlock()
+}
+
+// equalCustomFieldDefs reports whether the two provider-global custom field
+// definition sets are identical.
+func equalCustomFieldDefs(a, b map[int32]model.CustomFieldDef) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for k, av := range a {
+		bv, ok := b[k]
+		if !ok || av != bv {
+			return false
+		}
+	}
+	return true
 }
 
 // Object deleted.

pkg/controller/provider/container/vsphere/model_test.go
@@ -3,6 +3,7 @@ package vsphere
 import (
 	"path/filepath"
 	"reflect"
+	"sync"
 	"testing"
 
 	model "github.com/kubev2v/forklift/pkg/controller/provider/model/vsphere"
@@ -723,8 +724,15 @@ func newTestInventoryDB(t *testing.T) libmodel.DB {
 	return db
 }
 
+func newTestCollector() *Collector {
+	return &Collector{
+		customFieldDefs:   map[int32]model.CustomFieldDef{},
+		customFieldDefsMu: &sync.RWMutex{},
+	}
+}
+
 func TestUpsertCustomFieldDefs(t *testing.T) {
-	adapter := Collector{}
+	adapter := newTestCollector()
 	db := newTestInventoryDB(t)
 	defs := []types.CustomFieldDef{
 		{Name: "owner", Key: 100, ManagedObjectType: "VirtualMachine"},
@@ -780,7 +788,7 @@ func TestUpsertCustomFieldDefs(t *testing.T) {
 }
 
 func TestUpsertCustomFieldDefsSkipsNonVM(t *testing.T) {
-	adapter := Collector{}
+	adapter := newTestCollector()
 	db := newTestInventoryDB(t)
 
 	err := db.With(func(tx *libmodel.Tx) error {
@@ -799,6 +807,129 @@ func TestUpsertCustomFieldDefsSkipsNonVM(t *testing.T) {
 	}
 	if len(list) != 0 {
 		t.Errorf("expected no defs for non-VM object, got %d", len(list))
+	}
+}
+
+func TestUpsertCustomFieldDefsCaches(t *testing.T) {
+	adapter := newTestCollector()
+	db := newTestInventoryDB(t)
+
+	defs := []types.CustomFieldDef{
+		{Name: "owner", Key: 100, ManagedObjectType: "VirtualMachine"},
+		{Name: "env", Key: 200, ManagedObjectType: ""},
+	}
+	update := func(defs []types.CustomFieldDef) types.ObjectUpdate {
+		return types.ObjectUpdate{
+			Obj: types.ManagedObjectReference{Type: "VirtualMachine", Value: "vm-1"},
+			ChangeSet: []types.PropertyChange{
+				{
+					Op:   Assign,
+					Name: fAvailableField,
+					Val:  types.ArrayOfCustomFieldDef{CustomFieldDef: defs},
+				},
+			},
+		}
+	}
+
+	countRows := func() int {
+		t.Helper()
+		list := []model.CustomFieldDef{}
+		if err := db.List(&list, libmodel.ListOptions{Detail: libmodel.MaxDetail}); err != nil {
+			t.Fatalf("List returned error: %v", err)
+		}
+		return len(list)
+	}
+
+	if err := db.With(func(tx *libmodel.Tx) error {
+		return adapter.upsertCustomFieldDefs(tx, update(defs))
+	}); err != nil {
+		t.Fatalf("first upsert returned error: %v", err)
+	}
+	if got := countRows(); got != len(defs) {
+		t.Fatalf("expected %d rows after first upsert, got %d", len(defs), got)
+	}
+
+	// The same provider-global def set reported by a second VM should be
+	// skipped via the in-memory cache (no additional DB writes, no dup rows).
+	if err := db.With(func(tx *libmodel.Tx) error {
+		return adapter.upsertCustomFieldDefs(tx, update(defs))
+	}); err != nil {
+		t.Fatalf("repeated upsert returned error: %v", err)
+	}
+	if got := countRows(); got != len(defs) {
+		t.Fatalf("expected %d rows after repeated upsert, got %d", len(defs), got)
+	}
+
+	// A changed def set should be reflected in the DB.
+	changed := []types.CustomFieldDef{
+		{Name: "owner", Key: 100, ManagedObjectType: "VirtualMachine"},
+		{Name: "env-renamed", Key: 200, ManagedObjectType: ""},
+		{Name: "team", Key: 300, ManagedObjectType: "VirtualMachine"},
+	}
+	if err := db.With(func(tx *libmodel.Tx) error {
+		return adapter.upsertCustomFieldDefs(tx, update(changed))
+	}); err != nil {
+		t.Fatalf("changed upsert returned error: %v", err)
+	}
+	if got := countRows(); got != len(changed) {
+		t.Fatalf("expected %d rows after changed upsert, got %d", len(changed), got)
+	}
+}
+
+func TestUpsertCustomFieldDefsRollbackRetriesIdentical(t *testing.T) {
+	adapter := newTestCollector()
+	db := newTestInventoryDB(t)
+
+	defs := []types.CustomFieldDef{
+		{Name: "owner", Key: 100, ManagedObjectType: "VirtualMachine"},
+		{Name: "env", Key: 200, ManagedObjectType: ""},
+	}
+	u := types.ObjectUpdate{
+		Obj: types.ManagedObjectReference{Type: "VirtualMachine", Value: "vm-1"},
+		ChangeSet: []types.PropertyChange{
+			{
+				Op:   Assign,
+				Name: fAvailableField,
+				Val:  types.ArrayOfCustomFieldDef{CustomFieldDef: defs},
+			},
+		},
+	}
+
+	countRows := func() int {
+		t.Helper()
+		list := []model.CustomFieldDef{}
+		if err := db.List(&list, libmodel.ListOptions{Detail: libmodel.MaxDetail}); err != nil {
+			t.Fatalf("List returned error: %v", err)
+		}
+		return len(list)
+	}
+
+	// Stage the defs inside a transaction that is then rolled back.
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatalf("Begin returned error: %v", err)
+	}
+	if err := adapter.upsertCustomFieldDefs(tx, u); err != nil {
+		t.Fatalf("upsertCustomFieldDefs returned error: %v", err)
+	}
+	if err := tx.End(); err != nil {
+		t.Fatalf("rollback returned error: %v", err)
+	}
+	// The cache must not retain defs from the rolled-back transaction.
+	adapter.resetCustomFieldDefs()
+
+	if got := countRows(); got != 0 {
+		t.Fatalf("expected 0 rows after rollback, got %d", got)
+	}
+
+	// Retrying identical definitions after the rollback must reinsert them.
+	if err := db.With(func(tx *libmodel.Tx) error {
+		return adapter.upsertCustomFieldDefs(tx, u)
+	}); err != nil {
+		t.Fatalf("retry upsert returned error: %v", err)
+	}
+	if got := countRows(); got != len(defs) {
+		t.Fatalf("expected %d rows after retry, got %d", len(defs), got)
 	}
 }
 